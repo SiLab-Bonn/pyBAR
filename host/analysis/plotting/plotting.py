ddc7cb8cc6ab31a872f54f423de87558de125e16
Jens Janssen <janssen@physik.uni-bonn.de>
2013-05-18 21:51:37 +0000

    WORKING RELEASE backup copy just in case

--- host/analysis/plotting/plotting.py
+++ host/analysis/plotting/plotting.py
@@ -4,6 +4,7 @@ from matplotlib import colors, cm
 
 
 def make_occupancy(cols, rows, max_occ = None):
+    plt.clf()
     H, xedges, yedges = np.histogram2d(rows, cols, bins = (336, 80), range = [[1,336], [1,80]])
     #print xedges, yedges
     extent = [yedges[0]-0.5, yedges[-1]+0.5, xedges[-1]+0.5, xedges[0]-0.5]
@@ -18,9 +19,12 @@ def make_occupancy(cols, rows, max_occ = None):
     plt.ylabel('Row')
     plt.colorbar(boundaries = bounds, cmap = cmap, norm = norm, ticks = bounds)
 
-def plot_occupancy(cols, rows, max_occ = None):
+def plot_occupancy(cols, rows, max_occ = None, filename = None):
     make_occupancy(cols, rows, max_occ)
-    plt.show()
+    if filename is None:
+        plt.show()
+    else:
+        plt.savefig(filename)
     
 def save_occupancy(filename, cols, rows, max_occ = None):
     make_occupancy(cols, rows, max_occ)
