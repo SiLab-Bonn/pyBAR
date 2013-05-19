56544dd60b82285b3488bf53a626f44493b2a0b9
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-05-19 00:54:54 +0000

    -added options to base_scan and did some optimizations

--- trunk/host/scan/scan_utils.py
+++ trunk/host/scan/scan_utils.py
@@ -14,7 +14,7 @@ class FEI4ScanUtils(object):
         self.register = register
         self.register_utils = register_utils
     
-    def base_scan(self, command, repeat = 100, mask = 6, steps = None, dcs = None, hardware_repeat = False, same_mask_for_all_dc = False, read_function = None, digital_injection = False):
+    def base_scan(self, command, repeat = 100, mask = 6, steps = None, dcs = None, hardware_repeat = False, same_mask_for_all_dc = False, read_function = None, digital_injection = False, enable_c_high = True, enable_c_low = True, shift_masks = ["Enable"]):
         if not isinstance(command, BitVector.BitVector):
             raise TypeError
         
@@ -33,14 +33,16 @@ class FEI4ScanUtils(object):
         
         # preparing for scan
         commands = []
+        self.register.set_pixel_register_value("C_High", 1 if enable_c_high else 0)
+        self.register.set_pixel_register_value("C_Low", 1 if enable_c_low else 0)
         if digital_injection == True:
             #self.register.set_global_register_value("CalEn", 1) # for GlobalPulse instead Cal-Command
             self.register.set_global_register_value("DIGHITIN_SEL", 1)
-            commands.extend(self.register.get_commands("wrregister", name = ["DIGHITIN_SEL"]))
         else:
-            dimension = (80,336)
-            zero_mask = np.zeros(dimension, dtype = np.uint8)
-            self.register.set_pixel_register_value("EnableDigInj", zero_mask)
+            self.register.set_global_register_value("DIGHITIN_SEL", 0)
+            self.register.set_pixel_register_value("EnableDigInj", 0)
+        commands.extend(self.register.get_commands("wrregister", name = ["DIGHITIN_SEL"]))
+        commands.extend(self.register.get_commands("wrfrontend", same_mask_for_all_dc = True, name = ["C_High", "C_Low"]))
         self.register_utils.send_commands(commands)
             
         for mask_step in mask_steps:# range(steps):
@@ -51,14 +53,15 @@ class FEI4ScanUtils(object):
             #plt.pcolor(np.transpose(mask_array))
             #plt.colorbar()
             #plt.savefig('mask_step'+str(mask_step)+'.eps')
-            self.register.set_pixel_register_value("Enable", mask_array)
-            if digital_injection == True:
+            map(lambda mask: self.register.set_pixel_register_value(mask, mask_array), [shift_mask for shift_mask in shift_masks if (shift_mask.lower() != "EnableDigInj".lower())])
+            commands.extend(self.register.get_commands("wrfrontend", same_mask_for_all_dc = same_mask_for_all_dc, name = shift_masks))
+            if digital_injection == True: # FIXME: need to write EnableDigInj to FE?
                 self.register.set_pixel_register_value("EnableDigInj", mask_array)
-            commands.extend(self.register.get_commands("wrfrontend", same_mask_for_all_dc = same_mask_for_all_dc, name = ["Enable"]))
-            commands.extend(self.register.get_commands("wrfrontend", same_mask_for_all_dc = same_mask_for_all_dc if digital_injection == True else True, name = ["EnableDigInj"]))
-            if digital_injection == True:
+                commands.extend(self.register.get_commands("wrfrontend", same_mask_for_all_dc = same_mask_for_all_dc, name = ["EnableDigInj"]))
                 self.register.set_global_register_value("DIGHITIN_SEL", 1)
-                commands.extend(self.register.get_commands("wrregister", name = ["DIGHITIN_SEL"]))
+                commands.extend(self.register.get_commands("wrregister", name = ["DIGHITIN_SEL"])) # write DIGHITIN_SEL mask last
+#             else:
+#                 commands.extend(self.register.get_commands("wrfrontend", same_mask_for_all_dc = True, name = ["EnableDigInj"]))
             self.register_utils.send_commands(commands)
             print repeat, 'injection(s):', 'mask step', mask_step
             
