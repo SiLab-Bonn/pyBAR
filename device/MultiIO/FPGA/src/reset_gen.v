ffdfa3b99feef86fb91e4926aa6868dff86d0073
Jens Janssen <janssen@physik.uni-bonn.de>
2013-06-22 09:07:19 +0000

    code cleanup

--- device/MultiIO/FPGA/src/reset_gen.v
+++ device/MultiIO/FPGA/src/reset_gen.v
@@ -1,18 +1,17 @@
+`timescale 1 ps / 1ps
+`default_nettype none
 
 module reset_gen
 (
-  CLK,                     
-  RST                 
-); 
+    input wire              CLK,
+    output wire             RST
+);
 
-input CLK;
-output RST;
+reg [8:0] rst_cnt;
+initial rst_cnt = 9'b1_0000_0000;
 
- reg [8:0] rst_cnt;
- 
- initial rst_cnt = 9'b1_0000_0000;
- always@(posedge CLK)
-     if(rst_cnt[8] == 1)
+always@(posedge CLK)
+    if(rst_cnt[8] == 1)
         rst_cnt <= rst_cnt +1;
 
 assign RST = rst_cnt[8];
