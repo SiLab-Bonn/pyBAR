bb6739aeb39a1c53521da87760fa53149a96cdbe
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-08-08 13:06:15 +0000

    PRJ: add scan configuration file, import configuration from this file to keep other files clean

---
+++ trunk/host/scan_configuration.py
@@ -0,0 +1,4 @@
+chip_flavor = 'fei4a'
+config_file = r'C:\pyats\trunk\host\config\fei4default\configs\std_cfg_'+chip_flavor+'.cfg'
+bit_file = r'C:\pyats\trunk\host\config\FPGA\top.bit'
+outdir = r"C:\data"
