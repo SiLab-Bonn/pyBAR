5da73d125c7a6aa5eb17d25162fd3fcb811e5989
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-08-20 09:57:56 +0000

    SVN: revert commit

--- trunk/host/test_register.py
+++ trunk/host/test_register.py
@@ -1,32 +1,26 @@
 from scan.scan import ScanBase
 
-class TestRegisters(ScanBase):
-    def __init__(self, config_file, definition_file = None, bit_file = None, device = None, scan_identifier = "test_register", outdir = None):
-        super(TestRegisters, self).__init__(config_file, definition_file, bit_file, device, scan_identifier, outdir)
+chip_flavor = 'fei4a'
+config_file = r'C:\Users\Jens\Desktop\Python\python_workspace\test\source\config\fei4default\configs\std_cfg_'+chip_flavor+'.cfg'
+bit_file = r'C:\Users\Jens\Desktop\ModularReadoutSystem\device\trunk\MIO\FPGA\FEI4\ise\top.bit'
 
-    def start(self, configure = True):
-        super(TestRegisters, self).start(configure)
-        
-#         number_of_errors = scan.register_utils.test_global_register()
-#         print 'Global Register Test: Found', number_of_errors, "error(s)"
-        
-        #number_of_errors = scan.register_utils.test_pixel_register()
-        #print 'Pixel Register Test: Found', number_of_errors, "error(s)"
-        
-        sn = scan.register_utils.read_chip_sn()
-        print "Chip S/N:", sn
-        
-        print 'Reset SRAM FIFO...'
-        scan.readout_utils.reset_sram_fifo()
-        print 'Done!'
+class TestRegisters(ScanBase):
+    def __init__(self, config_file, bit_file):
+        super(TestRegisters, self).__init__(config_file, bit_file)
 
 if __name__ == "__main__":
-    chip_flavor = 'fei4b'
-    config_file = r'C:\pyats\trunk\host\config\fei4default\configs\plsr_cfg_'+chip_flavor+'.cfg'
-    bit_file = r'C:\pyats\trunk\host\config\FPGA\top.bit'
+    scan = TestRegisters(config_file, bit_file)
     
-    scan = TestRegisters(config_file = config_file, bit_file = bit_file)
+    number_of_errors = scan.register_utils.test_global_register()
+    print 'Global Register Test: Found', number_of_errors, "error(s)"
     
-    scan.start()
-
+    number_of_errors = scan.register_utils.test_pixel_register()
+    print 'Pixel Register Test: Found', number_of_errors, "error(s)"
+    
+    sn = scan.register_utils.read_chip_sn()
+    print "Chip S/N:", sn
+    
+    print 'Reset SRAM FIFO...'
+    scan.readout_utils.reset_sram_fifo()
+    print 'Done!'
     
