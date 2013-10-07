40404aff5395df46a348acb22a59785eb3c54c39
Jens Janssen <janssen@physik.uni-bonn.de>
2013-10-07 10:46:41 +0000

    ENH: follow new scan implementation

--- host/scan_analog.py
+++ host/scan_analog.py
@@ -17,9 +17,10 @@ class AnalogScan(ScanBase):
     def __init__(self, config_file, definition_file = None, bit_file = None, device = None, scan_identifier = "scan_analog", scan_data_path = None):
         super(AnalogScan, self).__init__(config_file = config_file, definition_file = definition_file, bit_file = bit_file, device = device, scan_identifier = scan_identifier, scan_data_path = scan_data_path)
         
-    def start(self, configure = True):
-        super(AnalogScan, self).start(configure)
-        
+#     def start(self, configure = True):
+#         super(AnalogScan, self).start(configure)
+
+    def scan(self):
         self.readout.start()
         
         commands = []
@@ -64,4 +65,5 @@ class AnalogScan(ScanBase):
 if __name__ == "__main__":
     import configuration
     scan = AnalogScan(config_file = configuration.config_file, bit_file = configuration.bit_file, scan_data_path = configuration.scan_data_path)
-    scan.start()
+    scan.start(use_thread = False)
+    scan.stop()
