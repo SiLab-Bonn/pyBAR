479383f3daa1b62175587d887f6acb8b9efe2616
davidlp <davidlp@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-10-09 17:30:45 +0000

    PRJ: renaming to signal that this is the swig test script

---
+++ trunk/host/analysis/RawDataConverter/Test_Swig.py
@@ -0,0 +1,33 @@
+# test the converter class
+import converter
+print "Creating converter object... ",
+Converter = converter.Converter()
+print "successfull"
+# Try calling some methods
+print "Testing conversion methods... ",
+Converter.setWarningOutput(False);
+Converter.setDebugOutput(False)
+Converter.setInfoOutput(False)
+Converter.setMaxTot(13)
+Converter.setNbCIDs(16)
+Converter.setFEi4B(False)
+Converter.setOutFileName('out.h5')
+Converter.createHitsTable(False)
+Converter.createMetaData(True)
+Converter.createErrorHist(True)
+Converter.createServiceRecordHist(True)
+Converter.createOccupancyHist(True)
+Converter.createRelBcidHist(True);
+Converter.createTotHist(True);
+Converter.createThresholdHists(False)
+Converter.createParameterData(False)
+#Converter.printOptions()
+print "successfull"
+print "Testing conversion... ",
+if(Converter.convertTable('in.h5')):
+    print "successfull"
+    #Converter.printSummary()
+else:
+    print "failed"
+
+
