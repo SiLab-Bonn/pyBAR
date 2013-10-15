bb3267fc53be08bc7954609b929de5116dd36299
DavidLP <pohl@physik.uni-bonn.de>
2013-10-15 15:42:42 +0000

    ENH: new data structure descriptions needed for pytables

--- host/analysis/data_struct.py
+++ host/analysis/data_struct.py
@@ -1,6 +1,5 @@
 import tables as tb
 
-
 class MetaTable(tb.IsDescription):
     start_index = tb.UInt32Col(pos=0)
     stop_index = tb.UInt32Col(pos=1)
@@ -10,3 +9,20 @@ class MetaTable(tb.IsDescription):
     timestamp = tb.Float64Col(pos=3)
     error = tb.UInt32Col(pos=4)
 
+class HitInfoTable(tb.IsDescription):
+    event_number = tb.UInt32Col(pos=0)
+    trigger_number = tb.UInt32Col(pos=1)
+    relative_BCID = tb.UInt8Col(pos=2)
+    LVLID = tb.UInt16Col(pos=3)
+    column = tb.UInt8Col(pos=4)
+    row = tb.UInt16Col(pos=5)
+    tot = tb.UInt8Col(pos=6)
+    BCID = tb.UInt16Col(pos=7)
+    trigger_status = tb.UInt8Col(pos=8)
+    service_record = tb.UInt32Col(pos=9)
+    event_status = tb.UInt8Col(pos=10)
+    
+class MetaInfoOutTable(tb.IsDescription):
+    event_number = tb.UInt32Col(pos=0)
+    time_stamp = tb.Float64Col(pos=1)
+    error_code = tb.UInt32Col(pos=2)
