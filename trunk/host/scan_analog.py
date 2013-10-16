35382885116951ad7b15007f888ae36eb2572352
davidlp <davidlp@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-10-16 16:36:05 +0000

    ENH: automated analyzes at the end of the scan added

--- trunk/host/scan_analog.py
+++ trunk/host/scan_analog.py
@@ -1,4 +1,3 @@
-from analysis.plotting.plotting import plot_occupancy
 import pprint
 import time
 import struct
@@ -16,11 +15,8 @@ from scan.scan import ScanBase
 class AnalogScan(ScanBase):
     def __init__(self, config_file, definition_file = None, bit_file = None, device = None, scan_identifier = "scan_analog", scan_data_path = None):
         super(AnalogScan, self).__init__(config_file = config_file, definition_file = definition_file, bit_file = bit_file, device = device, scan_identifier = scan_identifier, scan_data_path = scan_data_path)
-        
-#     def start(self, configure = True):
-#         super(AnalogScan, self).start(configure)
 
-    def scan(self):
+    def scan(self, **kwargs):
         self.readout.start()
         
         commands = []
@@ -59,11 +55,21 @@ class AnalogScan(ScanBase):
                 row_meta['stop_index'] = total_words
                 row_meta.append()
                 meta_data_table_h5.flush()
-        
-        plot_occupancy(*zip(*self.readout.get_col_row(data_words)), max_occ = repeat*2, filename = self.scan_data_filename+".pdf")
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
     import configuration
-    scan = AnalogScan(config_file = configuration.config_file, bit_file = configuration.bit_file, scan_data_path = configuration.scan_data_path)
+    scan = AnalogScan(config_file = configuration.config_file, bit_file  = configuration.bit_file, scan_data_path = configuration.scan_data_path)
     scan.start(use_thread = False)
     scan.stop()
+    from analysis.analyze_raw_data import AnalyzeRawData
+    output_file = scan.scan_data_filename+"_interpreted.h5"
+    with AnalyzeRawData(input_file = scan.scan_data_filename+".h5", output_file = output_file) as analyze_raw_data:
+        analyze_raw_data.interpret_word_table(FEI4B = True if(configuration.chip_flavor == 'fei4b') else False)
+        import analysis.plotting.plotting as plotting
+        with tb.openFile(output_file, 'r') as in_file:
+            plotting.plot_event_errors(error_hist = in_file.root.HistErrorCounter, filename = scan.scan_data_filename+"_eventErrors.pdf")
+            plotting.plot_service_records(service_record_hist = in_file.root.HistServiceRecord, filename = scan.scan_data_filename+"_serviceRecords.pdf")
+            plotting.plot_trigger_errors(trigger_error_hist=in_file.root.HistTriggerErrorCounter, filename = scan.scan_data_filename+"_tiggerErrors.pdf")
+            plotting.plot_tot(tot_hist=in_file.root.HistTot, filename = scan.scan_data_filename+"_tot.pdf")
+            plotting.plot_relative_bcid(relative_bcid_hist = in_file.root.HistRelBcid, filename = scan.scan_data_filename+"_relativeBCID.pdf")
+            plotting.plotThreeWay(hist = in_file.root.HistOcc[:,:,0], title = "Occupancy", label = "occupancy", filename = scan.scan_data_filename+"_occupancy.pdf")
