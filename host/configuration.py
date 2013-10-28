7c5d2e1b146424b3cb5af56e1aa2127e67358262
Jens Janssen <janssen@physik.uni-bonn.de>
2013-10-28 22:54:46 +0000

    PRJ: give more infos to the user

--- host/configuration.py
+++ host/configuration.py
@@ -1,8 +1,8 @@
 import os
 
-path = os.getcwd()
+path = os.getcwd() # current path
 
-chip_flavor = 'fei4a'
-config_file = os.path.join(path, r'config/fei4/configs/std_cfg_'+chip_flavor+'.cfg')
-bit_file = os.path.join(path, r'config/fpga/top.bit')
-scan_data_path = os.path.join(path, r'data')
+chip_flavor = 'fei4a' # chip flavor (e.g. fei4a, fei4b)
+config_file = os.path.join(path, r'config/fei4/configs/std_cfg_'+chip_flavor+'.cfg') # path to the FE configuration file
+bit_file = os.path.join(path, r'config/fpga/top.bit') # path to the FPGA bit file (usually no need to change it)
+scan_data_path = os.path.join(path, r'data') # data output path
