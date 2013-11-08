81d6191a85c98cf1e6b302c2fe8b806428836fa5
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-11-08 12:55:53 +0000

    ENH: self trigger scan stores data

--- trunk/host/scan_fei4_self_trigger.py
+++ trunk/host/scan_fei4_self_trigger.py
@@ -8,6 +8,7 @@ import math
 
 import numpy as np
 from scan.scan import ScanBase
+from daq.readout import open_raw_data_file
 
 logging.basicConfig(level=logging.INFO, format = "%(asctime)s [%(levelname)-8s] (%(threadName)-10s) %(message)s")
 
@@ -40,72 +41,72 @@ class FEI4SelfTriggerScan(ScanBase):
         # preload command
         #lvl1_command = self.register.get_commands("zeros", length=24)[0]+self.register.get_commands("lv1")[0]#+BitVector.BitVector(size = 10000)
         #self.register_utils.set_command(lvl1_command)
-
-        self.readout.start()
-        
-        wait_for_first_data = True
-        
-        
-        timeout_no_data = 60 # secs
-        scan_timeout = 1200
-        last_iteration = time.time()
-        saw_no_data_at_time = last_iteration
-        saw_data_at_time = last_iteration
-        scan_start_time = last_iteration
-        no_data_at_time = last_iteration
-        time_from_last_iteration = 0
-        scan_stop_time = scan_start_time + scan_timeout
-        while not self.stop_thread_event.wait(self.readout.readout_interval):
+        with open_raw_data_file(filename = self.scan_data_filename, title=self.scan_identifier) as raw_data_file:
+            self.readout.start()
             
-#                 if logger.isEnabledFor(logging.DEBUG):
-#                     lost_data = self.readout.get_lost_data_count()
-#                     if lost_data != 0:
-#                         logging.debug('Lost data count: %d', lost_data)
-#                         logging.debug('FIFO fill level: %4f', (float(fifo_size)/2**20)*100)
-#                         logging.debug('Collected triggers: %d', self.readout_utils.get_trigger_number())
-
-            if scan_start_time is not None and time.time() > scan_stop_time:
-                logging.info('Reached maximum scan time. Stopping Scan...')
-                self.stop_thread_event.set()
-            # TODO: read 8b10b decoder err cnt
-#                 if not self.readout_utils.read_rx_status():
-#                     logging.info('Lost data sync. Starting synchronization...')
-#                     self.readout_utils.set_ext_cmd_start(False)
-#                     if not self.readout_utils.reset_rx(1000):
-#                         logging.info('Failed. Stopping scan...')
-#                         self.stop_thread_event.set()
-#                     else:
-#                         logging.info('Done!')
-#                         self.readout_utils.set_ext_cmd_start(True)
-
-            time_from_last_iteration = time.time() - last_iteration
+            wait_for_first_data = True
+            
+            
+            timeout_no_data = 60 # secs
+            scan_timeout = 1200
             last_iteration = time.time()
-            try:
-                item = self.readout.data.popleft()
-                logging.info('data words: %d' % item["data"].shape[0])
-            except IndexError:
-                logging.info('no data words')
-                no_data_at_time = last_iteration
-                if wait_for_first_data == False and saw_no_data_at_time > (saw_data_at_time + timeout_no_data):
-                    logging.info('Reached no data timeout. Stopping Scan...')
-                    self.stop_thread_event.set()
-                elif wait_for_first_data == False:
-                    saw_no_data_at_time = no_data_at_time
-                
-                if no_data_at_time > (saw_data_at_time + 10):
-                    scan_stop_time += time_from_last_iteration
-                    
-                continue
-            
+            saw_no_data_at_time = last_iteration
             saw_data_at_time = last_iteration
+            scan_start_time = last_iteration
+            no_data_at_time = last_iteration
+            time_from_last_iteration = 0
+            scan_stop_time = scan_start_time + scan_timeout
+            while not self.stop_thread_event.wait(self.readout.readout_interval):
+                
+    #                 if logger.isEnabledFor(logging.DEBUG):
+    #                     lost_data = self.readout.get_lost_data_count()
+    #                     if lost_data != 0:
+    #                         logging.debug('Lost data count: %d', lost_data)
+    #                         logging.debug('FIFO fill level: %4f', (float(fifo_size)/2**20)*100)
+    #                         logging.debug('Collected triggers: %d', self.readout_utils.get_trigger_number())
+    
+                if scan_start_time is not None and time.time() > scan_stop_time:
+                    logging.info('Reached maximum scan time. Stopping Scan...')
+                    self.stop_thread_event.set()
+                # TODO: read 8b10b decoder err cnt
+    #                 if not self.readout_utils.read_rx_status():
+    #                     logging.info('Lost data sync. Starting synchronization...')
+    #                     self.readout_utils.set_ext_cmd_start(False)
+    #                     if not self.readout_utils.reset_rx(1000):
+    #                         logging.info('Failed. Stopping scan...')
+    #                         self.stop_thread_event.set()
+    #                     else:
+    #                         logging.info('Done!')
+    #                         self.readout_utils.set_ext_cmd_start(True)
+    
+                time_from_last_iteration = time.time() - last_iteration
+                last_iteration = time.time()
+                try:
+                    raw_data_file.append((self.readout.data.popleft()))
+                    logging.info('data words')
+                except IndexError:
+                    logging.info('no data words')
+                    no_data_at_time = last_iteration
+                    if wait_for_first_data == False and saw_no_data_at_time > (saw_data_at_time + timeout_no_data):
+                        logging.info('Reached no data timeout. Stopping Scan...')
+                        self.stop_thread_event.set()
+                    elif wait_for_first_data == False:
+                        saw_no_data_at_time = no_data_at_time
+                    
+                    if no_data_at_time > (saw_data_at_time + 10):
+                        scan_stop_time += time_from_last_iteration
+                        
+                    continue
+                
+                saw_data_at_time = last_iteration
+                
+                if wait_for_first_data == True:
+                    logging.info('Taking data...')
+                    wait_for_first_data = False
             
-            if wait_for_first_data == True:
-                logging.info('Taking data...')
-                wait_for_first_data = False
-        
-        self.stop_thread_event.set()
-        
-        self.readout.stop()
+            self.stop_thread_event.set()
+            
+            self.readout.stop()
 
 if __name__ == "__main__":
     import configuration
