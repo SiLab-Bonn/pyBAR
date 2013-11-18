4369b595295297c50b725b388c98b1810bb4021b
Jens Janssen <janssen@physik.uni-bonn.de>
2013-11-18 15:33:59 +0000

    ENH: added documentation ENH: default mask is 3 ENH: renaming of some parameters for better understanding MAINT: cleanup

--- host/scan/scan_utils.py
+++ host/scan/scan_utils.py
@@ -1,14 +1,6 @@
-import logging
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import colors, cm
-import time
-import struct
-
 import BitVector
 
-from utils.utils import get_all_from_queue
-
+import logging
 logging.basicConfig(level=logging.INFO, format = "%(asctime)s [%(levelname)-8s] (%(threadName)-10s) %(message)s")
 
 class FEI4ScanUtils(object):
@@ -16,22 +8,48 @@ class FEI4ScanUtils(object):
         self.register = register
         self.register_utils = register_utils
     
-    def base_scan(self, command, repeat = 100, mask = 6, steps = None, dcs = None, hardware_repeat = False, same_mask_for_all_dc = False, read_function = None, digital_injection = False, enable_c_high = None, enable_c_low = None, shift_masks = ["Enable", "C_High", "C_Low"]):
+    def base_scan(self, command, repeat = 100, hardware_repeat = True, mask = 3, mask_steps = None, double_columns = None, same_mask_for_all_dc = False, eol_function = None, digital_injection = False, enable_c_high = None, enable_c_low = None, shift_masks = ["Enable", "C_High", "C_Low"]):
+        '''Implementation of the scan loops (mask shifting, loop over double columns, repeatedly sending any arbitrary command).
+        
+        Parameters
+        ----------
+        command : BitVector
+            (FEI4) command that will be sent out serially.
+        repeat : int
+            Number of injections.
+        hardware_repeat : bool
+            If true, use FPGA to repeat commands. In general this is much faster than doing this in software.
+        mask : int
+            Number of mask steps.
+        mask_steps : list, tuple
+            List of mask steps which will be applied. Default is all mask steps. From 0 to (mask-1).
+        double_columns : list, tuple
+            List of double columns which will be enabled during scan. Default is all double columns. From 0 to 39 (double columns counted from zero).
+        same_mask_for_all_dc : bool
+            Use same mask for all double columns. Enabling this is in general not a good idea since all double columns will have the same configuration but the scan speed can increased by an order of magnitude.  
+        eol_function : function
+            End of loop function that will be called each time the innermost loop ends.  
+        digital_injection : bool
+            Enables digital injection.
+        enable_c_high : bool
+            Enables C_High pixel mask. Will be overwritten by shift_mask.
+        enable_c_low : bool
+            Enables C_Low pixel mask. Will be overwritten by shift_mask.
+        shift_masks : list, tuple
+            List of pixel masks that will be shifted. 
+        '''
         if not isinstance(command, BitVector.BitVector):
             raise TypeError
         
+        # pre-calculate often used commands
         conf_mode_command = self.register.get_commands("confmode")
         run_mode_command = self.register.get_commands("runmode")
         
-        if steps == None or steps == []:
+        if mask_steps == None or mask_steps == []:
             mask_steps = range(mask)
-        else:
-            mask_steps = steps
             
-        if dcs == None or dcs == []:
-            dc_steps = range(40)
-        else:
-            dc_steps = dcs
+        if double_columns == None or double_columns == []:
+            double_columns = range(40)
         
         # preparing for scan
         commands = []
@@ -51,7 +69,7 @@ class FEI4ScanUtils(object):
             commands.extend(self.register.get_commands("wrfrontend", same_mask_for_all_dc = True, name = ["C_Low"]))
         self.register_utils.send_commands(commands)
             
-        for mask_step in mask_steps:# range(steps):
+        for mask_step in mask_steps:# range(mask_steps):
             commands = []
             commands.extend(conf_mode_command)
             mask_array = self.register_utils.make_pixel_mask(mask = mask, row_offset = mask_step)
@@ -61,7 +79,7 @@ class FEI4ScanUtils(object):
             #plt.savefig('mask_step'+str(mask_step)+'.eps')
             map(lambda mask: self.register.set_pixel_register_value(mask, mask_array), [shift_mask for shift_mask in shift_masks if (shift_mask.lower() != "EnableDigInj".lower())])
             commands.extend(self.register.get_commands("wrfrontend", same_mask_for_all_dc = same_mask_for_all_dc, name = shift_masks))
-            if digital_injection == True: # FIXME: need to write EnableDigInj to FE?
+            if digital_injection == True: # TODO: write EnableDigInj to FE or do it manually?
                 self.register.set_pixel_register_value("EnableDigInj", mask_array)
                 commands.extend(self.register.get_commands("wrfrontend", same_mask_for_all_dc = same_mask_for_all_dc, name = ["EnableDigInj"]))
                 self.register.set_global_register_value("DIGHITIN_SEL", 1)
@@ -71,7 +89,7 @@ class FEI4ScanUtils(object):
             self.register_utils.send_commands(commands)
             logging.info('%d injection(s): mask step %d' % (repeat, mask_step))
             
-            for dc in dc_steps:
+            for dc in double_columns:
                 commands = []
                 #commands.extend(conf_mode_command)
                 self.register.set_global_register_value("Colpr_Addr", dc)
@@ -90,7 +108,7 @@ class FEI4ScanUtils(object):
                     for _ in range(repeat):
                         self.register_utils.send_command()
                 try:
-                    read_function()
+                    eol_function()
                 except TypeError:
                     pass
 
