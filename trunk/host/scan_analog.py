4c84fb9748bc6f1195c4d72457f0fd83699de79b
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-11-18 14:03:16 +0000

    ENH: added documentation ENH: default mask is 3

--- trunk/host/scan_analog.py
+++ trunk/host/scan_analog.py
@@ -10,10 +10,28 @@ class AnalogScan(ScanBase):
     def __init__(self, config_file, definition_file = None, bit_file = None, device = None, scan_identifier = "scan_analog", scan_data_path = None):
         super(AnalogScan, self).__init__(config_file = config_file, definition_file = definition_file, bit_file = bit_file, device = device, scan_identifier = scan_identifier, scan_data_path = scan_data_path)
 
-    def scan(self, configure = True, mask = 6, repeat = 100, steps = []):
+    def scan(self, configure = True, mask = 3, repeat = 100, scan_parameter = 'PlsrDAC', scan_paramter_value = 100):
+        '''Scan loop
+        
+        Parameters
+        ----------
+        mask : int
+            Number of mask steps.
+        repeat : int
+            Number of injections per scan step.
+        scan_parameter : string
+            Name of global register.
+        scan_paramter_value : int
+            Specify scan steps. These values will be written into global register scan_parameter.
+            
+        Note
+        ----
+        This scan is very similar to the threshold scan.
+        This scan can also be used for ToT verification: change scan_paramter_value to desired injection charge (in units of PulsrDAC).
+        '''
         commands = []
         commands.extend(self.register.get_commands("confmode"))
-        self.register.set_global_register_value("PlsrDAC", 100)
+        self.register.set_global_register_value(scan_parameter, scan_paramter_value)
         commands.extend(self.register.get_commands("wrregister", name = ["PlsrDAC"]))
         self.register_utils.send_commands(commands)
         
@@ -34,6 +52,7 @@ class AnalogScan(ScanBase):
         from analysis.analyze_raw_data import AnalyzeRawData
         output_file = scan.scan_data_filename+"_interpreted.h5"
         with AnalyzeRawData(input_file = scan.scan_data_filename+".h5", output_file = output_file) as analyze_raw_data:
+            analyze_raw_data.create_tot_hist = True
             analyze_raw_data.interpret_word_table(FEI4B = scan.register.fei4b)
             analyze_raw_data.plot_histograms(scan_data_filename = scan.scan_data_filename)
 
@@ -43,4 +62,3 @@ if __name__ == "__main__":
     scan.start(use_thread = False)
     scan.stop()
     scan.analyze()
-        
