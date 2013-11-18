6974c48f3618291b53b49f4d334d04024882b0e1
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-11-18 14:09:38 +0000

    ENH: update scan description MAINT: more cleanup

--- trunk/host/scan_digital.py
+++ trunk/host/scan_digital.py
@@ -1,9 +1,8 @@
 from daq.readout import get_col_row_array_from_data_record_array, save_raw_data_from_data_dict_iterable, convert_data_array, data_array_from_data_dict_iterable, is_data_record, is_data_from_channel
 from analysis.plotting.plotting import plot_occupancy
+from analysis.analyze_raw_data import AnalyzeRawData
 
 from scan.scan import ScanBase
-
-import numpy as np
 
 import logging
 logging.basicConfig(level=logging.INFO, format = "%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
@@ -12,11 +11,20 @@ class DigitalScan(ScanBase):
     def __init__(self, config_file, definition_file = None, bit_file = None, device = None, scan_identifier = "scan_digital", scan_data_path = None):
         super(DigitalScan, self).__init__(config_file = config_file, definition_file = definition_file, bit_file = bit_file, device = device, scan_identifier = scan_identifier, scan_data_path = scan_data_path)
         
-    def scan(self, configure = True, mask = 6, repeat = 100, steps = []):
+    def scan(self, configure = True, mask = 3, repeat = 100):
+        '''Scan loop
+        
+        Parameters
+        ----------
+        mask : int
+            Number of mask steps.
+        repeat : int
+            Number of injections.
+        '''
         self.readout.start()
         
         cal_lvl1_command = self.register.get_commands("cal")[0]+self.register.get_commands("zeros", length=35)[0]+self.register.get_commands("lv1")[0]+self.register.get_commands("zeros", mask_steps=mask)[0]
-        self.scan_utils.base_scan(cal_lvl1_command, repeat = repeat, mask = mask, steps = steps, dcs = [], same_mask_for_all_dc = True, hardware_repeat = True, enable_c_high = False, enable_c_low = False, digital_injection = True, read_function = None)
+        self.scan_utils.base_scan(cal_lvl1_command, repeat = repeat, mask = mask, steps = [], dcs = [], same_mask_for_all_dc = True, hardware_repeat = True, enable_c_high = False, enable_c_low = False, digital_injection = True, read_function = None)
         
         self.readout.stop(timeout=10.0)
         
@@ -26,13 +34,17 @@ class DigitalScan(ScanBase):
         # saving data
         save_raw_data_from_data_dict_iterable(self.readout.data, filename = self.scan_data_filename, title=self.scan_identifier)
         
+    def analyze(self):
+        output_file = scan.scan_data_filename+"_interpreted.h5"
+        with AnalyzeRawData(input_file = scan.scan_data_filename+".h5", output_file = output_file) as analyze_raw_data:
+            analyze_raw_data.create_tot_hist = False
+            analyze_raw_data.interpret_word_table(FEI4B = scan.register.fei4b)
+            analyze_raw_data.plot_histograms(scan_data_filename = scan.scan_data_filename)
+            
 if __name__ == "__main__":
     import configuration
     scan = DigitalScan(config_file = configuration.config_file, bit_file  = configuration.bit_file, scan_data_path = configuration.scan_data_path)
     scan.start(use_thread = False)
     scan.stop()
-    from analysis.analyze_raw_data import AnalyzeRawData
-    output_file = scan.scan_data_filename+"_interpreted.h5"
-    with AnalyzeRawData(input_file = scan.scan_data_filename+".h5", output_file = output_file) as analyze_raw_data:
-        analyze_raw_data.interpret_word_table(FEI4B = scan.register.fei4b)
-        analyze_raw_data.plotHistograms(scan_data_filename = scan.scan_data_filename)
+    scan.analyze()
+
