8e001ef6175fdcce66f4a064515bf4f1458fc600
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-11-19 14:21:20 +0000

    ENH: keep old definition of MetaTable ENH: added MetaTableV2 ENH: added generate_scan_parameter_description()

--- trunk/host/analysis/data_struct.py
+++ trunk/host/analysis/data_struct.py
@@ -1,6 +1,17 @@
 import tables as tb
 
+
 class MetaTable(tb.IsDescription):
+    start_index = tb.UInt32Col(pos=0)
+    stop_index = tb.UInt32Col(pos=1)
+    length = tb.UInt32Col(pos=2)
+    # https://github.com/PyTables/PyTables/issues/230
+    #timestamp = tb.Time64Col(pos=3)
+    timestamp = tb.Float64Col(pos=3)
+    error = tb.UInt32Col(pos=4)
+
+
+class MetaTableV2(tb.IsDescription):
     index_start = tb.UInt32Col(pos=0)
     index_stop = tb.UInt32Col(pos=1)
     data_length = tb.UInt32Col(pos=2)
@@ -9,7 +20,29 @@ class MetaTable(tb.IsDescription):
     timestamp_start = tb.Float64Col(pos=3)
     timestamp_stop = tb.Float64Col(pos=4)
     error = tb.UInt32Col(pos=5)
-    
+
+
+def generate_scan_parameter_description(scan_parameters):
+    '''Generate scan parameter dictionary. This is the only way to dynamically create table with dictionary, cannot be done with tables.IsDescription
+
+    Parameters
+    ----------
+    scan_parameters : list, tuple
+        List of scan parameters names (strings).
+
+    Returns
+    -------
+    table_description : dict
+        Table description.
+
+    Usage
+    -----
+    pytables.createTable(self.raw_data_file_h5.root, name = 'scan_parameters', description = generate_scan_parameter_description(['PlsrDAC']), title = 'scan_parameters', filters = filter_tables)
+    '''
+    table_description = dict([(key, tb.UInt32Col(pos=idx)) for idx, key in enumerate(scan_parameters)])
+    return table_description
+
+
 class HitInfoTable(tb.IsDescription):
     event_number = tb.UInt32Col(pos=0)
     trigger_number = tb.UInt32Col(pos=1)
@@ -22,17 +55,20 @@ class HitInfoTable(tb.IsDescription):
     trigger_status = tb.UInt8Col(pos=8)
     service_record = tb.UInt32Col(pos=9)
     event_status = tb.UInt8Col(pos=10)
-    
+
+
 class MetaInfoEventTable(tb.IsDescription):
     event_number = tb.UInt32Col(pos=0)
     time_stamp = tb.Float64Col(pos=1)
     error_code = tb.UInt32Col(pos=2)
-    
+
+
 class MetaInfoWordTable(tb.IsDescription):
     event_number = tb.UInt32Col(pos=0)
     start_word_index = tb.UInt32Col(pos=1)
     stop_word_index = tb.UInt32Col(pos=2)
-    
+
+
 class ClusterHitInfoTable(tb.IsDescription):
     event_number = tb.UInt32Col(pos=0)
     trigger_number = tb.UInt32Col(pos=1)
@@ -47,7 +83,8 @@ class ClusterHitInfoTable(tb.IsDescription):
     event_status = tb.UInt8Col(pos=10)
     cluster_id = tb.UInt16Col(pos=11)
     is_seed = tb.UInt8Col(pos=12)
-    
+
+
 class ClusterInfoTable(tb.IsDescription):
     event_number = tb.UInt32Col(pos=0)
     id = tb.UInt16Col(pos=1)
@@ -57,4 +94,3 @@ class ClusterInfoTable(tb.IsDescription):
     seed_column = tb.UInt8Col(pos=5)
     seed_row = tb.UInt16Col(pos=6)
     event_status = tb.UInt8Col(pos=7)
-
