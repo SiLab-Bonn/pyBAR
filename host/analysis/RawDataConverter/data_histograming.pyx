334cff33db982418ef273b913813dfebcad62c56
Jens Janssen <janssen@physik.uni-bonn.de>
2013-11-19 16:46:39 +0000

    MAINT: follow PEP8

--- host/analysis/RawDataConverter/data_histograming.pyx
+++ host/analysis/RawDataConverter/data_histograming.pyx
@@ -3,10 +3,10 @@
 
 import numpy as np
 cimport numpy as np
-from libcpp cimport bool    #to be able to use bool variables
+from libcpp cimport bool  # to be able to use bool variables
 
-np.import_array()   #if array is used it has to be imported, otherwise possible runtime error
-  
+np.import_array()  # if array is used it has to be imported, otherwise possible runtime error
+
 cdef extern from "Basis.h":
     cdef cppclass Basis:
         Basis()
@@ -24,60 +24,60 @@ cdef packed struct numpy_meta_data_v2:
     np.float64_t timestamp_start
     np.float64_t timestamp_stop
     np.uint32_t error
-    
+
 cdef packed struct numpy_hit_info:
-    np.uint32_t eventNumber  #event number value (unsigned long long: 0 to 18,446,744,073,709,551,615)
-    np.uint32_t triggerNumber #external trigger number for read out system
-    np.uint8_t relativeBCID #relative BCID value (unsigned char: 0 to 255)
-    np.uint16_t LVLID   #LVL1ID (unsigned short int: 0 to 65.535)
-    np.uint8_t column       #column value (unsigned char: 0 to 255)
-    np.uint16_t row     #row value (unsigned short int: 0 to 65.535)
-    np.uint8_t tot          #tot value (unsigned char: 0 to 255)
-    np.uint16_t BCID    #absolute BCID value (unsigned short int: 0 to 65.535)
-    np.uint8_t triggerStatus#event trigger status
-    np.uint32_t serviceRecord #event service records
-    np.uint8_t eventStatus #event status value (unsigned char: 0 to 255)
-    
+    np.uint32_t eventNumber  # event number value (unsigned long long: 0 to 18,446,744,073,709,551,615)
+    np.uint32_t triggerNumber  # external trigger number for read out system
+    np.uint8_t relativeBCID  # relative BCID value (unsigned char: 0 to 255)
+    np.uint16_t LVLID  # LVL1ID (unsigned short int: 0 to 65.535)
+    np.uint8_t column  # column value (unsigned char: 0 to 255)
+    np.uint16_t row  # row value (unsigned short int: 0 to 65.535)
+    np.uint8_t tot  # tot value (unsigned char: 0 to 255)
+    np.uint16_t BCID  # absolute BCID value (unsigned short int: 0 to 65.535)
+    np.uint8_t triggerStatus  # event trigger status
+    np.uint32_t serviceRecord  # event service records
+    np.uint8_t eventStatus  # event status value (unsigned char: 0 to 255)
+
 cdef packed struct numpy_par_info:
-    np.uint32_t scanParameter     #parameter setting
-          
+    np.uint32_t scanParameter  # parameter setting
+
 cdef extern from "Histogram.h":
     cdef cppclass MetaInfo:
         MetaInfo()
     cdef cppclass HitInfo:
         HitInfo()
     cdef cppclass ParInfo:
-        ParInfo() 
+        ParInfo()
     cdef cppclass Histogram(Basis):
         Histogram() except +
         void setErrorOutput(bool pToggle)
         void setWarningOutput(bool pToggle)
         void setInfoOutput(bool pToggle)
         void setDebugOutput(bool pToggle)
-        
+
         void createOccupancyHist(bool CreateOccHist)
         void createRelBCIDHist(bool CreateRelBCIDHist)
         void createTotHist(bool CreateTotHist)
-        
-        void getOccupancy(unsigned int& rNparameterValues, unsigned int*& rOccupancy, bool copy)  #returns the occupancy histogram for all hits
-        void getTotHist(unsigned int*& rTotHist, bool copy)          #returns the tot histogram for all hits
-        void getRelBcidHist(unsigned int*& rRelBcidHist, bool copy)   #returns the relative BCID histogram for all hits    
+
+        void getOccupancy(unsigned int& rNparameterValues, unsigned int*& rOccupancy, bool copy)  # returns the occupancy histogram for all hits
+        void getTotHist(unsigned int*& rTotHist, bool copy)  # returns the tot histogram for all hits
+        void getRelBcidHist(unsigned int*& rRelBcidHist, bool copy)  # returns the relative BCID histogram for all hits
 
         void addHits(HitInfo*& rHitInfo, const unsigned int& rNhits) except +
         void addScanParameter(const unsigned int& rNparInfoLength, ParInfo*& rParInfo)  except +
         void setNoScanParameter()
         void addMetaEventIndex(const unsigned int& rNmetaEventIndexLength, unsigned int*& rMetaEventIndex) except +
-       
-        unsigned int getMinParameter() #returns the minimum parameter from _parInfo
-        unsigned int getMaxParameter() #returns the maximum parameter from _parInfo
-        unsigned int getNparameters()  #returns the parameter range from _parInfo
-        
+
+        unsigned int getMinParameter()  # returns the minimum parameter from _parInfo
+        unsigned int getMaxParameter()  # returns the maximum parameter from _parInfo
+        unsigned int getNparameters()  # returns the parameter range from _parInfo
+
         void calculateThresholdScanArrays(double rMuArray[], double rSigmaArray[]) #takes the occupancy histograms for different parameters for the threshold arrays
-         
-        void test()         
+
+        void test()
 
 cdef class PyDataHistograming:
-    cdef Histogram* thisptr      # hold a C++ instance which we're wrapping
+    cdef Histogram* thisptr  # hold a C++ instance which we're wrapping
     def __cinit__(self):
         self.thisptr = new Histogram()
     def __dealloc__(self):
@@ -125,7 +125,7 @@ cdef class PyDataHistograming:
         return <unsigned int> self.thisptr.getNparameters()
 
     def calculate_threshold_scan_arrays(self, np.ndarray[np.float64_t, ndim=1] threshold, np.ndarray[np.float64_t, ndim=1] noise):
-        self.thisptr.calculateThresholdScanArrays(<double*> threshold.data, <double*> noise.data)    
+        self.thisptr.calculateThresholdScanArrays(<double*> threshold.data, <double*> noise.data)
 
     def test(self):
         self.thisptr.test()
