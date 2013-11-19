c09633c2c3958c4a9045fff711d54ee5a7f9c0df
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-11-19 15:45:23 +0000

    MAINT: follow PEP8 BUG: fix output PDF file name conflict

--- trunk/host/scan_analog.py
+++ trunk/host/scan_analog.py
@@ -5,15 +5,16 @@ from analysis.analyze_raw_data import AnalyzeRawData
 from scan.scan import ScanBase
 
 import logging
-logging.basicConfig(level=logging.INFO, format = "%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
+
 
 class AnalogScan(ScanBase):
-    def __init__(self, config_file, definition_file = None, bit_file = None, device = None, scan_identifier = "scan_analog", scan_data_path = None):
-        super(AnalogScan, self).__init__(config_file = config_file, definition_file = definition_file, bit_file = bit_file, device = device, scan_identifier = scan_identifier, scan_data_path = scan_data_path)
+    def __init__(self, config_file, definition_file=None, bit_file=None, device=None, scan_identifier="scan_analog", scan_data_path=None):
+        super(AnalogScan, self).__init__(config_file=config_file, definition_file=definition_file, bit_file=bit_file, device=device, scan_identifier=scan_identifier, scan_data_path=scan_data_path)
 
-    def scan(self, configure = True, mask = 3, repeat = 100, scan_parameter = 'PlsrDAC', scan_paramter_value = 100):
+    def scan(self, configure=True, mask=3, repeat=100, scan_parameter='PlsrDAC', scan_paramter_value=100):
         '''Scan loop
-        
+
         Parameters
         ----------
         mask : int
@@ -24,7 +25,7 @@ class AnalogScan(ScanBase):
             Name of global register.
         scan_paramter_value : int
             Specify scan steps. These values will be written into global register scan_parameter.
-            
+
         Note
         ----
         This scan is very similar to the threshold scan.
@@ -33,32 +34,32 @@ class AnalogScan(ScanBase):
         commands = []
         commands.extend(self.register.get_commands("confmode"))
         self.register.set_global_register_value(scan_parameter, scan_paramter_value)
-        commands.extend(self.register.get_commands("wrregister", name = ["PlsrDAC"]))
+        commands.extend(self.register.get_commands("wrregister", name=["PlsrDAC"]))
         self.register_utils.send_commands(commands)
-        
+
         self.readout.start()
-        
-        cal_lvl1_command = self.register.get_commands("cal")[0]+self.register.get_commands("zeros", length=40)[0]+self.register.get_commands("lv1")[0]+self.register.get_commands("zeros", mask_steps=mask)[0]
-        self.scan_loop(cal_lvl1_command, repeat = repeat, mask = mask, mask_steps = [], double_columns = [], same_mask_for_all_dc = True, hardware_repeat = True, digital_injection = False, eol_function = None)
-        
+
+        cal_lvl1_command = self.register.get_commands("cal")[0] + self.register.get_commands("zeros", length=40)[0] + self.register.get_commands("lv1")[0] + self.register.get_commands("zeros", mask_steps=mask)[0]
+        self.scan_loop(cal_lvl1_command, repeat=repeat, mask=mask, mask_steps=[], double_columns=[], same_mask_for_all_dc=True, hardware_repeat=True, digital_injection=False, eol_function=None)
+
         self.readout.stop(timeout=10.0)
-        
+
         # plotting data
-        plot_occupancy(*convert_data_array(data_array_from_data_dict_iterable(self.readout.data), filter_func=is_data_record, converter_func=get_col_row_array_from_data_record_array), max_occ = repeat*2, filename = self.scan_data_filename+".pdf")
-        
+        plot_occupancy(*convert_data_array(data_array_from_data_dict_iterable(self.readout.data), filter_func=is_data_record, converter_func=get_col_row_array_from_data_record_array), max_occ=repeat * 2, filename=self.scan_data_filename + "_occupancy.pdf")
+
         # saving data
-        save_raw_data_from_data_dict_iterable(self.readout.data, filename = self.scan_data_filename, title=self.scan_identifier)
-        
+        save_raw_data_from_data_dict_iterable(self.readout.data, filename=self.scan_data_filename, title=self.scan_identifier)
+
     def analyze(self):
-        output_file = scan.scan_data_filename+"_interpreted.h5"
-        with AnalyzeRawData(raw_data_file = scan.scan_data_filename+".h5", analyzed_data_file = output_file) as analyze_raw_data:
+        output_file = scan.scan_data_filename + "_interpreted.h5"
+        with AnalyzeRawData(raw_data_file=scan.scan_data_filename + ".h5", analyzed_data_file=output_file) as analyze_raw_data:
             analyze_raw_data.create_tot_hist = True
-            analyze_raw_data.interpret_word_table(FEI4B = scan.register.fei4b)
-            analyze_raw_data.plot_histograms(scan_data_filename = scan.scan_data_filename)
+            analyze_raw_data.interpret_word_table(FEI4B=scan.register.fei4b)
+            analyze_raw_data.plot_histograms(scan_data_filename=scan.scan_data_filename)
 
 if __name__ == "__main__":
     import configuration
-    scan = AnalogScan(config_file = configuration.config_file, bit_file  = configuration.bit_file, scan_data_path = configuration.scan_data_path)
-    scan.start(use_thread = False)
+    scan = AnalogScan(config_file=configuration.config_file, bit_file=configuration.bit_file, scan_data_path=configuration.scan_data_path)
+    scan.start(use_thread=False)
     scan.stop()
     scan.analyze()
