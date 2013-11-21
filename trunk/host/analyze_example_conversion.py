a318700b1966b479ede12e73cf7d9738aee8e2bb
davidlp <davidlp@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-11-21 16:13:14 +0000

    MAINT: use new raw data converter API

--- trunk/host/analyze_example_conversion.py
+++ trunk/host/analyze_example_conversion.py
@@ -1,52 +1,56 @@
 """This is an example module how to use the raw data analyzer. It takes a table with FE-I4 raw data and interprets, histograms and plots it.
-    The first "with-statement" interprets the raw data and can cluster hits in the same analysis loop if activated. The second "with-statement" just clusters
-    data from hits. This shows how to use the clusterizer afterwards if hit infos are already available. 
+    The first "with-statement" interprets the raw data and can also histogram and cluster hits in the same analysis loop if activated to save time. 
+    The second "with-statement" does the histogramig and clustering starting from hits. 
+    This shows how to use the analysis if hits are already available 
 """
-scan_name='scan_digital_0'
+scan_name = 'scan_threshold_13'
 
 chip_flavor = 'fei4a'
-input_file = "data/"+scan_name+".h5"
-output_file_hits = "data/"+scan_name+"_interpreted.h5"
-output_file_cluster = "data/"+scan_name+"_clustered.h5"
-scan_data_filename = "data/"+scan_name
- 
+input_file = 'data/'+scan_name + ".h5"
+output_file_hits = 'data/'+scan_name + "_interpreted.h5"
+output_file_hits_analyzed = 'data/'+scan_name + "_analyzed.h5"
+scan_data_filename = 'data/'+scan_name
+
 from datetime import datetime
 import logging
 from analysis.analyze_raw_data import AnalyzeRawData
-logging.basicConfig(level=logging.INFO, format = "%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
  
 start_time = datetime.now()
-with AnalyzeRawData(input_file = input_file, output_file = output_file_hits) as analyze_raw_data:
-    analyze_raw_data.create_hit_table = False            # can be set to false to omit hit table creation, std. setting is false
-    analyze_raw_data.create_cluster_hit_table = False   # adds the cluster id and seed info to each hit, std. setting is false
-    analyze_raw_data.create_cluster_table = False       # enables the creation of a table with all clusters, std. setting is false
-    
-    analyze_raw_data.create_occupancy_hist = True       # creates a colxrow histogram with accumulated hits for each scan parameter
-    analyze_raw_data.create_tot_hist = True             # creates a tot histogram
-    analyze_raw_data.create_rel_bcid_hist = True        # creates a histogram with the relative BCID of the hits
+with AnalyzeRawData(raw_data_file=input_file, analyzed_data_file=output_file_hits) as analyze_raw_data:
+    analyze_raw_data.create_hit_table = False  # can be set to false to omit hit table creation, std. setting is false
+    analyze_raw_data.create_cluster_hit_table = False  # adds the cluster id and seed info to each hit, std. setting is false
+    analyze_raw_data.create_cluster_table = False  # enables the creation of a table with all clusters, std. setting is false
+  
+    analyze_raw_data.create_occupancy_hist = True  # creates a colxrow histogram with accumulated hits for each scan parameter
+    analyze_raw_data.create_tot_hist = True  # creates a tot histogram
+    analyze_raw_data.create_rel_bcid_hist = True  # creates a histogram with the relative BCID of the hits
     analyze_raw_data.create_service_record_hist = True  # creates a histogram with all SR send out from the FE
-    analyze_raw_data.create_error_hist = True           # creates a histogram summing up the event errors that occurred
-    analyze_raw_data.create_trigger_error_hist = False  # creates a histogram summing up the trigger errors
-    analyze_raw_data.create_cluster_size_hist = False   # enables cluster size histogramming, can save some time, std. setting is false
-    analyze_raw_data.create_cluster_tot_hist = False    # enables cluster tot histogramming per cluster size, std. setting is false
-    analyze_raw_data.create_threshold_hists = False     # makes only sense if threshold scan data is analyzed, std. setting is false
-    analyze_raw_data.create_threshold_mask = True       # masking of noisy or black pixels during histogramming, only affecting fast-algorithm
-    
-    analyze_raw_data.create_meta_word_index = False     # stores the start and stop raw data word index for every event, std. setting is false
-    analyze_raw_data.create_meta_event_index = False    # stores the event number for each readout in an additional meta data array, default: False
-    
-    analyze_raw_data.interpreter.set_warning_output(True) # std. setting is True
-    analyze_raw_data.interpreter.debug_events(0,0,True) # events to be printed onto the console for debugging, usually deactivated
-    analyze_raw_data.interpret_word_table(FEI4B = True if(chip_flavor == 'fei4b') else False) # the actual start conversion command
-    analyze_raw_data.interpreter.print_summary()        # prints the interpreter summary
-    analyze_raw_data.plot_histograms(scan_data_filename = scan_data_filename) # plots all activated histograms into one pdf
-     
-with AnalyzeRawData(input_file = output_file_hits, output_file = output_file_cluster) as analyze_raw_data:
-    analyze_raw_data.create_cluster_hit_table = True    # adds the cluster id and seed info to each hit, std. setting is false
-    analyze_raw_data.create_cluster_table = True        # enables the creation of a table with all clusters, std. setting is false
-    analyze_raw_data.create_cluster_size_hist = True    # enables cluster size histogramming, can save some time, std. setting is false
-    analyze_raw_data.create_cluster_tot_hist = True     # enables cluster tot histogramming per cluster size, std. setting is false
-    analyze_raw_data.cluster_hit_table()                # the command to start the clustering from the hit information
-    analyze_raw_data.plot_histograms(scan_data_filename = scan_data_filename) # plots all activated histograms into one pdf
+    analyze_raw_data.create_error_hist = True  # creates a histogram summing up the event errors that occurred
+    analyze_raw_data.create_trigger_error_hist = True  # creates a histogram summing up the trigger errors
+    analyze_raw_data.create_cluster_size_hist = False  # enables cluster size histogramming, can save some time, std. setting is false
+    analyze_raw_data.create_cluster_tot_hist = False  # enables cluster tot histogramming per cluster size, std. setting is false
+    analyze_raw_data.create_threshold_hists = False  # makes only sense if threshold scan data is analyzed, std. setting is false
+    analyze_raw_data.create_fitted_threshold_hists = False
+    analyze_raw_data.create_threshold_mask = False  # masking of noisy or black pixels during histogramming, only affecting fast-algorithm
+  
+    analyze_raw_data.create_meta_word_index = False  # stores the start and stop raw data word index for every event, std. setting is false
+    analyze_raw_data.create_meta_event_index = True  # stores the event number for each readout in an additional meta data array, default: False
+   
+    analyze_raw_data.interpreter.set_warning_output(False)  # std. setting is True
+    analyze_raw_data.interpreter.debug_events(0, 0, False)  # events to be printed onto the console for debugging, usually deactivated
+    analyze_raw_data.interpret_word_table(FEI4B=True if(chip_flavor == 'fei4b') else False)  # the actual start conversion command
+    analyze_raw_data.interpreter.print_summary()  # prints the interpreter summary
+    analyze_raw_data.plot_histograms(scan_data_filename=scan_data_filename)  # plots all activated histograms into one pdf
 
-logging.info('Script runtime %.1f seconds' % (datetime.now()-start_time).total_seconds())
+logging.info('Script runtime %.1f seconds' % (datetime.now() - start_time).total_seconds())
+
+# start_time = datetime.now()
+# with AnalyzeRawData(raw_data_file=input_file, analyzed_data_file=output_file_hits) as analyze_raw_data:
+#     analyze_raw_data.create_cluster_hit_table = True    # adds the cluster id and seed info to each hit, std. setting is false
+#     analyze_raw_data.create_cluster_table = True        # enables the creation of a table with all clusters, std. setting is false
+#     analyze_raw_data.create_cluster_size_hist = True    # enables cluster size histogramming, can save some time, std. setting is false
+#     analyze_raw_data.create_cluster_tot_hist = True     # enables cluster tot histogramming per cluster size, std. setting is false
+#     analyze_raw_data.analyze_hit_table(analyzed_data_out_file='data/'+scan_name + "_reinterpreted.h5")  # the command to start the analysis of hit information
+#     analyze_raw_data.plot_histograms(scan_data_filename = scan_data_filename) # plots all activated histograms into one pdf
+# logging.info('Script runtime %.1f seconds' % (datetime.now() - start_time).total_seconds())
