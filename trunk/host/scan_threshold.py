6e466c4d616d0a3ff579bf439a1c26a9eb170251
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-11-22 13:15:07 +0000

    BUG: fix wrong parameter name

--- trunk/host/scan_threshold.py
+++ trunk/host/scan_threshold.py
@@ -11,7 +11,7 @@ class ThresholdScan(ScanBase):
     def __init__(self, config_file, definition_file=None, bit_file=None, device=None, scan_identifier="scan_threshold", scan_data_path=None):
         super(ThresholdScan, self).__init__(config_file=config_file, definition_file=definition_file, bit_file=bit_file, device=device, scan_identifier=scan_identifier, scan_data_path=scan_data_path)
 
-    def scan(self, mask=3, repeat=100, scan_parameter='PlsrDAC', scan_paramter_value=None):
+    def scan(self, mask=3, repeat=100, scan_parameter='PlsrDAC', scan_paramter_values=None):
         '''Scan loop
 
         Parameters
@@ -22,13 +22,13 @@ class ThresholdScan(ScanBase):
             Number of injections per scan step.
         scan_parameter : string
             Name of global register.
-        scan_paramter_value : list, tuple
+        scan_paramter_values : list, tuple
             Specify scan steps. These values will be written into global register scan_parameter.
         '''
-        if scan_paramter_value is None:
+        if scan_paramter_values is None:
             scan_paramter_value_list = range(0, 101, 1)  # default
         else:
-            scan_paramter_value_list = list(scan_paramter_value)
+            scan_paramter_value_list = list(scan_paramter_values)
 
         with open_raw_data_file(filename=self.scan_data_filename, title=self.scan_identifier, scan_parameters=[scan_parameter]) as raw_data_file:
 
