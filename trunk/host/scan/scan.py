82ec12ba05ef7bb6a900970f617702dd4c4aae1d
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-11-25 09:49:33 +0000

    ENH: added possibility to disable pixels by additional disable mask API: make parameters more meaningful

--- trunk/host/scan/scan.py
+++ trunk/host/scan/scan.py
@@ -210,23 +210,23 @@ class ScanBase(object):
                 f.write(value)
         self.lock.release()
 
-    def scan_loop(self, command, repeat=100, hardware_repeat=True, mask=3, mask_steps=None, double_columns=None, same_mask_for_all_dc=False, eol_function=None, digital_injection=False, enable_c_high=None, enable_c_low=None, shift_masks=["Enable", "C_High", "C_Low"], restore_shift_masks=True):
+    def scan_loop(self, command, repeat_command=100, hardware_repeat=True, mask_steps=3, enable_mask_steps=None, enable_double_columns=None, same_mask_for_all_dc=False, eol_function=None, digital_injection=False, enable_c_high=None, enable_c_low=None, shift_masks=["Enable", "C_High", "C_Low"], restore_shift_masks=True, mask=None):
         '''Implementation of the scan loops (mask shifting, loop over double columns, repeatedly sending any arbitrary command).
 
         Parameters
         ----------
         command : BitVector
             (FEI4) command that will be sent out serially.
-        repeat : int
-            Number of injections.
+        repeat_command : int
+            The number of repetitions command will be sent out each mask step.
         hardware_repeat : bool
             If true, use FPGA to repeat commands. In general this is much faster than doing this in software.
-        mask : int
+        mask_steps : int
             Number of mask steps.
-        mask_steps : list, tuple
-            List of mask steps which will be applied. Default is all mask steps. From 0 to (mask-1).
-        double_columns : list, tuple
-            List of double columns which will be enabled during scan. Default is all double columns. From 0 to 39 (double columns counted from zero).
+        enable_mask_steps : list, tuple
+            List of mask steps which will be applied. Default is all mask steps. From 0 to (mask-1). A value equal None or empty list will select all mask steps.
+        enable_double_columns : list, tuple
+            List of double columns which will be enabled during scan. Default is all double columns. From 0 to 39 (double columns counted from zero). A value equal None or empty list will select all double columns.
         same_mask_for_all_dc : bool
             Use same mask for all double columns. Enabling this is in general not a good idea since all double columns will have the same configuration but the scan speed can increased by an order of magnitude.
         eol_function : function
@@ -234,13 +234,15 @@ class ScanBase(object):
         digital_injection : bool
             Enables digital injection.
         enable_c_high : bool
-            Enables C_High pixel mask. Will be overwritten by shift_mask.
+            Enables C_High pixel mask. No change if value is equal None. Note: will be overwritten during mask shifting if in shift_masks.
         enable_c_low : bool
-            Enables C_Low pixel mask. Will be overwritten by shift_mask.
+            Enables C_Low pixel mask. No change if value is equal None. Note: will be overwritten during mask shifting if in shift_masks.
         shift_masks : list, tuple
             List of pixel masks that will be shifted.
         restore_shift_masks : bool
             Writing the initial (restored) FE pixel configuration into FE after finishing the scan loop.
+        mask : array-like
+            Additional mask. Must be convertible to an array of booleans with the same shape as mask array. True indicates a masked (i.e. invalid) data. Masked pixels will be selected (enabled) during mask shifting.
         '''
         if not isinstance(command, BitVector.BitVector):
             raise TypeError
@@ -253,11 +255,11 @@ class ScanBase(object):
         conf_mode_command = self.register.get_commands("confmode")
         run_mode_command = self.register.get_commands("runmode")
 
-        if mask_steps == None or mask_steps == []:
-            mask_steps = range(mask)
+        if enable_mask_steps == None or not enable_mask_steps:
+            enable_mask_steps = range(mask_steps)
 
-        if double_columns == None or double_columns == []:
-            double_columns = range(40)
+        if enable_double_columns == None or not enable_double_columns:
+            enable_double_columns = range(40)
 
         # preparing for scan
         commands = []
@@ -269,35 +271,35 @@ class ScanBase(object):
             self.register.set_global_register_value("DIGHITIN_SEL", 0)
             self.register.set_pixel_register_value("EnableDigInj", 0)
         commands.extend(self.register.get_commands("wrregister", name=["DIGHITIN_SEL"]))
-        if(enable_c_high != None):
+        if enable_c_high is not None:
             self.register.set_pixel_register_value("C_High", 1 if enable_c_high else 0)
             commands.extend(self.register.get_commands("wrfrontend", same_mask_for_all_dc=True, name=["C_High"]))
-        if(enable_c_low != None):
+        if enable_c_low is not None:
             self.register.set_pixel_register_value("C_Low", 1 if enable_c_low else 0)
             commands.extend(self.register.get_commands("wrfrontend", same_mask_for_all_dc=True, name=["C_Low"]))
         self.register_utils.send_commands(commands)
 
-        for mask_step in mask_steps:
+        for mask_step in enable_mask_steps:
             commands = []
             commands.extend(conf_mode_command)
-            mask_array = self.register_utils.make_pixel_mask(mask=mask, row_offset=mask_step)
-            #plt.imshow(np.transpose(mask_array), interpolation='nearest', aspect="auto")
-            #plt.pcolor(np.transpose(mask_array))
+            curr_mask = self.register_utils.make_pixel_mask(steps=mask_steps, shift=mask_step, mask=mask)
+            #plt.imshow(np.transpose(curr_mask), interpolation='nearest', aspect="auto")
+            #plt.pcolor(np.transpose(curr_mask))
             #plt.colorbar()
             #plt.savefig('mask_step'+str(mask_step)+'.eps')
-            map(lambda mask: self.register.set_pixel_register_value(mask, mask_array), [shift_mask for shift_mask in shift_masks if (shift_mask.lower() != "EnableDigInj".lower())])
+            map(lambda mask_name: self.register.set_pixel_register_value(mask_name, curr_mask), [shift_mask_name for shift_mask_name in shift_masks if (shift_mask_name.lower() != "EnableDigInj".lower())])
             commands.extend(self.register.get_commands("wrfrontend", same_mask_for_all_dc=same_mask_for_all_dc, name=shift_masks))
             if digital_injection == True:  # TODO: write EnableDigInj to FE or do it manually?
-                self.register.set_pixel_register_value("EnableDigInj", mask_array)
+                self.register.set_pixel_register_value("EnableDigInj", curr_mask)
                 commands.extend(self.register.get_commands("wrfrontend", same_mask_for_all_dc=same_mask_for_all_dc, name=["EnableDigInj"]))
                 self.register.set_global_register_value("DIGHITIN_SEL", 1)
                 commands.extend(self.register.get_commands("wrregister", name=["DIGHITIN_SEL"]))  # write DIGHITIN_SEL mask last
 #             else:
 #                 commands.extend(self.register.get_commands("wrfrontend", same_mask_for_all_dc=True, name=["EnableDigInj"]))
             self.register_utils.send_commands(commands)
-            logging.info('%d injection(s): mask step %d' % (repeat, mask_step))
+            logging.info('%d injection(s): mask step %d' % (repeat_command, mask_step))
 
-            for dc in double_columns:
+            for dc in enable_double_columns:
                 commands = []
                 #commands.extend(conf_mode_command)
                 self.register.set_global_register_value("Colpr_Addr", dc)
@@ -308,12 +310,12 @@ class ScanBase(object):
                 #commands.extend(run_mode_command)
                 self.register_utils.send_commands(commands)
 
-                #print repeat, 'injections:', 'mask step', mask_step, 'dc', dc
+                #print repeat_command, 'injections:', 'mask step', mask_step, 'dc', dc
                 bit_length = self.register_utils.set_command(command)
                 if hardware_repeat == True:
-                    self.register_utils.send_command(repeat=repeat, wait_for_cmd=True, command_bit_length=bit_length)
+                    self.register_utils.send_command(repeat=repeat_command, wait_for_cmd=True, command_bit_length=bit_length)
                 else:
-                    for _ in range(repeat):
+                    for _ in range(repeat_command):
                         self.register_utils.send_command()
                 try:
                     eol_function()
