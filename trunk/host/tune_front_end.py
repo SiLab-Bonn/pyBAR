e29fa420dd75df3f850583b8c8b50139ccc757c5
davidlp <davidlp@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-11-29 15:22:06 +0000

    BUG: register objects are now used correctly for all scans ENH: one can turn off global/local tuning

--- trunk/host/tune_front_end.py
+++ trunk/host/tune_front_end.py
@@ -17,8 +17,8 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(leve
 target_threshold = 50  # in PlsrDAC
 target_charge = 270  # in PlsrDAC
 target_tot = 5
-global_iterations = 1  # set 1..5, 0 is global threshold tuning only
-local_iterations = 1  # set 1..5, 0 is local threshold tuning only
+global_iterations = 1  # set -1..5, 0 is global threshold tuning only, -1 disables global tuning
+local_iterations = 1  # set -1..5, 0 is local threshold tuning only, -1 disables local tuning
 cfg_name = "tuning"
 
 if __name__ == "__main__":
@@ -27,35 +27,33 @@ if __name__ == "__main__":
     gdac_tune_scan = GdacTune(config_file=configuration.config_file, bit_file=configuration.bit_file, scan_data_path=configuration.scan_data_path)
     gdac_tune_scan.set_target_threshold(target_threshold)
 
-    feedback_tune_scan = FeedbackTune(config_file=configuration.config_file, bit_file=None, scan_data_path=configuration.scan_data_path, device=gdac_tune_scan.device)
-    feedback_tune_scan.register = gdac_tune_scan.register
+    feedback_tune_scan = FeedbackTune(config_file=gdac_tune_scan.register, bit_file=None, scan_data_path=configuration.scan_data_path, device=gdac_tune_scan.device)
     feedback_tune_scan.set_target_charge(target_charge)
     feedback_tune_scan.set_target_tot(target_tot)
 
-    tdac_tune_scan = TdacTune(config_file=configuration.config_file, bit_file=None, scan_data_path=configuration.scan_data_path, device=gdac_tune_scan.device)
-    tdac_tune_scan.register = gdac_tune_scan.register
+    tdac_tune_scan = TdacTune(config_file=gdac_tune_scan.register, bit_file=None, scan_data_path=configuration.scan_data_path, device=gdac_tune_scan.device)
     tdac_tune_scan.set_target_threshold(target_threshold)
     tdac_tune_scan.set_start_tdac()  # set TDAC = 0
-    tdac_tune_scan.set_tdac_bit(bit_position=4, bit_value=1)  # set start value TDAC = 15
+    tdac_tune_scan.set_tdac_bit(bit_position=4, bit_value=1)  # set start value TDAC = 16
 
-    fdac_tune_scan = FdacTune(config_file=configuration.config_file, bit_file=None, scan_data_path=configuration.scan_data_path, device=gdac_tune_scan.device)
-    fdac_tune_scan.register = gdac_tune_scan.register
+    fdac_tune_scan = FdacTune(config_file=gdac_tune_scan.register, bit_file=None, scan_data_path=configuration.scan_data_path, device=gdac_tune_scan.device)
     fdac_tune_scan.set_target_charge(target_charge)
     fdac_tune_scan.set_target_tot(target_tot)
     fdac_tune_scan.set_start_fdac()  # set FDAC = 0
-    fdac_tune_scan.set_fdac_bit(bit_position=3, bit_value=1)  # set start value FDAC = 7
+    fdac_tune_scan.set_fdac_bit(bit_position=3, bit_value=1)  # set start value FDAC = 8
+
+    analog_scan = AnalogScan(config_file=gdac_tune_scan.register, bit_file=None, scan_data_path=configuration.scan_data_path, device=gdac_tune_scan.device)
 
     difference_bit = int(8 / (global_iterations if global_iterations > 0 else 1))
-#     print difference_bit
 
     output_pdf_filename = configuration.scan_data_path + "/" + cfg_name
-
     output_pdf = PdfPages(output_pdf_filename)
 
     PrmpVbpf = 0
     Vthin_AC = 0
     Vthin_AF = 0
 
+    start_bit = 7
     for iteration in range(0, global_iterations):  # tune iteratively with decreasing range to save time
         start_bit = 7 - difference_bit * iteration
         logging.info("Global tuning iteration %d" % iteration)
@@ -66,21 +64,22 @@ if __name__ == "__main__":
         feedback_tune_scan.start(configure=True, plots_filename=output_pdf)
         feedback_tune_scan.stop()
 
+    if global_iterations >= 0:
+        gdac_tune_scan.set_gdac_tune_bits(range(start_bit, -1, -1))  # needed to reset the last extra bit 0 = 0 test
+        gdac_tune_scan.start(configure=True, plots_filename=output_pdf)
+        gdac_tune_scan.stop()
+
     Vthin_AC = gdac_tune_scan.register.get_global_register_value("Vthin_AltCoarse")
     Vthin_AF = gdac_tune_scan.register.get_global_register_value("Vthin_AltFine")
     PrmpVbpf = feedback_tune_scan.register.get_global_register_value("PrmpVbpf")
-
-    gdac_tune_scan.start(configure=True, plots_filename=output_pdf)
-    gdac_tune_scan.stop()
-    Vthin_AC = gdac_tune_scan.register.get_global_register_value("Vthin_AltCoarse")
-    Vthin_AF = gdac_tune_scan.register.get_global_register_value("Vthin_AltFine")
     logging.info("Results of global tuning PrmpVbpf/Vthin_AltCoarse,Vthin_AltFine = %d/%d,%d" % (PrmpVbpf, Vthin_AC, Vthin_AF))
 
     difference_bit = int(5 / (local_iterations if local_iterations > 0 else 1))
 
+    start_bit = 4
     for iteration in range(0, local_iterations):  # tune iteratively
-        start_bit = 4  # -difference_bit*iteration
         logging.info("Lokal tuning iteration %d" % iteration)
+        start_bit = 4  # -difference_bit*iteration
         tdac_tune_scan.set_tdac_tune_bits(range(start_bit, -1, -1))
         fdac_tune_scan.set_fdac_tune_bits(range(start_bit - 1, -1, -1))
         tdac_tune_scan.start(configure=True, plots_filename=output_pdf)
@@ -88,18 +87,20 @@ if __name__ == "__main__":
         fdac_tune_scan.start(configure=True, plots_filename=output_pdf)
         fdac_tune_scan.stop()
 
-    tdac_tune_scan.start(configure=True, plots_filename=output_pdf)
-    tdac_tune_scan.stop()
+    if local_iterations >= 0:
+        tdac_tune_scan.set_tdac_tune_bits(range(start_bit, -1, -1))  # needed to reset the last extra bit 0 = 0 test
+        tdac_tune_scan.start(configure=True, plots_filename=output_pdf)
+        tdac_tune_scan.stop()
 
-    gdac_tune_scan.register.save_configuration(name=cfg_name)
+    gdac_tune_scan.register.save_configuration(name=cfg_name)  # save the final config
 
     if(local_iterations > 0):
-        plotThreeWay(hist=fdac_tune_scan.register.get_pixel_register_value("FDAC").transpose(), title="FDAC distribution after last FDAC tuning", x_axis_title='FDAC', filename=output_pdf)
+        plotThreeWay(hist=fdac_tune_scan.register.get_pixel_register_value("FDAC").transpose(), title="FDAC distribution after last FDAC tuning", x_axis_title='FDAC', filename=output_pdf, maximum=16)
         plotThreeWay(hist=fdac_tune_scan.result.transpose(), title="TOT mean after last FDAC tuning", x_axis_title='mean TOT', filename=output_pdf)
-
+ 
     if(global_iterations > 0):
-        plotThreeWay(hist=tdac_tune_scan.register.get_pixel_register_value("TDAC").transpose(), title="TDAC distribution after complete tuning", x_axis_title='TDAC', filename=output_pdf)
-        plotThreeWay(hist=tdac_tune_scan.result.transpose(), title="Occupancy after complete tuning", x_axis_title='Occupancy', filename=output_pdf)
+        plotThreeWay(hist=tdac_tune_scan.register.get_pixel_register_value("TDAC").transpose(), title="TDAC distribution after complete tuning", x_axis_title='TDAC', filename=output_pdf, maximum=32)
+        plotThreeWay(hist=tdac_tune_scan.result.transpose(), title="Occupancy after complete tuning", x_axis_title='Occupancy', filename=output_pdf, maximum=100)
 
     output_pdf.close()
     logging.info("Tuning finished in " + str(datetime.now() - startTime))
