eff9d6d384f64b2aa257f44d0d4df0aae476b51a
davidlp <davidlp@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-12-05 09:43:12 +0000

    MAINT: change the data_struct imports

--- trunk/host/analysis/analyze_raw_data.py
+++ trunk/host/analysis/analyze_raw_data.py
@@ -11,7 +11,7 @@ from functools import partial
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
 
-import data_struct
+from RawDataConverter import data_struct
 from plotting import plotting
 from matplotlib.backends.backend_pdf import PdfPages
 from RawDataConverter.data_interpreter import PyDataInterpreter
