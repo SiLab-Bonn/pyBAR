e7f8f4083506fb26150245cb853fc1ab0f72aa7f
davidlp <davidlp@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-12-06 13:40:32 +0000

    ENH: new function to get the scan parameters from the meta_data table

--- trunk/host/analysis/analysis_utils.py
+++ trunk/host/analysis/analysis_utils.py
@@ -5,7 +5,10 @@ import logging
 import pandas as pd
 import numpy as np
 import tables as tb
+from scipy.sparse import coo_matrix
 from datetime import datetime
+
+from analysis.RawDataConverter.data_histograming import PyDataHistograming
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
 
@@ -179,13 +182,72 @@ class AnalysisUtils(object):
         tot_avr = np.divide(tot_avr, occupancy)
         return np.ma.array(tot_avr, mask=(occupancy == 0))  # return array with masked pixel without any hit
 
-    def histogram_occupancy_per_pixel(self, array, labels=['column', 'row'], mask_no_hit=False):
-        occupancy = np.histogram2d(x=array[labels[0]], y=array[labels[1]], bins=(80, 336), range=[[0, 80], [0, 336]])
+    def histogram_occupancy_per_pixel(self, array, labels=['column', 'row'], mask_no_hit=False, fast=False):
+        if fast:
+            occupancy = self.fast_histogram2d(x=array[labels[0]], y=array[labels[1]], bins=(80, 336))
+        else:
+            occupancy = np.histogram2d(x=array[labels[0]], y=array[labels[1]], bins=(80, 336), range=[[0, 80], [0, 336]])
         if mask_no_hit:
             return np.ma.array(occupancy[0], mask=(occupancy[0] == 0)), occupancy[1], occupancy[2]
         else:
             return occupancy
 
+#     def fast_occupancy_histograming(self, hits): TODO bring this to work
+#         hit_histograming = PyDataHistograming()
+#         hit_histograming.set_info_output(True)
+#         hit_histograming.set_debug_output(True)
+#         hit_histograming.create_occupancy_hist(True)
+# 
+#         hit_histograming.add_hits(hits, hits.shape[0])
+#         occupancy_hist = np.zeros(80 * 336 * hit_histograming.get_n_parameters(), dtype=np.uint32)  # create linear array as it is created in histogram class
+#         hit_histograming.get_occupancy(occupancy_hist)
+#         occupancy_hist = np.reshape(a=self.occupancy.view(), newshape=(80, 336, hit_histograming.get_n_parameters()), order='F')  # make linear array to 3d array (col,row,parameter)
+#         occupancy_hist = np.swapaxes(occupancy_hist, 0, 1)
+#         return occupancy_hist
+
+    def fast_histogram2d(self, x, y, bins):
+        '''WARNING: GIVES NOT EXACT RESULTS'''
+        nx = bins[0]-1
+        ny = bins[1]-1
+
+        print nx, ny
+
+        xmin, xmax = x.min(), x.max()
+        ymin, ymax = y.min(), y.max()
+        dx = (xmax - xmin) / (nx - 1.0)
+        dy = (ymax - ymin) / (ny - 1.0)
+
+        weights = np.ones(x.size)
+
+        # Basically, this is just doing what np.digitize does with one less copy
+        xyi = np.vstack((x,y)).T
+        xyi -= [xmin, ymin]
+        xyi /= [dx, dy]
+        xyi = np.floor(xyi, xyi).T
+
+        # Now, we'll exploit a sparse coo_matrix to build the 2D histogram...
+        grid = coo_matrix((weights, xyi), shape=(nx, ny)).toarray()
+
+        return grid, np.linspace(xmin, xmax, nx), np.linspace(ymin, ymax, ny)
+
+    def get_scan_parameter(self, meta_data_array):
+        '''Takes the numpy meta data array and returns the different scan parameter settings and the name aligned in a dictionary
+
+        Parameters
+        ----------
+        meta_data_array : numpy.ndarray
+
+        Returns
+        -------
+        python.dict{string, numpy.Histogram}
+        '''
+
+        if len(meta_data_array.dtype.names) < 5:  # no meta_data found
+            return
+        scan_parameters = {}
+        for scan_par_name in meta_data_array.dtype.names[len(meta_data_array.dtype.names) - 1:len(meta_data_array.dtype.names)]:
+            scan_parameters[scan_par_name] = np.unique(meta_data_array[scan_par_name])
+        return scan_parameters
 
 if __name__ == "__main__":
     pass
