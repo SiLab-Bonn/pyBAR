1f91eac5f18b043ee858b927882e0e4be852f999
Jens Janssen <janssen@physik.uni-bonn.de>
2013-12-08 13:45:28 +0000

    ENH: disable additional occ plot, since it is identical to the occ plot inside result pdf

--- host/scan_analog.py
+++ host/scan_analog.py
@@ -45,7 +45,7 @@ class AnalogScan(ScanBase):
         self.readout.stop(timeout=10.0)
 
         # plotting data
-        plot_occupancy(hist=make_occupancy_hist(*convert_data_array(data_array_from_data_dict_iterable(self.readout.data), filter_func=is_data_record, converter_func=get_col_row_array_from_data_record_array)), z_max='median', filename=self.scan_data_filename + "_occupancy.pdf")
+#         plot_occupancy(hist=make_occupancy_hist(*convert_data_array(data_array_from_data_dict_iterable(self.readout.data), filter_func=is_data_record, converter_func=get_col_row_array_from_data_record_array)), z_max='median', filename=self.scan_data_filename + "_occupancy.pdf")
 
         # saving data
         save_raw_data_from_data_dict_iterable(self.readout.data, filename=self.scan_data_filename, title=self.scan_identifier)
