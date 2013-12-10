71c6483ab9d8c5bd0a4b85dbeab24a55963c9871
DavidLP <pohl@physik.uni-bonn.de>
2013-12-10 13:16:49 +0000

    ENH: clean up ENH: calibration is now taken from array not from table (3 orders faster) ENH: interpolation is done in parallel (vectorized) for all chosen pixels, much faster...

--- host/analyze_source_scan_gdac_data.py
+++ host/analyze_source_scan_gdac_data.py
@@ -1,83 +1,92 @@
 import numpy as np
 import tables as tb
 from scipy.interpolate import interp1d
-import matplotlib.pyplot as plt
-from analysis.plotting.plotting import plot_scurves, plotThreeWay
 
-from scipy.interpolate import interp1d
+from analysis.plotting.plotting import plot_profile_histogram, plot_scatter
+from analysis import analysis_utils
 
 import logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
 
 
-def central_difference(x, y):  
-    if (len(x) != len(y)):
-        raise ValueError("x, y must have the same length")
-    z1 = np.hstack((y[0], y[:-1]))
-    z2 = np.hstack((y[1:], y[-1]))
-    dx1 = np.hstack((0, np.diff(x)))
-    dx2 = np.hstack((np.diff(x), 0))
-    return (z2 - z1) / (dx2 + dx1)
-
-
 def get_mean_threshold(gdac, mean_threshold_calibration):
-    interpolation = interp1d(mean_threshold_calibration['gdac'], mean_threshold_calibration['mean_threshold'], kind='slinear')
+    interpolation = interp1d(mean_threshold_calibration['gdac'], mean_threshold_calibration['mean_threshold'], kind='slinear', bounds_error=True)
     return interpolation(gdac)
 
 
-def get_pixel_threshold(column, row, gdac, threshold_calibration):
-    pixel_gdacs = threshold_calibration[np.logical_and(threshold_calibration['column'] == column, threshold_calibration['row'] == row)]['gdac']
-    pixel_thresholds = threshold_calibration[np.logical_and(threshold_calibration['column'] == column, threshold_calibration['row'] == row)]['threshold']
+def get_pixel_thresholds_from_table(column, row, gdacs, threshold_calibration_table):
+    pixel_gdacs = threshold_calibration_table[np.logical_and(threshold_calibration_table['column'] == column, threshold_calibration_table['row'] == row)]['gdac']
+    pixel_thresholds = threshold_calibration_table[np.logical_and(threshold_calibration_table['column'] == column, threshold_calibration_table['row'] == row)]['threshold']
 #     print pixel_gdacs, pixel_thresholds
     interpolation = interp1d(x=pixel_gdacs, y=pixel_thresholds, kind='slinear', bounds_error=True)
-    return interpolation(gdac)
+    return interpolation(gdacs)
+
+
+def get_pixel_thresholds(gdacs, pixel_gdacs, threshold_calibration_array):
+    '''Calculates the threshold for all pixels in threshold_calibration_array at the given GDAC settings via linear interpolation. The GDAC settings used during calibration have to be given.
+
+    Parameters
+    ----------
+    gdacs : array like
+        The GDAC settings where the threshold should be determined from the calibration
+    pixel_gdacs : array like
+        GDAC settings used during calibration, needed to translate the index of the calibration array to a value. 
+    threshold_calibration_array : numpy.array, shape=(80,336,# of GDACs during calibration)
+        The calibration array
+
+    Returns
+    -------
+    numpy.array, shape=(80,336,# of GDACs during calibration)
+        The threshold values for each pixel at gdacs.
+    '''
+    if len(pixel_gdacs) != threshold_calibration_array.shape[2]:
+        raise ValueError('Length of the provided pixel GDACs does not match the third dimension of the calibration array')
+    interpolation = interp1d(x=pixel_gdacs, y=threshold_calibration_array, kind='slinear', bounds_error=True)
+    return interpolation(gdacs)
+
+#     plt.plot(bin_centers, central_difference(bin_centers, polynom_fit(bin_centers)), 'r-')
+#     plt.show()
+
 
 if __name__ == "__main__":
     scan_name = 'scan_fei4_trigger_141'
     chip_flavor = 'fei4a'
     input_file_hits = 'data/' + scan_name + "_interpreted.h5"
     input_file_calibration = 'data/calibrate_threshold_gdac.h5'
-    scan_parameters = range(100, 5001, 15)
-    pixel_row = 200
-    pixel_column = 35
+    gdac_range = range(100, 114, 1)  # the GDAC range used during the calibration
+    gdac_range.extend((np.exp(np.array(range(0, 150)) / 10.) / 10. + 100).astype('<u8')[50:-40].tolist())  # exponential GDAC range to correct for logarithmic threshold(GDAC) function
 
     with tb.openFile(input_file_calibration, mode="r") as in_file_calibration_h5:  # read calibration file from calibrate_threshold_gdac scan
         with tb.openFile(input_file_hits, mode="r") as in_file_hits_h5:  # read scan data file from scan_fei4_trigger_gdac scan
             hits = in_file_hits_h5.root.HistOcc[:]
             mean_threshold_calibration = in_file_calibration_h5.root.MeanThresholdCalibration[:]
-            threshold_calibration = in_file_calibration_h5.root.ThresholdCalibration[:]
-    
-    #         print get_pixel_threshold(pixel_column, pixel_row, scan_parameters, threshold_calibration=threshold_calibration)
-        #     print hits[200:201, 35:36, :]
-        #     print np.gradient(hits[200, 35, :], 15)
-    
-    
-            x = get_mean_threshold(scan_parameters, mean_threshold_calibration)
-            y = hits[190:220, 32:37, :]
-    # 
-    # #         plot_scurves(y, max_occ = 300, scan_paramter_name='GDAC', scan_parameters=scan_parameters)
-#             plot_scurves(y, max_occ = 300, scan_paramter_name='Threshold [e]', scan_parameters=x * 55)
-    # #         plt.close()
-    #         
-    # #         y = 
-    # #         
-    #         print x,y
-    
-    #         pixel_gdacs = threshold_calibration[np.logical_and(threshold_calibration['column'] == pixel_column, threshold_calibration['row'] == pixel_row)]['gdac']
-    #         pixel_thresholds = threshold_calibration[np.logical_and(threshold_calibration['column'] == pixel_column, threshold_calibration['row'] == pixel_row)]['threshold']
-    
-            x = get_pixel_threshold(pixel_column, pixel_row, scan_parameters, threshold_calibration=threshold_calibration)
-#             x_2 = get_mean_threshold(scan_parameters, mean_threshold_calibration=mean_threshold_calibration)
-            y = hits[pixel_row, pixel_column, :]
-            
-#             x = np.arange(0,3.1*3,0.1)
-#             y = np.sin(x)
-    #         x = x/30.
-    #         plt.plot(x, np.sin(x), x, central_difference(x, np.sin(x)))
-        
-            plt.plot(x * 55, y, 'o')
-    #         plt.plot(x, y, 'o', x, central_difference(x, y))
-    #         plt.plot(scan_parameters, y, 'o', scan_parameters, )
-            plt.show()
+            threshold_calibration_table = in_file_calibration_h5.root.ThresholdCalibration[:]
+            threshold_calibration_array = in_file_calibration_h5.root.HistThresholdCalibration[:]
 
+            gdac_range_calibration = gdac_range
+            gdac_range_source_scan = analysis_utils.get_scan_parameter(meta_data_array=in_file_hits_h5.root.meta_data[:])['GDAC']
 
+            logging.info('Analyzing source scan data with %d different GDAC settings from %d to %d with minimum step sizes from %d to %d' % (len(gdac_range_source_scan), np.min(gdac_range_source_scan), np.max(gdac_range_source_scan), np.min(np.gradient(gdac_range_source_scan)), np.max(np.gradient(gdac_range_source_scan))))
+            logging.info('Use calibration data with %d different GDAC settings from %d to %d with minimum step sizes from %d to %d' % (len(gdac_range_calibration), np.min(gdac_range_calibration), np.max(gdac_range_calibration), np.min(np.gradient(gdac_range_calibration)), np.max(np.gradient(gdac_range_calibration))))
+
+            pixel_thresholds = get_pixel_thresholds(gdacs=gdac_range_source_scan, pixel_gdacs=gdac_range_calibration, threshold_calibration_array=threshold_calibration_array)  # interpolates the threshold at the source scan GDAC setting from the calibration
+            pixel_hits = np.swapaxes(hits, 0, 1)  # create hit array with shape (col, row, ...)
+
+            # choose good region
+            selected_pixel_thresholds = pixel_thresholds[33:37, 190:215, :]
+            selected_pixel_hits = pixel_hits[33:37, 190:215, :]
+
+            # reshape to one dimension
+            x = np.reshape(selected_pixel_thresholds, newshape=(selected_pixel_thresholds.shape[0] * selected_pixel_thresholds.shape[1], selected_pixel_thresholds.shape[2])).ravel()
+            y = np.reshape(selected_pixel_hits, newshape=(selected_pixel_hits.shape[0] * selected_pixel_hits.shape[1], selected_pixel_hits.shape[2])).ravel()
+
+            #nothing should be NAN, NAN is not supported yet
+            if np.isnan(x).sum() > 0 or np.isnan(y).sum() > 0:
+                logging.warning('There are pixels with NaN threshold or hit values, analysis will be wrong')
+
+            plot_profile_histogram(x=x * 55., y=y, n_bins=len(gdac_range_source_scan) / 2, title='Mean pixel hit rate for different pixel thresholds', x_label='pixel threshold [e]', y_label='mean hit rate')
+
+            x = get_mean_threshold(gdac_range_source_scan, mean_threshold_calibration)
+            y = selected_pixel_hits.mean(axis=(0, 1))
+
+            plot_scatter(x * 55, y, title='Mean hit rate at different thresholds', x_label='mean threshold [e]', y_label='mean hit rate')
