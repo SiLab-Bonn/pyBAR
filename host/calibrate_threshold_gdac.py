b7b4c00c91407b8d878534c9f692c8cf248ab78a
Jens Janssen <janssen@physik.uni-bonn.de>
2013-12-13 06:49:43 +0000

    BUG: fix parameter name ENH: store scan data filenames in dict, no ambiguities

--- host/calibrate_threshold_gdac.py
+++ host/calibrate_threshold_gdac.py
@@ -71,7 +71,7 @@ def store_calibration_data_as_array(out_file_h5, mean_threshold_calibration, mea
     logging.info("done")
 
 
-def create_calibration(scan_identifier, is_FEI4B=False, create_plots=True):
+def create_calibration(scan_identifier, scan_data_filenames, is_FEI4B=False, create_plots=True):
     logging.info("Analyzing and plotting results...")
     output_h5_filename = 'data/' + scan_identifier + '.h5'
     logging.info('Saving calibration in: %s' % output_h5_filename)
@@ -87,7 +87,8 @@ def create_calibration(scan_identifier, is_FEI4B=False, create_plots=True):
 
     for gdac_index, gdac in enumerate(gdac_range):
         logging.info("Analyzing GDAC %d, progress %d %%" % (gdac, int(float(float(gdac_index) / float(len(gdac_range)) * 100.))))
-        raw_data_file = 'data/' + scan_identifier + '_' + str(gdac) + "_0"
+        #raw_data_file = 'data/' + scan_identifier + '_' + str(gdac) + "_0"
+        raw_data_file = scan_data_filenames[gdac]
         analyzed_data_file = raw_data_file + '_interpreted.h5'
         analyze(raw_data_file=raw_data_file, analyzed_data_file=analyzed_data_file, FEI4B=is_FEI4B)
         with tb.openFile(analyzed_data_file, mode="r") as in_file_h5:
@@ -101,7 +102,7 @@ def create_calibration(scan_identifier, is_FEI4B=False, create_plots=True):
             parameter_settings = analysis_utils.get_scan_parameter(meta_data_array=meta_data_array)
             scan_parameters = parameter_settings['PlsrDAC']
             if create_plots:
-                plot_scurves(occupancy_hist=occupancy_masked, scan_parameters=scan_parameters, scan_paramter_name='PlsrDAC', filename=output_pdf)
+                plot_scurves(occupancy_hist=occupancy_masked, scan_parameters=scan_parameters, scan_parameter_name='PlsrDAC', filename=output_pdf)
             # fill the calibration data arrays
             mean_threshold_calibration[gdac_index] = np.ma.mean(thresholds_masked)
 #             mean_threshold_rms_calibration[gdac_index] = np.ma.sqrt(np.ma.mean((thresholds_masked - mean_threshold_calibration[gdac_index]) ** 2))  # TODO: use numpy.ma.std here?
@@ -141,19 +142,21 @@ def set_gdac(value, register, register_utils):
 
 if __name__ == "__main__":
     scan_identifier = "calibrate_threshold_gdac"
-    
+
     startTime = datetime.now()
-    logging.info('Taking threshold at following GDACs: %s' % str(gdac_range))
+    logging.info('Taking threshold data at following GDACs: %s' % str(gdac_range))
+    scan_data_filenames = {}
     scan_threshold_fast = ThresholdScanFast(config_file=configuration.config_file, bit_file=configuration.bit_file, scan_data_path=configuration.scan_data_path)
     for i, gdac in enumerate(gdac_range):
         set_gdac(gdac, scan_threshold_fast.register, scan_threshold_fast.register_utils)
         scan_threshold_fast.scan_identifier = scan_identifier + '_' + str(gdac)
         scan_threshold_fast.start(configure=True, scan_parameter_range=(scan_threshold_fast.scan_parameter_start, 800), scan_parameter_stepsize=2, search_distance=10, minimum_data_points=10, ignore_columns=ignore_columns)
         scan_threshold_fast.stop()
+        scan_data_filenames[gdac] = scan_threshold_fast.scan_data_filename
 
     logging.info("Calibration finished in " + str(datetime.now() - startTime))
 
     # analyze and plot the data from all scans
-    create_calibration(scan_identifier, is_FEI4B=scan_threshold_fast.register.fei4b, create_plots=True)
+    create_calibration(scan_identifier, scan_data_filenames=scan_data_filenames, is_FEI4B=scan_threshold_fast.register.fei4b, create_plots=True)
 
     logging.info("Finished!")
