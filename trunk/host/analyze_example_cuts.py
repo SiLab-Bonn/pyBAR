fadebbca2ab114d8acaea484dafc659df845fb44
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-12-16 17:46:31 +0000

    MAINT: test

--- trunk/host/analyze_example_cuts.py
+++ trunk/host/analyze_example_cuts.py
@@ -15,12 +15,12 @@ import logging
 from analysis.RawDataConverter import data_struct
 from analysis.analysis_utils import get_events_with_cluster_size, get_events_with_n_cluster, write_hits_in_events
 
-chunk_size = 100000000
+chunk_size = 10000000
 
 
 def select_hits():
     n_cluster_condition = 'n_cluster==1'
-    cluster_size_condition = 'cluster_size>=0'
+    cluster_size_condition = 'cluster_size==1'
     with tb.openFile(input_file_hits, mode="r") as in_hit_file_h5:
         with tb.openFile(output_file_hits, mode="w") as out_hit_file_h5:
             hit_table_out = out_hit_file_h5.createTable(out_hit_file_h5.root, name='Hits', description=data_struct.HitInfoTable, title='hit_data', filters=tb.Filters(complib='blosc', complevel=5, fletcher32=False))
@@ -42,11 +42,11 @@ def select_hits():
                     last_event_start_index = n_cluster
 
                 selected_events_1 = get_events_with_cluster_size(event_number=cluster['event_number'], cluster_size=cluster['size'], condition=cluster_size_condition)  # select the events with clusters of a certain size
-                selected_events_2 = get_events_with_n_cluster(event_number=cluster['event_number'], condition=n_cluster_condition)  # select the events with a certain cluster number
-                selected_events = selected_events_1[np.in1d(selected_events_1, selected_events_2, assume_unique=True)]  # select events with both conditions above
-                logging.info('Selected ' + str(len(selected_events)) + ' events with ' + n_cluster_condition + ' and ' + cluster_size_condition)
-                write_hits_in_events(hit_table_in=in_hit_file_h5.root.Hits, hit_table_out=hit_table_out, events=selected_events)  # write the hits of the selected events into a new table
-                logging.info('%d %%' % int(float(float(iCluster) / float(table_size) * 100.)))
+#                 selected_events_2 = get_events_with_n_cluster(event_number=cluster['event_number'], condition=n_cluster_condition)  # select the events with a certain cluster number
+#                 selected_events = selected_events_1[np.in1d(selected_events_1, selected_events_2, assume_unique=True)]  # select events with both conditions above
+#                 logging.info('Selected ' + str(len(selected_events)) + ' events with ' + n_cluster_condition + ' and ' + cluster_size_condition)
+#                 write_hits_in_events(hit_table_in=in_hit_file_h5.root.Hits, hit_table_out=hit_table_out, events=selected_events)  # write the hits of the selected events into a new table
+#                 logging.info('%d %%' % int(float(float(iCluster) / float(table_size) * 100.)))
 
 if __name__ == "__main__":
     start_time = datetime.now()
