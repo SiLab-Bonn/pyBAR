0b5730c2698b565613425e68fc0163e177a9fcce
davidlp <davidlp@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-12-17 10:04:35 +0000

    ENH: example how to copy the meta data to a new file included

--- trunk/host/analyze_example_cuts.py
+++ trunk/host/analyze_example_cuts.py
@@ -32,7 +32,7 @@ def select_hits():
                 selected_events = selected_events_1[np.in1d(selected_events_1, selected_events_2, assume_unique=True)]  # select events with both conditions above
                 logging.info('Selected ' + str(len(selected_events)) + ' events with ' + n_cluster_condition + ' and ' + cluster_size_condition)
                 last_word_number = write_hits_in_events(hit_table_in=in_hit_file_h5.root.Hits, hit_table_out=hit_table_out, events=selected_events, start_hit_word=last_word_number)  # write the hits of the selected events into a new table
-
+            in_hit_file_h5.root.meta_data.copy(out_hit_file_h5.root)  # copy meta_data note to new file
 if __name__ == "__main__":
     start_time = datetime.now()
     select_hits()
