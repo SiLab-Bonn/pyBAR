785c623abdca34db69a56df7d748c565acc0c4da
DavidLP <pohl@physik.uni-bonn.de>
2013-12-17 08:54:29 +0000

    ENH: read cluster in chunks to avoid memory errors in very big tables

--- host/analyze_example_cuts.py
+++ host/analyze_example_cuts.py
@@ -1,6 +1,6 @@
 """This is an example module how to use the analysis_utils to apply cuts on interpreted hit and cluster data.
 """
-scan_name = 'SCC45_raw_data'
+scan_name = 'scan_fei4_trigger_141'
 
 chip_flavor = 'fei4a'
 input_file = 'data/' + scan_name + ".h5"
@@ -13,7 +13,7 @@ import numpy as np
 from datetime import datetime
 import logging
 from analysis.RawDataConverter import data_struct
-from analysis.analysis_utils import get_events_with_cluster_size, get_events_with_n_cluster, write_hits_in_events
+from analysis.analysis_utils import get_events_with_cluster_size, get_events_with_n_cluster, write_hits_in_events, data_aligned_at_events
 
 chunk_size = 10000000
 
@@ -25,28 +25,12 @@ def select_hits():
         with tb.openFile(output_file_hits, mode="w") as out_hit_file_h5:
             hit_table_out = out_hit_file_h5.createTable(out_hit_file_h5.root, name='Hits', description=data_struct.HitInfoTable, title='hit_data', filters=tb.Filters(complib='blosc', complevel=5, fletcher32=False))
             cluster_table = in_hit_file_h5.root.Cluster
-            table_size = cluster_table.shape[0]
-            last_event_start_index = 0
-            n_cluster = 0  # number of hits in actual chunk
-            for iCluster in range(0, table_size, chunk_size):
-                offset = last_event_start_index - n_cluster if iCluster != 0 else 0  # reread last hits of last event of last chunk
-                cluster = cluster_table.read(iCluster + offset, iCluster + chunk_size)
-                n_cluster = cluster.shape[0]
-
-                # align hits at events, omit last event
-                last_event = cluster["event_number"][-1]
-                last_event_start_index = np.where(cluster["event_number"] == last_event)[0][0]
-
-                # do not omit the last words of the last events of the last chunk
-                if(iCluster == range(0, table_size, chunk_size)[-1]):
-                    last_event_start_index = n_cluster
-
-                selected_events_1 = get_events_with_cluster_size(event_number=cluster['event_number'], cluster_size=cluster['size'], condition=cluster_size_condition)  # select the events with clusters of a certain size
-#                 selected_events_2 = get_events_with_n_cluster(event_number=cluster['event_number'], condition=n_cluster_condition)  # select the events with a certain cluster number
-#                 selected_events = selected_events_1[np.in1d(selected_events_1, selected_events_2, assume_unique=True)]  # select events with both conditions above
-#                 logging.info('Selected ' + str(len(selected_events)) + ' events with ' + n_cluster_condition + ' and ' + cluster_size_condition)
-#                 write_hits_in_events(hit_table_in=in_hit_file_h5.root.Hits, hit_table_out=hit_table_out, events=selected_events)  # write the hits of the selected events into a new table
-#                 logging.info('%d %%' % int(float(float(iCluster) / float(table_size) * 100.)))
+            for data in data_aligned_at_events(cluster_table):
+                selected_events_1 = get_events_with_cluster_size(event_number=data['event_number'], cluster_size=data['size'], condition=cluster_size_condition)  # select the events with clusters of a certain size
+                selected_events_2 = get_events_with_n_cluster(event_number=data['event_number'], condition=n_cluster_condition)  # select the events with a certain cluster number
+                selected_events = selected_events_1[np.in1d(selected_events_1, selected_events_2, assume_unique=True)]  # select events with both conditions above
+                logging.info('Selected ' + str(len(selected_events)) + ' events with ' + n_cluster_condition + ' and ' + cluster_size_condition)
+                write_hits_in_events(hit_table_in=in_hit_file_h5.root.Hits, hit_table_out=hit_table_out, events=selected_events)  # write the hits of the selected events into a new table
 
 if __name__ == "__main__":
     start_time = datetime.now()
