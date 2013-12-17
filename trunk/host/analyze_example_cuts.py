6ea92535f4f41aaf2fbc4fc0c0ddc62a63a83fe1
davidlp <davidlp@65a71841-4583-47e0-a5e0-152ae865ff8c>
2013-12-17 09:38:04 +0000

    ENH: uses the new feature to tell the write_hits_in_events function the start hit index

--- trunk/host/analyze_example_cuts.py
+++ trunk/host/analyze_example_cuts.py
@@ -25,12 +25,13 @@ def select_hits():
         with tb.openFile(output_file_hits, mode="w") as out_hit_file_h5:
             hit_table_out = out_hit_file_h5.createTable(out_hit_file_h5.root, name='Hits', description=data_struct.HitInfoTable, title='hit_data', filters=tb.Filters(complib='blosc', complevel=5, fletcher32=False))
             cluster_table = in_hit_file_h5.root.Cluster
+            last_word_number = 0
             for data in data_aligned_at_events(cluster_table):
                 selected_events_1 = get_events_with_cluster_size(event_number=data['event_number'], cluster_size=data['size'], condition=cluster_size_condition)  # select the events with clusters of a certain size
                 selected_events_2 = get_events_with_n_cluster(event_number=data['event_number'], condition=n_cluster_condition)  # select the events with a certain cluster number
                 selected_events = selected_events_1[np.in1d(selected_events_1, selected_events_2, assume_unique=True)]  # select events with both conditions above
                 logging.info('Selected ' + str(len(selected_events)) + ' events with ' + n_cluster_condition + ' and ' + cluster_size_condition)
-                write_hits_in_events(hit_table_in=in_hit_file_h5.root.Hits, hit_table_out=hit_table_out, events=selected_events)  # write the hits of the selected events into a new table
+                last_word_number = write_hits_in_events(hit_table_in=in_hit_file_h5.root.Hits, hit_table_out=hit_table_out, events=selected_events, start_hit_word=last_word_number)  # write the hits of the selected events into a new table
 
 if __name__ == "__main__":
     start_time = datetime.now()
