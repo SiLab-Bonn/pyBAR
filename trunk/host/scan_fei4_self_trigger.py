2ec6a936a81d9007396e6b39e0c41b17a811b1de
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2014-01-03 12:37:43 +0000

    MAINT: remove kwargs

--- trunk/host/scan_fei4_self_trigger.py
+++ trunk/host/scan_fei4_self_trigger.py
@@ -12,7 +12,7 @@ class FEI4SelfTriggerScan(ScanBase):
     def __init__(self, config_file, definition_file=None, bit_file=None, device=None, scan_identifier="scan_fei4_self_trigger", scan_data_path=None):
         super(FEI4SelfTriggerScan, self).__init__(config_file=config_file, definition_file=definition_file, bit_file=bit_file, device=device, scan_identifier=scan_identifier, scan_data_path=scan_data_path)
 
-    def scan(self, col_span=[1, 80], row_span=[1, 336], timeout_no_data=10, scan_timeout=600, **kwargs):
+    def scan(self, col_span=[1, 80], row_span=[1, 336], timeout_no_data=10, scan_timeout=600):
         '''Scan loop
 
         Parameters
