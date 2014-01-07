88f42d6b6d84c9a36c8dea9d4965d4270b0936e0
DavidLP <pohl@physik.uni-bonn.de>
2014-01-07 12:08:43 +0000

    ENH: cluster size plot is created now ENH: one can select only good data points ENH: the S-curve fit is substituted by a generic spline fit ENH: the result is differentiated and the MPV determined

--- host/analyze_source_scan_gdac_data.py
+++ host/analyze_source_scan_gdac_data.py
@@ -2,18 +2,12 @@ import numpy as np
 import tables as tb
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
-from scipy.special import erf
-from scipy.optimize import curve_fit
 
-from analysis.plotting.plotting import plot_profile_histogram, plot_scatter, plot_occupancy
+from analysis.plotting.plotting import plot_scatter, plot_occupancy
 from analysis import analysis_utils
 
 import logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
-
-
-def scurve(x, A, mu, sigma):
-    return 0.5 * (1 - A) * erf((x - mu) / (np.sqrt(2) * sigma)) + 0.5 * A
 
 
 def get_mean_threshold(gdac, mean_threshold_calibration):
@@ -78,6 +72,30 @@ def get_hit_rate_correction(gdacs, calibration_gdacs, cluster_size_histogram):
     return interpolation(gdacs)
 
 
+def plot_cluster_sizes(in_file_cluster_h5, in_file_calibration_h5, gdac_range, vcal_calibration):
+    mean_threshold_calibration = in_file_calibration_h5.root.MeanThresholdCalibration[:]
+    hist = in_file_cluster_h5.root.AllHistClusterSize[:]
+    hist_sum = np.sum(hist, axis=1)
+    hist_rel = hist / hist_sum[:, np.newaxis] * 100
+    x = get_mean_threshold(gdac_range, mean_threshold_calibration)
+    plt.grid(True)
+    plt.plot(x * vcal_calibration, hist_rel[:, 1], '-o')
+    plt.plot(x * vcal_calibration, hist_rel[:, 2], '-o')
+    plt.plot(x * vcal_calibration, hist_rel[:, 3], '-o')
+    plt.plot(x * vcal_calibration, hist_rel[:, 4], '-o')
+    plt.plot(x * vcal_calibration, hist_rel[:, 5], '-o')
+    plt.title('Frequency of different cluster sizes for different thresholds')
+    plt.xlabel('threshold [e]')
+    plt.ylabel('cluster size frequency [%]')
+    plt.legend(["1 hit cluster", "2 hit cluster", "3 hit cluster", "4 hit cluster", "5 hit cluster"], loc='best')
+#             plt.ylim(0, 100)
+#             plt.xlim(0, 12000)
+    fig = plt.gca()
+    fig.patch.set_facecolor('white')
+    plt.show()
+    plt.close()
+
+
 def select_hot_region(hits, cut_threshold=0.8):
     '''Takes the hit array and masks all pixels with occupancy < (max_occupancy-min_occupancy) * cut_threshold.
 
@@ -98,16 +116,20 @@ def select_hot_region(hits, cut_threshold=0.8):
     return np.ma.masked_where(hits < cut_threshold * (np.amax(hits) - np.amin(hits)), hits)
 
 if __name__ == "__main__":
-    scan_name = 'scan_fei4_trigger_gdac_0'
+    scan_name = 'scan_fei4_trigger_gdac'
     folder = 'K:\\data\\FE-I4\\ChargeRecoMethod\\'
-    input_file_hits = folder + 'bias_20\\' + scan_name + "_cut_1_analyzed.h5"
+    input_file_hits = folder + 'bias_2\\' + scan_name + "_cut_1_analyzed.h5"
     input_file_calibration = folder + 'calibration\\calibrate_threshold_gdac_SCC_99.h5'
-    input_file_correction = folder + 'bias_20\\' + scan_name + "_cluster_sizes.h5"
+    input_file_correction = folder + 'bias_2\\' + scan_name + "_cluster_sizes.h5"
 
-    scan_name = 'bias_20\\scan_fei4_trigger_gdac_0'
+    scan_name = 'bias_2\\scan_fei4_trigger_gdac_0'
     folder = 'K:\\data\\FE-I4\\ChargeRecoMethod\\'
 
     use_cluster_rate_correction = True
+
+    smoothness = 200  # the smoothness of the spline fit to the data
+    vcal_calibration = 55.  # calibration electrons/PlsrDAC
+    n_bins = 200  # number of bins for the profile histogram
 
     with tb.openFile(input_file_calibration, mode="r") as in_file_calibration_h5:  # read calibration file from calibrate_threshold_gdac scan
         with tb.openFile(input_file_hits, mode="r") as in_file_hits_h5:  # read scan data file from scan_fei4_trigger_gdac scan
@@ -124,6 +146,7 @@ if __name__ == "__main__":
                 correction_h5 = tb.openFile(input_file_correction, mode="r")
                 cluster_size_histogram = correction_h5.root.AllHistClusterSize[:]
                 correction_factors = get_hit_rate_correction(gdacs=gdac_range_source_scan, calibration_gdacs=gdac_range_source_scan, cluster_size_histogram=cluster_size_histogram)
+                plot_cluster_sizes(correction_h5, in_file_calibration_h5, gdac_range=gdac_range_source_scan, vcal_calibration=vcal_calibration)
 
             logging.info('Analyzing source scan data with %d different GDAC settings from %d to %d with minimum step sizes from %d to %d' % (len(gdac_range_source_scan), np.min(gdac_range_source_scan), np.max(gdac_range_source_scan), np.min(np.gradient(gdac_range_source_scan)), np.max(np.gradient(gdac_range_source_scan))))
             logging.info('Use calibration data with %d different GDAC settings from %d to %d with minimum step sizes from %d to %d' % (len(gdac_range_calibration), np.min(gdac_range_calibration), np.max(gdac_range_calibration), np.min(np.gradient(gdac_range_calibration)), np.max(np.gradient(gdac_range_calibration))))
@@ -146,26 +169,37 @@ if __name__ == "__main__":
             if np.isnan(x).sum() > 0 or np.isnan(y).sum() > 0:
                 logging.warning('There are pixels with NaN threshold or hit values, analysis will be wrong')
 
-            # calculated profile histogram, fit scurve to it and plot results
-            x_p, y_p, y_p_e = analysis_utils.get_profile_histogram(x, y, n_bins=100)  # profile histogram data
-            plot_profile_histogram(x=x_p * 55., y=y_p / 100., n_bins=len(gdac_range_source_scan) / 2, title='\'Single hit cluster\'-rate for different pixel thresholds', x_label='Pixel threshold [e]', y_label='Single hit cluster rate [a.u.]')
+            # calculated profile histogram
+            x_p, y_p, y_p_e = analysis_utils.get_profile_histogram(x, y, n_bins=n_bins)  # profile histogram data
 
-            # fit Scurve and plot differentiated results
-            popt, _ = curve_fit(scurve, x_p, y_p, sigma=y_p_e, p0=[11.5, 164, 73])  # fit scurve
-            y_p_f = scurve(x_p, popt[0], popt[1], popt[2])  # calculate y from the fitted scurve function
-            plt.plot(x_p * 55., -analysis_utils.central_difference(x_p, y_p), 'o')  # plot differentiated data
-            plt.plot(x_p * 55., -analysis_utils.central_difference(x_p, y_p_f), '-')  # plot differentiated fit data
+            # select only the data point where the calibration worked
+            selected_data = np.logical_or(x_p > 4840 / vcal_calibration, x_p < 4180 / vcal_calibration)
+            x_p = x_p[selected_data]
+            y_p = y_p[selected_data]
+            y_p_e = y_p_e[selected_data]
+
+            # fit spline, differentiate, determine MPV and plot
+            smoothed_data = analysis_utils.smooth_differentiation(x_p, y_p, weigths=1 / y_p_e, order=3, smoothness=smoothness, derivation=0)
+            smoothed_data_diff = analysis_utils.smooth_differentiation(x_p, y_p, weigths=1 / y_p_e, order=3, smoothness=smoothness, derivation=1)
+            p1 = plt.errorbar(x_p * vcal_calibration, y_p, yerr=y_p_e, fmt='o')  # plot differentiated data with error bars of data
+            p2, = plt.plot(x_p * vcal_calibration, smoothed_data, '-r')  # plot smoothed data
+            p3, = plt.plot(x_p * vcal_calibration, -100. * smoothed_data_diff, '-', lw=2)  # plot differentiated data
+            mpv_index = np.argmax(-analysis_utils.smooth_differentiation(x_p, y_p, weigths=1 / y_p_e, order=3, smoothness=smoothness, derivation=1))
+            p4, = plt.plot([x_p[mpv_index] * vcal_calibration, x_p[mpv_index] * vcal_calibration], [0, -100. * smoothed_data_diff[mpv_index]], 'k-', lw=2)
+            text = 'MPV ' + str(int(x_p[mpv_index] * vcal_calibration)) + ' e'
+            plt.text(1.01 * x_p[mpv_index] * vcal_calibration, -10. * smoothed_data_diff[mpv_index], text, ha='left')
+            plt.legend([p1, p2, p3, p4], ['data', 'smoothed spline', 'spline differentiation', text], prop={'size': 12})
             plt.title('\'Single hit cluster\'-occupancy for different pixel thresholds')
             plt.xlabel('Pixel threshold [e]')
             plt.ylabel('Single hit cluster occupancy [a.u.]')
-            plt.ylim((0, 8))
+            plt.ylim((0, 1.05 * np.amax(y_p)))
             plt.show()
 
-            # calculate and plot mean results
-            x_mean = get_mean_threshold(gdac_range_source_scan, mean_threshold_calibration)
-            y_mean = selected_pixel_hits.mean(axis=(0))
-
-            plot_scatter(x_mean * 55, y_mean, title='Mean single pixel cluster rate at different thresholds', x_label='mean threshold [e]', y_label='mean single pixel cluster rate')
+            #  calculate and plot mean results
+#             x_mean = get_mean_threshold(gdac_range_source_scan, mean_threshold_calibration)
+#             y_mean = selected_pixel_hits.mean(axis=(0))
+#
+#             plot_scatter(x_mean * 55, y_mean, title='Mean single pixel cluster rate at different thresholds', x_label='mean threshold [e]', y_label='mean single pixel cluster rate')
 
     if use_cluster_rate_correction:
         correction_h5.close()
