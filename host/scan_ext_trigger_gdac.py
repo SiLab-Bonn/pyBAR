19ea132628aff4a450159d6142752c5f6ee4bc64
Jens Janssen <janssen@physik.uni-bonn.de>
2014-01-09 19:46:23 +0000

    ENH: reset trigger counter each scan step

--- host/scan_ext_trigger_gdac.py
+++ host/scan_ext_trigger_gdac.py
@@ -91,8 +91,7 @@ class ExtTriggerGdacScan(ScanBase):
                 lvl1_command = self.register.get_commands("zeros", length=14)[0] + self.register.get_commands("lv1")[0]  # + self.register.get_commands("zeros", length=1000)[0]
                 self.register_utils.set_command(lvl1_command)
                 # setting up external trigger
-                self.readout_utils.set_trigger_number(0)
-                self.readout_utils.configure_trigger_fsm(mode=0, trigger_data_msb_first=False, disable_veto=False, trigger_data_delay=0, trigger_clock_cycles=16, enable_reset=False, invert_lemo_trigger_input=False, trigger_low_timeout=0)
+                self.readout_utils.configure_trigger_fsm(mode=0, trigger_data_msb_first=False, disable_veto=False, trigger_data_delay=0, trigger_clock_cycles=16, enable_reset=False, invert_lemo_trigger_input=False, trigger_low_timeout=0, reset_trigger_counter=True)
                 self.readout_utils.configure_command_fsm(enable_ext_trigger=True, diable_clock=False, disable_command_trigger=False)
 
                 show_trigger_message_at = 10 ** (int(math.ceil(math.log10(max_triggers))) - 1)
