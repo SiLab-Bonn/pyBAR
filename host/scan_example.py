021c0a2ee3466a69cba6a3633c7fc21a724052b8
Jens Janssen <janssen@physik.uni-bonn.de>
2014-01-15 08:01:04 +0000

    ENH: adding more comments on class variables

--- host/scan_example.py
+++ host/scan_example.py
@@ -15,6 +15,10 @@ scan_configuration = {
 
 # inherit from ScanBase class
 class ExampleScan(ScanBase):
+    # a class variable
+    some_class_varible = None
+
+    # initializing the newly-created class instance
     def __init__(self, configuration_file, definition_file=None, bit_file=None, force_download=False, device=None, scan_data_path=None, device_identifier=""):
         # accessing inherited methods that have been overridden in a class
         super(ExampleScan, self).__init__(configuration_file=configuration_file, definition_file=definition_file, bit_file=bit_file, force_download=force_download, device=device, scan_data_path=scan_data_path, device_identifier=device_identifier, scan_identifier="example_scan")
