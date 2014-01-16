dd740c02cc5df1375fbf23488f3d6a838469dc6d
davidlp <davidlp@65a71841-4583-47e0-a5e0-152ae865ff8c>
2014-01-16 10:45:52 +0000

    ENH: settings are mentioned on top of the file ENH: better exception catching ENH: one can define a delay time for waitong for new data ENH: one can plot the occupancy map not integrated

--- trunk/host/analyze_real_time.py
+++ trunk/host/analyze_real_time.py
@@ -18,15 +18,29 @@ from analysis.RawDataConverter.data_histograming import PyDataHistograming
 from analysis.plotting import plotting
 # from analysis.RawDataConverter.data_clusterizer import PyDataClusterizer
 
+scan_configuration = {
+    "scan_name": 'input_copy',
+    "folder": 'data//',
+    "chip_flavor": 'fei4a',
+    "trig_count": 16,
+    "integrate" : False,
+    "chunk_size": 2000000,
+    "no_data_wait_time": 1,
+    "warnings": True,
+    "infos": False
+}
+
 
 def analyze_raw_data(in_file_h5, start, stop):
     logging.info('Analyze raw data from word index ' + str(start) + ' to ' + str(stop))
     n_hits = 0
-    for iWord in range(start, stop, chunk_size):
-        if stop - start <= chunk_size:
+    if not scan_configuration['integrate']:
+        histograming.reset()
+    for iWord in range(start, stop, scan_configuration['chunk_size']):
+        if stop - start <= scan_configuration['chunk_size']:
             stop_index = stop
         else:
-            stop_index = iWord + chunk_size
+            stop_index = iWord + scan_configuration['chunk_size']
         try:
             logging.info('Take chunk from ' + str(iWord) + ' to ' + str(stop_index))
             raw_data = in_file_h5.root.raw_data.read(iWord, stop_index)
@@ -36,6 +50,8 @@ def analyze_raw_data(in_file_h5, start, stop):
             n_hits += Nhits
         except tb.exceptions.NoSuchNodeError:
             logging.info('File in inconsistent state, omit hits')
+        except tb.exceptions.HDF5ExtError:
+            logging.info('File in inconsistent state, omit hits')
 
     occupancy = np.zeros(80 * 336 * histograming.get_n_parameters(), dtype=np.uint32)  # create linear array as it is created in histogram class
     histograming.get_occupancy(occupancy)
@@ -43,7 +59,7 @@ def analyze_raw_data(in_file_h5, start, stop):
     occupancy_array = np.swapaxes(occupancy_array, 0, 1)
     occupancy_array_ma = np.ma.array(occupancy_array)[:, :, 0]
 #     plotting.plot_fancy_occupancy(occupancy_array_ma)
-#     plot_fancy_occupancy(hist=occupancy_array_ma, filename=None, z_max='maximum')
+    plotting.plot_occupancy(occupancy_array_ma)
     error_counter_hist = np.zeros(16, dtype=np.uint32)
     interpreter.get_error_counters(error_counter_hist)
     plotting.plot_event_errors(error_counter_hist)
@@ -56,8 +72,11 @@ def analyze_raw_data_file(input_file):
     old_last_index_read = 0
     total_hits = 0
     interpreter.set_hits_array(hits)  # set the temporary hit data to be filled by the interpreter
+    start_time_scan = 0
+    start_time_scan_set = False
+    wait_for_data = False
+    start_time_analysis = datetime.now()
     while True:# (datetime.now() - start_time).total_seconds() < 50:
-        time.sleep(0.2)
         try:
             with tb.openFile(input_file, mode="r") as in_file_h5:
                 meta_data = in_file_h5.root.meta_data[:]
@@ -65,15 +84,25 @@ def analyze_raw_data_file(input_file):
                 meta_event_index = np.zeros((meta_data.shape[0],), dtype=[('metaEventIndex', np.uint32)])  # this array is filled by the interpreter and holds the event number per read out
                 interpreter.set_meta_event_data(meta_event_index)
                 last_index_read = len(in_file_h5.root.meta_data) - 1
+                if not start_time_scan_set:
+                    start_time_scan = meta_data[old_last_index_read]['timestamp_start']
+                    start_time_scan_set = True
                 if (last_index_read != old_last_index_read):
 #                     print 'new meta data from',old_last_index_read,'to',last_index_read
+#                     print 'scan time', meta_data[old_last_index_read]['timestamp_start'] - start_time_scan
+#                     print 'analysis time', (datetime.now() - start_time_analysis).total_seconds()
+#                     print meta_data[old_last_index_read]['timestamp_start'] - start_time_scan - (datetime.now() - start_time_analysis).total_seconds()
                     start_index = meta_data[old_last_index_read]['index_start']
                     stop_index = meta_data[last_index_read]['index_start']
                     total_hits += analyze_raw_data(in_file_h5, start=start_index, stop=stop_index)
                     old_last_index_read = last_index_read
                 else:
-                    logging.info('No new data, stop monitor')
-                    break
+                    if not wait_for_data:
+                        logging.info('No new data, wait %d seconds for new data' % scan_configuration['no_data_wait_time'])
+                        wait_for_data = True
+                        time.sleep(scan_configuration['no_data_wait_time'])
+                    else:
+                        break
         except tb.exceptions.HDF5ExtError:
             logging.info('File in inconsistent state, read again')
 
@@ -81,23 +110,18 @@ if __name__ == "__main__":
     interpreter = PyDataInterpreter()
     histograming = PyDataHistograming()
 
-    # settings
-    scan_name = 'scan_threshold_fast_1_copy'
-    folder = 'data/'
-    chip_flavor = 'fei4a'
-    chunk_size = 2000000
-    interpreter.set_info_output(False)
-    interpreter.set_warning_output(True)
-    interpreter.set_FEI4B(False)
-    interpreter.set_trig_count(16)
+    interpreter.set_info_output(scan_configuration['infos'])
+    interpreter.set_warning_output(scan_configuration['warnings'])
+    interpreter.set_FEI4B(False if scan_configuration['chip_flavor'] == 'fei4a' else True)
+    interpreter.set_trig_count(scan_configuration['trig_count'])
     histograming.set_no_scan_parameter()
     histograming.create_occupancy_hist(True)
     plt.ion()
 
-    hits = np.empty((chunk_size,), dtype=tb.dtype_from_descr(data_struct.HitInfoTable))  # hold the hits per analyze_raw_data call
+    hits = np.empty((scan_configuration['chunk_size'],), dtype=tb.dtype_from_descr(data_struct.HitInfoTable))  # hold the hits per analyze_raw_data call
 
     start_time = datetime.now()
-    analyze_raw_data_file(input_file=folder + scan_name + '.h5')
+    analyze_raw_data_file(input_file=scan_configuration['folder'] + scan_configuration['scan_name'] + '.h5')
     logging.info('Script runtime %.1f seconds' % (datetime.now() - start_time).total_seconds())
 
     plt.ioff()
