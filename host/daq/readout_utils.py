e23fbef8b44b155d1d3cea62e5d3a7dde1678e82
Jens Janssen <janssen@physik.uni-bonn.de>
2014-01-19 15:50:53 +0000

    MAINT:

--- host/daq/readout_utils.py
+++ host/daq/readout_utils.py
@@ -115,7 +115,7 @@ class ReadoutUtils(object):
             array = self.device.ReadExternal(address=0x8200 + 2, size=1)  # get stored register value
             reg = struct.unpack('B', array)
             if reg & 0x80:
-                logging.info('TLU detected at RJ45')
+                logging.info('TLU detected at RJ45 port')
         else:
             logging.info('Using RJ45 port for trigger')
 
