f058c7be2469d21f78c6d76acbfa552b99606c07
DavidLP <pohl@physik.uni-bonn.de>
2014-01-21 20:20:45 +0000

    ENH: one can monitore all files in a folder ENH: the email text can be changed as a configuration parameter

--- host/analyze_data_taking_status.py
+++ host/analyze_data_taking_status.py
@@ -14,10 +14,12 @@ from watchdog.events import FileSystemEventHandler
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s")
 
 configuration = {
-    "path_to_monitor": 'data',  # the monitor the watchdog checks
+    "path_to_monitor": 'data/SCC_99',  # the monitor the watchdog checks
     "timeout": 1,  # the timeout in seconds until an alert email is send if the data files did not change
-    'data_files': ['foo.h5', 'bar.h5'],  # the files that are monitored for changes
+    'data_files': None,  # the files that are monitored for changes
     "check_subfolders": True,  # check also the subfolders of 'path_to_monitor'
+    "email_text_alert": 'Sorry... go to work ;-)\nSincerely Mr. Beam',  # outgoing mail server
+    "email_text_alert_cleared": 'Very nice, I see that the data taking works again\nSincerely Mr. Beam',  # outgoing mail server
     "email_host": 'mail.gmx.net',  # outgoing mail server
     "email_to": ["pohl@physik.uni-bonn.de", "janssen@physik.uni-bonn.de"],  # the Email adresses the status emails are send to
     "email_account": ['mr_beam@gmx.de', 'pidub123']  # email account name and password used to send email
@@ -41,6 +43,8 @@ def send_mail(text, subject=''):
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
+        if configuration['data_files'] is None:
+            data_changed.set()
         if os.path.basename(event.src_path) in configuration['data_files']:
             data_changed.set()
 
@@ -60,12 +64,12 @@ if __name__ == '__main__':
             if not data_changed.is_set():
                 logging.info('data taking BAD')
                 if not alert_status.is_set():
-                    send_mail("Sorry... go to work ;-)\nSincerely Mr. Beam", subject='TESTBEAM WARNING')
+                    send_mail(configuration['email_text_alert'], subject='TESTBEAM WARNING')
                     alert_status.set()
             else:
                 logging.info('data taking OK')
                 if alert_status.is_set():
-                    send_mail("Very nice, I see that the data taking works again\nSincerely Mr. Beam", subject='TESTBEAM INFO')
+                    send_mail(configuration['email_text_alert_cleared'], subject='TESTBEAM INFO')
                     alert_status.clear()
             data_changed.clear()
     except KeyboardInterrupt:
