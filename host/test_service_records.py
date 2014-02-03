c07dbb92ded71b4991ed421d87f35b7668b50ca8
Jens Janssen <janssen@physik.uni-bonn.de>
2014-02-03 18:14:03 +0000

    BUG: fix scan

--- host/test_service_records.py
+++ host/test_service_records.py
@@ -2,7 +2,7 @@
 
 """
 from scan.scan import ScanBase
-from daq.readout import save_raw_data_from_data_dict_iterable
+from daq.readout import save_raw_data_from_data_dict_iterable, FEI4Record
 from analysis.analyze_raw_data import AnalyzeRawData
 
 import logging
@@ -10,16 +10,21 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(leve
 
 
 class TestServiceRecords(ScanBase):
-    def __init__(self, configuration_file, definition_file=None, bit_file=None, device=None, scan_identifier="test_service_records", scan_data_path=None):
-        super(TestServiceRecords, self).__init__(configuration_file=configuration_file, definition_file=definition_file, bit_file=bit_file, device=device, scan_identifier=scan_identifier, scan_data_path=scan_data_path)
+    def __init__(self, configuration_file, definition_file=None, bit_file=None, force_download=False, device=None, scan_data_path=None, device_identifier=""):
+        super(TestServiceRecords, self).__init__(configuration_file=configuration_file, definition_file=definition_file, bit_file=bit_file, force_download=force_download, device=device, scan_data_path=scan_data_path, device_identifier=device_identifier, scan_identifier="test_service_records")
 
     def scan(self):
         self.register.create_restore_point()
 
-        read_service_records()
+        read_service_records(self)
 
         # saving data
-        save_raw_data_from_data_dict_iterable((self.readout.read_data_dict(),), filename=self.scan_data_filename, title=self.scan_identifier)
+        data_dict = self.readout.read_data_dict()
+        save_raw_data_from_data_dict_iterable((data_dict,), filename=self.scan_data_filename, title=self.scan_identifier)
+
+        # debug
+#         for data in data_dict["data"]:
+#             print FEI4Record(data, chip_flavor=self.register.chip_flavor)
 
         self.register.restore()
         self.register_utils.configure_global()
@@ -38,7 +43,7 @@ def read_service_records(self):
     logging.info('Reading Service Records...')
     commands = []
     commands.extend(self.register.get_commands("confmode"))
-    self.send_commands(commands)
+    self.register_utils.send_commands(commands)
     self.readout.reset_sram_fifo()
     commands = []
     self.register.set_global_register_value('ReadErrorReq', 1)
@@ -46,7 +51,7 @@ def read_service_records(self):
     commands.extend(self.register.get_commands("globalpulse", width=0))
     self.register.set_global_register_value('ReadErrorReq', 0)
     commands.extend(self.register.get_commands("wrregister", name=['ReadErrorReq']))
-    self.send_commands(commands)
+    self.register_utils.send_commands(commands)
 
 
 if __name__ == "__main__":
@@ -54,3 +59,4 @@ if __name__ == "__main__":
     scan = TestServiceRecords(**configuration.device_configuration)
     scan.start(configure=False)
     scan.stop()
+    scan.analyze()
