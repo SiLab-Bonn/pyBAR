c4beb2442c6ad1bec888e188cb52642197e2bc7b
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2014-02-03 18:35:27 +0000

    ENH: update scan loop to detect errors ENH: better behavior on Ctrl-C ENH: fix default scan parameters

--- trunk/host/scan_ext_trigger.py
+++ trunk/host/scan_ext_trigger.py
@@ -12,10 +12,11 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)-8s] (%
 scan_configuration = {
     "mode": 0,
     "trigger_latency": 232,
+    "trigger_delay": 14,
     "col_span": [1, 80],
     "row_span": [1, 336],
     "timeout_no_data": 10,
-    "scan_timeout": 10 * 60,
+    "scan_timeout": 1 * 60,
     "max_triggers": 10000,
     "enable_hitbus": False
 }
@@ -25,7 +26,7 @@ class ExtTriggerScan(ScanBase):
     def __init__(self, configuration_file, definition_file=None, bit_file=None, force_download=False, device=None, scan_data_path=None, device_identifier=""):
         super(ExtTriggerScan, self).__init__(configuration_file=configuration_file, definition_file=definition_file, bit_file=bit_file, force_download=force_download, device=device, scan_data_path=scan_data_path, device_identifier=device_identifier, scan_identifier="ext_trigger_scan")
 
-    def scan(self, mode=0, trigger_latency=232, col_span=[1, 80], row_span=[1, 336], timeout_no_data=10, scan_timeout=10 * 60, max_triggers=10000, enable_hitbus=False):
+    def scan(self, mode=0, trigger_latency=232, trigger_delay=14, col_span=[1, 80], row_span=[1, 336], timeout_no_data=10, scan_timeout=10 * 60, max_triggers=10000, enable_hitbus=False):
         '''Scan loop
 
         Parameters
@@ -37,10 +38,12 @@ class ExtTriggerScan(ScanBase):
             2: TLU simple handshake (automatic detection of TLU connection (TLU port/RJ45)).
             3: TLU trigger data handshake (automatic detection of TLU connection (TLU port/RJ45)).
         trigger_latency : int
-            FE trigger latency. From 0 to 255.
+            FE global register Trig_Lat.
             Some ballpark estimates:
             External scintillator/TLU: 232
             FE Hit-OR: 216
+        trigger_delay : int
+            Delay between trigger and LVL1 command.
         col_span : list, tuple
             Column range (from minimum to maximum value). From 1 to 80.
         row_span : list, tuple
@@ -87,75 +90,62 @@ class ExtTriggerScan(ScanBase):
 
         wait_for_first_trigger = True
 
-        with open_raw_data_file(filename=self.scan_data_filename, title=self.scan_identifier) as raw_data_file:
+        with open_raw_data_file(filename=self.scan_data_filename, title=self.scan_identifier, mode='w') as raw_data_file:
             self.readout.start()
-
             # preload command
-            lvl1_command = self.register.get_commands("zeros", length=14)[0] + self.register.get_commands("lv1")[0]  # + self.register.get_commands("zeros", length=1000)[0]
+            lvl1_command = self.register.get_commands("zeros", length=trigger_delay)[0] + self.register.get_commands("lv1")[0]  # + self.register.get_commands("zeros", length=200)[0]
             self.register_utils.set_command(lvl1_command)
             # setting up external trigger
-            self.readout_utils.configure_trigger_fsm(mode=mode, trigger_data_msb_first=False, disable_veto=False, trigger_data_delay=0, trigger_clock_cycles=16, enable_reset=False, invert_lemo_trigger_input=False, trigger_low_timeout=0, reset_trigger_counter=True)
+            self.readout_utils.configure_trigger_fsm(mode=mode, trigger_data_msb_first=False, disable_veto=False, trigger_data_delay=0, trigger_clock_cycles=16, enable_reset=False, invert_lemo_trigger_input=False, force_use_rj45=False, trigger_low_timeout=10, reset_trigger_counter=True)
             self.readout_utils.configure_command_fsm(enable_ext_trigger=True, neg_edge=False, diable_clock=False, disable_command_trigger=False)
 
-            show_trigger_message_at = 10 ** (int(math.ceil(math.log10(max_triggers))) - 1)
-            last_iteration = time.time()
-            saw_no_data_at_time = last_iteration
-            saw_data_at_time = last_iteration
-            scan_start_time = last_iteration
-            no_data_at_time = last_iteration
+            show_trigger_message_at = 10 ** (int(math.floor(math.log10(max_triggers) - math.log10(3) / math.log10(10))))
+            time_current_iteration = time.time()
+            saw_no_data_at_time = time_current_iteration
+            saw_data_at_time = time_current_iteration
+            scan_start_time = time_current_iteration
+            no_data_at_time = time_current_iteration
             time_from_last_iteration = 0
             scan_stop_time = scan_start_time + scan_timeout
             current_trigger_number = 0
             last_trigger_number = 0
             while not self.stop_thread_event.wait(self.readout.readout_interval):
-
-#                 if logger.isEnabledFor(logging.DEBUG):
-#                     lost_data = self.readout.get_lost_data_count()
-#                     if lost_data != 0:
-#                         logging.debug('Lost data count: %d', lost_data)
-#                         logging.debug('FIFO fill level: %4f', (float(fifo_size)/2**20)*100)
-#                         logging.debug('Collected triggers: %d', self.readout.get_trigger_number())
-
+                time_last_iteration = time_current_iteration
+                time_current_iteration = time.time()
+                time_from_last_iteration = time_current_iteration - time_last_iteration
                 current_trigger_number = self.readout_utils.get_trigger_number()
                 if (current_trigger_number % show_trigger_message_at < last_trigger_number % show_trigger_message_at):
                     logging.info('Collected triggers: %d', current_trigger_number)
+                    if not any(self.readout.get_rx_sync_status()):
+                        self.stop_thread_event.set()
+                        logging.error('No RX sync. Stopping Scan...')
+                    if any(self.readout.get_rx_8b10b_error_count()):
+                        self.stop_thread_event.set()
+                        logging.error('RX 8b10b error(s) detected. Stopping Scan...')
+                    if any(self.readout.get_rx_fifo_discard_count()):
+                        self.stop_thread_event.set()
+                        logging.error('RX FIFO discard error(s) detected. Stopping Scan...')
                 last_trigger_number = current_trigger_number
                 if max_triggers is not None and current_trigger_number >= max_triggers:
                     logging.info('Reached maximum triggers. Stopping Scan...')
                     self.stop_thread_event.set()
-                if scan_start_time is not None and time.time() > scan_stop_time:
+                if scan_start_time is not None and time_current_iteration > scan_stop_time:
                     logging.info('Reached maximum scan time. Stopping Scan...')
                     self.stop_thread_event.set()
-                # TODO: read 8b10b decoder err cnt
-#                 if not self.readout.read_rx_status():
-#                     logging.info('Lost data sync. Starting synchronization...')
-#                     self.readout.configure_command_fsm(False)
-#                     if not self.readout.reset_rx(1000):
-#                         logging.info('Failed. Stopping scan...')
-#                         self.stop_thread_event.set()
-#                     else:
-#                         logging.info('Done!')
-#                         self.readout.configure_command_fsm(True)
+                try:
+                    raw_data_file.append((self.readout.data.popleft(),))
+                except IndexError:  # no data
+                    no_data_at_time = time_current_iteration
+                    if not wait_for_first_trigger and saw_no_data_at_time > (saw_data_at_time + timeout_no_data):
+                        logging.info('Reached no data timeout. Stopping Scan...')
+                        self.stop_thread_event.set()
+                    elif not wait_for_first_trigger:
+                        saw_no_data_at_time = no_data_at_time
 
-                time_from_last_iteration = time.time() - last_iteration
-                last_iteration = time.time()
-                while True:
-                    try:
-                        raw_data_file.append((self.readout.data.popleft(),))
-                    except IndexError:  # no data
-                        no_data_at_time = last_iteration
-                        if wait_for_first_trigger == False and saw_no_data_at_time > (saw_data_at_time + timeout_no_data):
-                            logging.info('Reached no data timeout. Stopping Scan...')
-                            self.stop_thread_event.set()
-                        elif wait_for_first_trigger == False:
-                            saw_no_data_at_time = no_data_at_time
-
-                        if no_data_at_time > (saw_data_at_time + 10):
-                            scan_stop_time += time_from_last_iteration
-
-                        break  # jump out while loop
-
-                    saw_data_at_time = last_iteration
+                    if no_data_at_time > (saw_data_at_time + 10):
+                        scan_stop_time += time_from_last_iteration
+                else:
+                    saw_data_at_time = time_current_iteration
 
                     if wait_for_first_trigger == True:
                         logging.info('Taking data...')
@@ -163,11 +153,8 @@ class ExtTriggerScan(ScanBase):
 
             self.readout_utils.configure_command_fsm(enable_ext_trigger=False)
             self.readout_utils.configure_trigger_fsm(mode=0)
-
             logging.info('Total amount of triggers collected: %d', self.readout_utils.get_trigger_number())
-
             self.readout.stop()
-
             raw_data_file.append(self.readout.data)
 
     def analyze(self):
@@ -175,9 +162,10 @@ class ExtTriggerScan(ScanBase):
         output_file = self.scan_data_filename + "_interpreted.h5"
         with AnalyzeRawData(raw_data_file=scan.scan_data_filename + ".h5", analyzed_data_file=output_file) as analyze_raw_data:
             analyze_raw_data.interpreter.set_trig_count(self.register.get_global_register_value("Trig_Count"))
-            analyze_raw_data.create_cluster_size_hist = True
             analyze_raw_data.create_source_scan_hist = True
+            analyze_raw_data.create_cluster_size_hist = True
             analyze_raw_data.create_cluster_tot_hist = True
+            analyze_raw_data.create_cluster_table = False
             analyze_raw_data.interpreter.set_warning_output(False)
             analyze_raw_data.interpret_word_table(fei4b=scan.register.fei4b)
             analyze_raw_data.interpreter.print_summary()
