ea92ca6cee55a8a95382457ca93b41475a5dcb6f
DavidLP <pohl@physik.uni-bonn.de>
2014-02-03 13:32:03 +0000

    ENH: two different time outs can be set now: if data is available and if not

--- host/analyze_data_taking_status.py
+++ host/analyze_data_taking_status.py
@@ -16,11 +16,12 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(leve
 
 configuration = {
     "path_to_monitor": 'data/SCC_99',  # the monitor the watchdog checks
-    "timeout": 30 * 60,  # the timeout in seconds until an alert email is send if the data files did not change
+    "timeout": 30 * 60,  # the timeout in seconds until an alert email is send (if the data files did not change)
+    "timeout_alert": 10,  # the timeout in seconds until the alert status is reset (if the data files did change)
     'data_files': None,  # the files that are monitored for changes
     "check_subfolders": True,  # check also the subfolders of 'path_to_monitor'
     "email_text_alert": 'Sorry, SCC 99 did not collect data within the last 30 min. Usually one GDAC setting takes less than 15. min to collect the desired triggers. Please check ;-)\nSincerely Mr. Beam',  # outgoing mail server
-    "email_text_alert_cleared": 'Very nice, I see that the data taking works again\nSincerely Mr. Beam',  # outgoing mail server
+    "email_text_alert_cleared": 'Very nice, I see that the data taking for SCC 99 works again\nSincerely Mr. Beam',  # outgoing mail server
     "email_host": 'mail.gmx.net',  # outgoing mail server
     "email_to": ["pohl@physik.uni-bonn.de", "janssen@physik.uni-bonn.de"],  # the Email adresses the status emails are send to
     "email_account": ['mr_beam@gmx.de', 'pidub123']  # email account name and password used to send email
@@ -51,6 +52,7 @@ class MyHandler(FileSystemEventHandler):
 
 if __name__ == '__main__':
     logging.info('Configuration:\n' + pprint.pformat(configuration))
+    timeout = configuration['timeout']
     alert_status = Event()
     data_changed = Event()
     alert_status.clear()
@@ -61,16 +63,18 @@ if __name__ == '__main__':
 
     try:
         while True:
-            time.sleep(configuration['timeout'])
+            time.sleep(timeout)
             if not data_changed.is_set():
                 logging.info('data taking BAD')
                 if not alert_status.is_set():
                     send_mail(configuration['email_text_alert'], subject='TESTBEAM WARNING')
+                    timeout = configuration['timeout_alert']
                     alert_status.set()
             else:
                 logging.info('data taking OK')
                 if alert_status.is_set():
                     send_mail(configuration['email_text_alert_cleared'], subject='TESTBEAM INFO')
+                    timeout = configuration['timeout']
                     alert_status.clear()
             data_changed.clear()
     except KeyboardInterrupt:
