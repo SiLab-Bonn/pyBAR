692bdeba5a6cce74c3c43bf8a52025c579e1275d
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2014-02-08 18:35:38 +0000

    BUG: FEI4 sources ENH: added missing vmap

--- trunk/device/MultiIO/FPGA/scripts/vsim.tcl
+++ trunk/device/MultiIO/FPGA/scripts/vsim.tcl
@@ -4,6 +4,7 @@
 # vlog_top
 # vsim_top
 # wave_top
+# (restart)
 # run 1ms
 # ----------------------------------------------------------------
 
@@ -16,7 +17,12 @@ set XILINX   $env(XILINX)
 # ----------------------------------------------------------------
 #set path to basil
 # ----------------------------------------------------------------
-set BASIL "../../../../../.."
+set BASIL "../../../../../../basil"
+
+# ----------------------------------------------------------------
+#set path to FEI4 model
+# ----------------------------------------------------------------
+set FEI4 "../../../../../../fei4/trunk/models/fei4a"
 
 # ----------------------------------------------------------------
 # run Xilinx->EDK->Compile Simulation Librarie
@@ -24,37 +30,43 @@ set BASIL "../../../../../.."
 # ----------------------------------------------------------------
 
 # ----------------------------------------------------------------
-#map liblaries - on windows or diffrent linux this can be diffrent
-#vmap unisims_ver $XILINX/verilog/questasim/10.1c/lin64/unisims_ver
-#vmap simprims_ver $XILINX/verilog/questasim/10.1c/lin64/simprims_ver 
+#map liblaries - this can be different on other platforms than Windows
+#vmap unisims_ver $XILINX/ISE/verilog/questasim/10.1c/lin64/unisims_ver
+#vmap simprims_ver $XILINX/ISE/verilog/questasim/10.1c/lin64/simprims_ver 
 # ----------------------------------------------------------------
 
 proc vlog_top {} {
 
     global XILINX
     global BASIL
+    global FEI4
     
-    if { [file exists "work"] == 1} {               
+    if {[file exists "work"] == 1} {
         echo "Deleting old work library ..."
         vdel -all -lib work
     }
 
+    vmap unisims_ver $XILINX/EDK/unisims_ver
+    vmap simprims_ver $XILINX/EDK/simprims_ver
+    #vmap unisims_ver $XILINX/ISE/verilog/questasim/10.1e/nt64/unisims_ver
+    #vmap simprims_ver $XILINX/ISE/verilog/questasim/10.1e/nt64/simprims_ver
+
     vlib work
 
-    vlog $XILINX/verilog/src/glbl.v
+    vlog $XILINX/ISE/verilog/src/glbl.v
 
-    vlog -lint $BASIL/basil/trunk/device/modules/utils/*.v
-    vlog -lint $BASIL/basil/trunk/device/modules/sram_fifo/*.v
-    vlog -lint $BASIL/basil/trunk/device/modules/fei4_rx/*.v
-    vlog -lint $BASIL/basil/trunk/device/modules/cmd_seq/*.v
-    vlog -lint $BASIL/basil/trunk/device/modules/rrp_arbiter/*.v +incdir+$BASIL/basil/trunk/device/modules/rrp_arbiter
-    vlog -lint $BASIL/basil/trunk/device/modules/tlu/*.v
+    vlog -lint $BASIL/trunk/device/modules/utils/*.v
+    vlog -lint $BASIL/trunk/device/modules/sram_fifo/*.v
+    vlog -lint $BASIL/trunk/device/modules/fei4_rx/*.v
+    vlog -lint $BASIL/trunk/device/modules/cmd_seq/*.v
+    vlog -lint $BASIL/trunk/device/modules/rrp_arbiter/*.v +incdir+$BASIL/trunk/device/modules/rrp_arbiter
+    vlog -lint $BASIL/trunk/device/modules/tlu/*.v
 
     vlog ../src/top.v
     vlog ../src/clk_gen.v
 
-    vlog  -novopt $BASIL/fei4/trunk/models/fei4a/fei4_top.sv +incdir+$BASIL/fei4/trunk/models/fei4a +define+TEST_DC=1
-    vlog  +incdir+$BASIL/fei4/trunk/models/fei4a +incdir+$BASIL/basil/trunk/device/modules/tb +incdir+../tb ../tb/top_tb.sv
+    vlog  -novopt $FEI4/fei4_top.sv +incdir+$FEI4 +define+TEST_DC=1
+    vlog  +incdir+$FEI4 +incdir+$BASIL/trunk/device/modules/tb +incdir+../tb ../tb/top_tb.sv
 
     #vlog ../ise/netgen/par/top_timesim.v
 }
@@ -68,10 +80,10 @@ proc wave_top {} {
     add wave -group top sim:/top_tb/*
     add wave -group uut sim:/top_tb/uut/*
     add wave -group clkgen sim:/top_tb/uut/iclkgen/*
-    
+
     add wave -group cmd sim:/top_tb/uut/icmd/i_cmd_seq_core/*
     add wave -group fifo_sram sim:/top_tb/uut/i_out_fifo/i_sram_fifo/*
-    
+
     add wave -group {fei4_rx_0 sim:/top_tb/uut/rx_gen[0]/ifei4_rx/i_fei4_rx_core/*}
     add wave -group tlu sim:/top_tb/uut/tlu_controller_module/i_tlu_controller/*
 
@@ -83,14 +95,14 @@ proc wave_top {} {
     #add wave -group fei4_cmd sim:/top_tb/fei4_inst/i_CMD/*
     #add wave -group fei4_dob sim:/top_tb/fei4_inst/i_DOB/*
     #add wave -group fei4_eochl sim:/top_tb/fei4_inst/i_EOCHL/*
-    
+
     #add wave -group fei4_c0_r0 {sim:/top_tb/fei4_inst/i_digital_array/genblk1[0]/i_DDC/column_gen[0]/i_region_logic/*}
     #add wave -group fei4_c0_r0_core {sim:/top_tb/fei4_inst/i_digital_array/genblk1[0]/i_DDC/column_gen[0]/i_region_logic/quad_core/*}
     #add wave -group fei4_darray {sim:/top_tb/fei4_inst/i_digital_array/*}
     #add wave -group fei4_aarray {sim:/top_tb/fei4_inst/i_FEND_DIGI_ARRAY/*}
     #add wave  -group fei4_aarray_0_0 {sim:/top_tb/fei4_inst/i_FEND_DIGI_ARRAY/genblk1[0]/DC_INST/genblk1[0]/PIX_L/*}
     #add wave -group fei4_c0_r0_m0 {sim:/top_tb/fei4_inst/i_digital_array/genblk1[0]/i_DDC/column_gen[0]/i_region_logic/quad_core/latency_mem[0]/mem/*}
-    
+
 }
 
 
