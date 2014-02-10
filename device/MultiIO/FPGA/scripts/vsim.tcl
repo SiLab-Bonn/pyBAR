d2a04a1e060d267af444c1f4f64d13ee427dc982
Jens Janssen <janssen@physik.uni-bonn.de>
2014-02-10 09:50:39 +0000

    MAINT: cleanup

--- device/MultiIO/FPGA/scripts/vsim.tcl
+++ device/MultiIO/FPGA/scripts/vsim.tcl
@@ -1,18 +1,18 @@
 # ----------------------------------------------------------------
 # TO RUN (in work directory):
 # source vsim.tcl
-# vlog_libs (once)
+# vlog_libs (needed once)
 # vlog_top
 # vsim_top
 # wave_top
-# (restart)
+# restart (to clear waveforms)
 # run 1ms
 # ----------------------------------------------------------------
 
 # ----------------------------------------------------------------
-# set system envioremtn XILINX to ISE directory
-# or set XILINX "C:/Xilinx/14.6/ISE_DS"
+# set system environment XILINX to ISE directory
 # ----------------------------------------------------------------
+#set XILINX "C:/Xilinx/14.6/ISE_DS/ISE"
 set XILINX   $env(XILINX)
 
 # ----------------------------------------------------------------
@@ -26,12 +26,15 @@ set BASIL "../../../../../../basil"
 set FEI4 "../../../../../../fei4/trunk/models/fei4a"
 
 # ----------------------------------------------------------------
-# run Xilinx->EDK->Compile Simulation Librarie
+# run Xilinx->EDK->Compile Simulation libraries
 # copy latest C:\Xilinx\xx.x\ISE_DS\EDK\modelsim.ini to work folder
 # ----------------------------------------------------------------
 
 # ----------------------------------------------------------------
 #map liblaries - this can be different on other platforms than Windows
+#vmap unisims_ver $XILINX/EDK/unisims_ver
+#vmap simprims_ver $XILINX/EDK/simprims_ver
+# or
 #vmap unisims_ver $XILINX/ISE/verilog/questasim/10.1c/lin64/unisims_ver
 #vmap simprims_ver $XILINX/ISE/verilog/questasim/10.1c/lin64/simprims_ver 
 # ----------------------------------------------------------------
@@ -43,7 +46,7 @@ proc vlog_libs {} {
     vlog -work simprims_ver $XILINX/verilog/src/simprims/*.v
 
     vlib unisims_ver
-    vlog -work unisims_ver $XILINX/verilog/src/unisims/*.v    
+    vlog -work unisims_ver $XILINX/verilog/src/unisims/*.v
 }
 
 proc vlog_top {} {
@@ -79,7 +82,7 @@ proc vlog_top {} {
 
 proc vsim_top {} {
     vsim -novopt -t 1ps -L unisims_ver  work.top_tb glbl
-    #vsim -novopt -t 1ps -L unisims_ver  -L simprims_ver work.top_tb glbl 
+    #vsim -novopt -t 1ps -L unisims_ver  -L simprims_ver work.top_tb glbl
 }
 
 proc wave_top {} {
@@ -90,7 +93,6 @@ proc wave_top {} {
     add wave -group cmd sim:/top_tb/uut/icmd/i_cmd_seq_core/*
     add wave -group fifo_sram sim:/top_tb/uut/i_out_fifo/i_sram_fifo/*
 
-    
     add wave -group fei4_rx_0 {sim:/top_tb/uut/rx_gen[0]/ifei4_rx/i_fei4_rx_core/*}
     add wave -group tlu sim:/top_tb/uut/tlu_controller_module/i_tlu_controller/*
 
@@ -107,9 +109,8 @@ proc wave_top {} {
     #add wave -group fei4_c0_r0_core {sim:/top_tb/fei4_inst/i_digital_array/genblk1[0]/i_DDC/column_gen[0]/i_region_logic/quad_core/*}
     #add wave -group fei4_darray {sim:/top_tb/fei4_inst/i_digital_array/*}
     #add wave -group fei4_aarray {sim:/top_tb/fei4_inst/i_FEND_DIGI_ARRAY/*}
-    #add wave  -group fei4_aarray_0_0 {sim:/top_tb/fei4_inst/i_FEND_DIGI_ARRAY/genblk1[0]/DC_INST/genblk1[0]/PIX_L/*}
+    #add wave -group fei4_aarray_0_0 {sim:/top_tb/fei4_inst/i_FEND_DIGI_ARRAY/genblk1[0]/DC_INST/genblk1[0]/PIX_L/*}
     #add wave -group fei4_c0_r0_m0 {sim:/top_tb/fei4_inst/i_digital_array/genblk1[0]/i_DDC/column_gen[0]/i_region_logic/quad_core/latency_mem[0]/mem/*}
 
 }
-
 
