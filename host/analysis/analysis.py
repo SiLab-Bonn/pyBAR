822d86ae69f8fcdeca66b964081fcd64ef782b8a
DavidLP <pohl@physik.uni-bonn.de>
2014-02-14 14:56:15 +0000

    ENH: new function analyze_cluster_size_per_scan_parameter

--- host/analysis/analysis.py
+++ host/analysis/analysis.py
@@ -191,7 +191,7 @@ def analyse_n_cluster_per_event(scan_base, combine_n_readouts=1000, max_chunk_si
     plotting.plot_scatter_time(x, y, title='Number of cluster per event as a function of time', filename=output_pdf, legend=('0 cluster', '1 cluster', '2 cluster', '3 cluster'))
 
 
-def select_hits_from_cluster_info(input_file_hits, output_file_hits, cluster_size_condition, n_cluster_condition):
+def select_hits_from_cluster_info(input_file_hits, output_file_hits, cluster_size_condition, n_cluster_condition, output_pdf=None, **kwarg):
     ''' Takes a hit table and stores only selected hits into a new table. The selection is done on an event base and events are selected if they have a certain number of cluster or cluster size.
     To increase the analysis speed a event index for the input hit file is created first.
 
@@ -223,7 +223,7 @@ def select_hits_from_cluster_info(input_file_hits, output_file_hits, cluster_siz
             in_hit_file_h5.root.meta_data.copy(out_hit_file_h5.root)  # copy meta_data note to new file
 
 
-def histogram_tdc_hits(scan_bases):
+def histogram_tdc_hits(scan_bases, output_pdf=None, **kwarg):
     ''' This method takes the interpreted hit table, selects events with only one hit with one hit cluster and histograms the TDC info for these hits.
     The created TDC histogram is stored in a hdf5 file.
 
@@ -253,6 +253,116 @@ def histogram_tdc_hits(scan_bases):
             tdc_hist_per_pixel = coo_matrix((np.ones(shape=(len(pixel,)), dtype=np.uint8), (pixel, hits[:]['TDC'])), shape=(80 * 336, 4096)).todense()  # use sparse matrix to keep memory usage decend
             tdc_hist_array = out_file_histograms_h5.createCArray(out_file_histograms_h5.root, name='HistTdc', title='TDC Histograms', atom=tb.Atom.from_dtype(tdc_hist_per_pixel.dtype), shape=tdc_hist_per_pixel.shape, filters=tb.Filters(complib='blosc', complevel=5, fletcher32=False))
             tdc_hist_array[:] = tdc_hist_per_pixel
+
+
+def analyze_cluster_size_per_scan_parameter(scan_bases, output_file_cluster_size, parameter='GDAC', max_chunk_size=10000000, overwrite_output_files=False, output_pdf=None, **kwarg):
+    ''' This method takes multiple hit files and determines the cluster size for different scan parameter values of
+
+     Parameters
+    ----------
+    scan_bases: list of str
+        has the scan base names (e.g.:  ['//data//SCC_50_fei4_self_trigger_scan_390', ]
+    output_file_cluster_size: str
+        The data file with the results
+    parameter: int
+        The name of the parameter to separate the data into (e.g.: PlsrDAC)
+    max_chunk_size: int
+        the maximum chink size used during read, if too big memory error occurs, if too small analysis takes longer
+    overwrite_output_files: bool
+        Set to true to overwrite the output file if it already exists
+    output_pdf: PdfPages
+        PdfPages file object, if none the plot is printed to screen
+    '''
+    logging.info('Analyze the cluster sizes for different ' + parameter + ' settings for ' + str(len(scan_bases)) + ' different files')
+    if os.path.isfile(output_file_cluster_size) and not overwrite_output_files:  # skip analysis if already done
+            logging.info('Analyzed cluster size file ' + output_file_cluster_size + ' already exists. Skip cluster size analysis.')
+    else:
+        with tb.openFile(output_file_cluster_size, mode="w") as out_file_h5:  # file to write the data into
+            filter_table = tb.Filters(complib='blosc', complevel=5, fletcher32=False)  # compression of the written data
+            parameter_goup = out_file_h5.createGroup(out_file_h5.root, parameter, title=parameter)  # note to store the data
+            cluster_size_total = None  # final array for the cluster size per GDAC
+            for index in range(0, len(scan_bases)):  # loop over all hit files
+                with tb.openFile(scan_bases[index], mode="r+") as in_hit_file_h5:  # open the actual hit file
+                    meta_data_array = in_hit_file_h5.root.meta_data[:]
+                    scan_parameter = analysis_utils.get_scan_parameter(meta_data_array)  # get the scan parameters
+                    if scan_parameter:  # if a GDAC scan parameter was used analyze the cluster size per GDAC setting
+                        scan_parameter_values = scan_parameter.itervalues().next()  # scan parameter settings used
+                        if len(scan_parameter_values) == 1:  # only analyze per scan step if there are more than one scan step
+                            logging.info('Extract from ' + scan_bases[index] + ' the cluster size for ' + parameter + ' = ' + str(scan_parameter_values[0]))
+                            cluster_size_hist = in_hit_file_h5.root.HistClusterSize[:]
+                            plotting.plot_cluster_size(hist=cluster_size_hist, title='Cluster size (' + str(np.sum(cluster_size_hist)) + ' entries) for ' + parameter + ' =' + str(scan_parameter_values[0]), filename=output_pdf)
+                            if cluster_size_total is None:  # true if no data was appended to the array yet
+                                cluster_size_total = cluster_size_hist
+                            else:
+                                cluster_size_total = np.vstack([cluster_size_total, cluster_size_hist])
+                            actual_parameter_group = out_file_h5.createGroup(parameter_goup, name=parameter + '_' + str(scan_parameter_values[0]), title=parameter + '_' + str(scan_parameter_values[0]))
+                            cluster_size_hist_table = out_file_h5.createCArray(actual_parameter_group, name='HistClusterSize', title='Cluster Size Histogram', atom=tb.Atom.from_dtype(cluster_size_hist.dtype), shape=cluster_size_hist.shape, filters=filter_table)
+                            cluster_size_hist_table[:] = cluster_size_hist
+                        else:
+                            logging.info('Analyze ' + scan_bases[index] + ' per scan parameter ' + parameter + ' for ' + str(len(scan_parameter_values)) + ' values from ' + str(np.amin(scan_parameter_values)) + ' to ' + str(np.amax(scan_parameter_values)))
+                            event_numbers = analysis_utils.get_meta_data_at_scan_parameter(meta_data_array, parameter)['event_number']  # get the event numbers in meta_data where the scan parameter changes
+                            parameter_ranges = np.column_stack((scan_parameter_values, analysis_utils.get_event_range(event_numbers)))
+                            hit_table = in_hit_file_h5.root.Hits
+                            analysis_utils.index_event_number(hit_table)
+                            total_hits = 0
+                            total_hits_2 = 0
+                            index = 0  # index where to start the read out, 0 at the beginning
+                            chunk_size = max_chunk_size
+                            # initialize the analysis and set settings
+                            analyze_data = AnalyzeRawData()
+                            analyze_data.create_cluster_size_hist = True
+                            analyze_data.create_cluster_tot_hist = True
+                            analyze_data.histograming.set_no_scan_parameter()  # one has to tell the histogramer the # of scan parameters for correct occupancy hist allocation
+                            for parameter_index, parameter_range in enumerate(parameter_ranges):  # loop over the selected events
+                                analyze_data.reset()  # resets the data of the last analysis
+                                logging.info('Analyze GDAC = ' + str(parameter_range[0]) + ' ' + str(int(float(float(parameter_index) / float(len(parameter_ranges)) * 100.))) + '%')
+                                start_event_number = parameter_range[1]
+                                stop_event_number = parameter_range[2]
+                                logging.info('Data from events = [' + str(start_event_number) + ',' + str(stop_event_number) + '[')
+                                actual_parameter_group = out_file_h5.createGroup(parameter_goup, name=parameter + '_' + str(parameter_range[0]), title=parameter + '_' + str(parameter_range[0]))
+                                # loop over the hits in the actual selected events with optimizations: variable chunk size, start word index given
+                                readout_hit_len = 0  # variable to calculate a optimal chunk size value from the number of hits for speed up
+                                for hits, index in analysis_utils.data_aligned_at_events(hit_table, start_event_number=start_event_number, stop_event_number=stop_event_number, start=index, chunk_size=chunk_size):
+                                    total_hits += hits.shape[0]
+                                    analyze_data.analyze_hits(hits)  # analyze the selected hits in chunks
+                                    readout_hit_len += hits.shape[0]
+                                chunk_size = int(1.05 * readout_hit_len) if int(1.05 * readout_hit_len) < max_chunk_size else max_chunk_size  # to increase the readout speed, estimated the number of hits for one read instruction
+                                if chunk_size < 50:  # limit the lower chunk size, there can always be a crazy event with more than 20 hits
+                                    chunk_size = 50
+                                # get occupancy hist
+                                occupancy = np.zeros(80 * 336 * analyze_data.histograming.get_n_parameters(), dtype=np.uint32)  # create linear array as it is created in histogram class
+                                analyze_data.histograming.get_occupancy(occupancy)  # just here to check histograming is consistend
+
+                                # store and plot cluster size hist
+                                cluster_size_hist = np.zeros(1024, dtype=np.uint32)
+                                analyze_data.clusterizer.get_cluster_size_hist(cluster_size_hist)
+                                cluster_size_hist_table = out_file_h5.createCArray(actual_parameter_group, name='HistClusterSize', title='Cluster Size Histogram', atom=tb.Atom.from_dtype(cluster_size_hist.dtype), shape=cluster_size_hist.shape, filters=filter_table)
+                                cluster_size_hist_table[:] = cluster_size_hist
+                                plotting.plot_cluster_size(hist=cluster_size_hist, title='Cluster size (' + str(np.sum(cluster_size_hist)) + ' entries) for ' + parameter + ' = ' + str(scan_parameter_values[parameter_index]), filename=output_pdf)
+                                if cluster_size_total is None:  # true if no data was appended to the array yet
+                                    cluster_size_total = cluster_size_hist
+                                else:
+                                    cluster_size_total = np.vstack([cluster_size_total, cluster_size_hist])
+
+                                total_hits_2 += np.sum(occupancy)
+
+                            if total_hits != total_hits_2:
+                                logging.warning('Analysis shows inconsistent number of hits. Check needed!')
+                            logging.info('Analyzed %d hits!' % total_hits)
+                    else:  # no scan parameter is given, therefore the data file contains hits of only one GDAC setting and no analysis is necessary
+                        parameter_value = analysis_utils.get_parameter_value_from_file_names([scan_bases[index]], parameter).keys()[0]  # get the parameter value from the file name
+                        logging.info('Extract from ' + scan_bases[index] + ' the cluster size for ' + parameter + ' = ' + str(parameter_value))
+                        cluster_size_hist = in_hit_file_h5.root.HistClusterSize[:]
+                        plotting.plot_cluster_size(hist=cluster_size_hist, title='Cluster size (' + str(np.sum(cluster_size_hist)) + ' entries) for ' + parameter + ' =' + str(parameter_value), filename=output_pdf)
+                        if cluster_size_total is None:  # true if no data was appended to the array yet
+                            cluster_size_total = cluster_size_hist
+                        else:
+                            cluster_size_total = np.vstack([cluster_size_total, cluster_size_hist])
+                        actual_parameter_group = out_file_h5.createGroup(parameter_goup, name=parameter + '_' + str(parameter_value), title=parameter + '_' + str(parameter_value))
+                        cluster_size_hist_table = out_file_h5.createCArray(actual_parameter_group, name='HistClusterSize', title='Cluster Size Histogram', atom=tb.Atom.from_dtype(cluster_size_hist.dtype), shape=cluster_size_hist.shape, filters=filter_table)
+                        cluster_size_hist_table[:] = cluster_size_hist
+            cluster_size_total_out = out_file_h5.createCArray(out_file_h5.root, name='AllHistClusterSize', title='All Cluster Size Histograms', atom=tb.Atom.from_dtype(cluster_size_total.dtype), shape=cluster_size_total.shape, filters=filter_table)
+            cluster_size_total_out[:] = cluster_size_total
 
 
 if __name__ == "__main__":
