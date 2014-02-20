54ed04cf9f8f5f90f6d1ac0edbffb8c9f027aa5f
DavidLP <pohl@physik.uni-bonn.de>
2014-02-20 15:42:23 +0000

    ENH: new select_hits_for_tdc_info method

--- host/analysis/analysis.py
+++ host/analysis/analysis.py
@@ -257,6 +257,35 @@ def select_hits_from_cluster_info(input_file_hits, output_file_hits, cluster_siz
             in_hit_file_h5.root.meta_data.copy(out_hit_file_h5.root)  # copy meta_data note to new file
 
 
+def select_hits_for_tdc_info(input_file_hits, output_file_hits, cluster_size_condition, n_cluster_condition, output_pdf=None, **kwarg):
+    ''' Takes a hit table and stores only hits that have a meaningful tdc info. The tdc info can be used if only one hit occurred in the event with one tdc word.
+
+     Parameters
+    ----------
+    input_file_hits: str
+        the input file name with hits
+    output_file_hits: str
+        the output file name for the hits
+    '''
+    logging.info('Write hits of events from ' + str(input_file_hits) + ' with ' + cluster_size_condition + ' and ' + n_cluster_condition + ' into ' + str(output_file_hits))
+    with tb.openFile(input_file_hits, mode="r+") as in_hit_file_h5:
+        analysis_utils.index_event_number(in_hit_file_h5.root.Hits)
+        analysis_utils.index_event_number(in_hit_file_h5.root.Cluster)
+        with tb.openFile(output_file_hits, mode="w") as out_hit_file_h5:
+            hit_table_out = out_hit_file_h5.createTable(out_hit_file_h5.root, name='Hits', description=data_struct.HitInfoTable, title='hit_data', filters=tb.Filters(complib='blosc', complevel=5, fletcher32=False))
+            cluster_table = in_hit_file_h5.root.Cluster
+            last_word_number = 0
+            for data, _ in analysis_utils.data_aligned_at_events(cluster_table):
+                selected_events_1 = analysis_utils.get_events_with_cluster_size(event_number=data['event_number'], cluster_size=data['size'], condition='cluster_size==1')  # select the events with only 1 hit cluster
+                selected_events_2 = analysis_utils.get_events_with_n_cluster(event_number=data['event_number'], condition='n_cluster==1')  # select the events with only 1 cluster
+                selected_events_3 = analysis_utils.get_events_with_error_code(event_number=data['event_number'], event_status=data['event_status'], select_mask=0b0000011100000000, condition=0b0000000100000000)  # select events with one tdc word and no tdc overflow
+                selected_events = selected_events_1[analysis_utils.in1d_sorted(selected_events_1, selected_events_2)]  # select events with the first two conditions above
+                selected_events = selected_events[analysis_utils.in1d_sorted(selected_events, selected_events_3)]  # select events with all conditions above
+                logging.info('Selected ' + str(len(selected_events)) + ' events matching the TDC conditions')
+                last_word_number = analysis_utils.write_hits_in_events(hit_table_in=in_hit_file_h5.root.Hits, hit_table_out=hit_table_out, events=selected_events, start_hit_word=last_word_number)  # write the hits of the selected events into a new table
+            in_hit_file_h5.root.meta_data.copy(out_hit_file_h5.root)  # copy meta_data note to new file
+
+
 def histogram_tdc_hits(scan_bases, output_pdf=None, **kwarg):
     ''' This method takes the interpreted hit table, selects events with only one hit with one hit cluster and histograms the TDC info for these hits.
     The created TDC histogram is stored in a hdf5 file.
