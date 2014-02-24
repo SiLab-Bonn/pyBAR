91fc2fd0a5e5ce5497d1c5efade7a92ca5e63dbc
DavidLP <pohl@physik.uni-bonn.de>
2014-02-24 08:58:11 +0000

    ENH: info output of a more detailed option is now set correctly; e.g. debug out also sets info out

--- host/analysis/RawDataConverter/Basis.h
+++ host/analysis/RawDataConverter/Basis.h
@@ -29,16 +29,16 @@ public:
 	//helper functions
 	bool fileExists(const std::string& pFileName);		//check if a file exists
 	double StrToDouble(std::string const& pValue);		//converts a std::string to a double
-	int StrToInt(std::string const& pValue);			    //converts a std::string to a int
+	int StrToInt(std::string const& pValue);			//converts a std::string to a int
 	std::string IntToStr(unsigned int const& pValue);	//converts a int to a std::string
 	std::string DoubleToStr(double const& pValue);		//converts a double to a std::string
-	std::string IntToBin(unsigned int pValue);			  //converts an unsigned int to a binary string
-	bool isInf(double pValue);							          //checks if the value is infinite
-	bool isNan(double pValue);							          //checks if the value is not a number
-	bool isFinite(double pValue);						          //check if the value is neither NaN nor Inf
+	std::string IntToBin(unsigned int pValue);			//converts an unsigned int to a binary string
+	bool isInf(double pValue);							//checks if the value is infinite
+	bool isNan(double pValue);							//checks if the value is not a number
+	bool isFinite(double pValue);						//check if the value is neither NaN nor Inf
 	bool getStringSeparated(std::string pLine, std::string pSeparator, std::string& pLeft, std::string& pRight);
 
-	void setBugReport(bool pCreateReport = true);		  //activates the trace back output
+	void setBugReport(bool pCreateReport = true);		  		//activates the trace back output
 	void setBugReportFileName(std::string pBugReportFileName);	//set the file name for the trace back
 
 	virtual void setErrorOutput(bool pToggle = true);
@@ -61,11 +61,11 @@ protected:
 
 private:
 	std::string _sourceFileName;						//the file name of the cxx file
-	bool _error;										        //toggle error output on/off
-	bool _warning;										      //toggle warning output on/off
-	bool _info;											        //toggle info output on/off
-	bool _debug;										        //toggle debug output on/off
-	bool _debugReport;									    //toggle bug reprot on/off
-	std::string _bugReportFileName;				  //set bug report file name
+	bool _error;										//toggle error output on/off
+	bool _warning;										//toggle warning output on/off
+	bool _info;											//toggle info output on/off
+	bool _debug;										//toggle debug output on/off
+	bool _debugReport;									//toggle bug reprot on/off
+	std::string _bugReportFileName;				  		//set bug report file name
 };
 
