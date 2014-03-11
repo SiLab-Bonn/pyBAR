4395b2094eb8dbe9af4b3fa4e71cc6a9453d67fb
davidlp <davidlp@65a71841-4583-47e0-a5e0-152ae865ff8c>
2014-03-11 17:02:16 +0000

    ENH: two new functions getEventsInBothArrays, in1d_sorted

--- trunk/host/analysis/RawDataConverter/AnalysisFunctions.h
+++ trunk/host/analysis/RawDataConverter/AnalysisFunctions.h
@@ -10,6 +10,7 @@
 #include "Basis.h"
 #include "defines.h"
 
+// counts from the event number column of the cluster table how often a cluster occurs in every event
 unsigned int getNclusterInEvents(int64_t*& rEventNumber, const unsigned int& rSize, int64_t*& rResultEventNumber, unsigned int*& rResultCount)
 {
 	unsigned int tResultIndex = 0;
@@ -31,4 +32,51 @@ unsigned int getNclusterInEvents(int64_t*& rEventNumber, const unsigned int& rSi
 	rResultEventNumber[tResultIndex] = tLastValue;
 	return tResultIndex+1;
 }
+
+//takes two event arrays and calculates an intersection array of event numbers occurring in both arrays
+unsigned int getEventsInBothArrays(int64_t*& rEventArrayOne, const unsigned int& rSizeArrayOne, int64_t*& rEventArrayTwo, const unsigned int& rSizeArrayTwo, int64_t*& rEventArrayIntersection)
+{
+	int64_t tActualEventNumber = -1;
+	unsigned int tActualIndex = 0;
+	unsigned int tActualResultIndex = 0;
+	for (unsigned int i=0; i<rSizeArrayOne; ++i){  // loop over all event numbers in first array
+		if (rEventArrayOne[i] == tActualEventNumber)  // omit the same event number occuring again
+			continue;
+		tActualEventNumber = rEventArrayOne[i];
+		for(unsigned int j = tActualIndex; j<rSizeArrayTwo; ++j){
+			if (rEventArrayTwo[j] >= tActualEventNumber){
+				tActualIndex = j;
+				break;
+			}
+		}
+		if (rEventArrayTwo[tActualIndex] == tActualEventNumber){
+			rEventArrayIntersection[tActualResultIndex] = tActualEventNumber;
+			tActualResultIndex++;
+		}
+	}
+	return tActualResultIndex++;
+}
+
+//does the same as np.in1d but uses the fact that the arrays are sorted
+void in1d_sorted(int64_t*& rEventArrayOne, const unsigned int& rSizeArrayOne, int64_t*& rEventArrayTwo, const unsigned int& rSizeArrayTwo, uint8_t*& rSelection)
+{
+	rSelection[0] = true;
+	int64_t tActualEventNumber = -1;
+	unsigned int tActualIndex = 0;
+	for (unsigned int i=0; i<rSizeArrayOne; ++i){  // loop over all event numbers in first array
+		tActualEventNumber = rEventArrayOne[i];
+		for(unsigned int j = tActualIndex; j<rSizeArrayTwo; ++j){
+			if (rEventArrayTwo[j] >= tActualEventNumber){
+				tActualIndex = j;
+				break;
+			}
+		}
+		if (rEventArrayTwo[tActualIndex] == tActualEventNumber)
+			rSelection[i] = 1;
+		else
+			rSelection[i] = 0;
+	}
+}
+
+
 
