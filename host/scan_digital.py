f21ff44b1b36850cf959b260427846d82e7c7c1c
Jens Janssen <janssen@physik.uni-bonn.de>
2014-03-25 16:24:34 +0000

    MAINT:

--- host/scan_digital.py
+++ host/scan_digital.py
@@ -53,7 +53,7 @@ class DigitalScan(ScanBase):
             analyze_raw_data.create_tot_hist = False
             analyze_raw_data.interpret_word_table(fei4b=scan.register.fei4b)
             analyze_raw_data.plot_histograms(scan_data_filename=scan.scan_data_filename)
-#             analyze_raw_data.interpreter.print_summary()
+            analyze_raw_data.interpreter.print_summary()
 
 if __name__ == "__main__":
     import configuration
