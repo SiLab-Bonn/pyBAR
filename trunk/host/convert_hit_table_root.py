fa8119d49338b66d3aa592a6ce06e65c60501ba9
davidlp <davidlp@65a71841-4583-47e0-a5e0-152ae865ff8c>
2014-03-28 06:48:14 +0000

    BUG: adding missing tot info ENH: data struct has now the correct type

--- trunk/host/convert_hit_table_root.py
+++ trunk/host/convert_hit_table_root.py
@@ -7,43 +7,45 @@ from ROOT import TFile, TTree
 from ROOT import gROOT, AddressOf
 
 
-def convert_hit_table(input_filename, output_filename):
+def init_hit_struct():
     gROOT.ProcessLine(\
     "struct HitInfo{\
       ULong64_t event_number;\
       UInt_t trigger_number;\
-      UShort_t relative_BCID;\
+      UChar_t relative_BCID;\
       UShort_t LVL1ID;\
-      UShort_t column;\
+      UChar_t column;\
       UShort_t row;\
-      UShort_t tot;\
+      UChar_t tot;\
       UShort_t BCID;\
       UShort_t TDC;\
-      UShort_t trigger_status;\
+      UChar_t trigger_status;\
       UInt_t service_record;\
       UShort_t event_status;\
     };");
-
     from ROOT import HitInfo
+    return HitInfo()
 
+def convert_hit_table(input_filename, output_filename):
     with tb.open_file(input_filename, 'r') as in_file_h5:
         hits = in_file_h5.root.Hits
 
-        myHit = HitInfo()
+        myHit = init_hit_struct()
         out_file_root = TFile(output_filename, 'RECREATE')
         tree = TTree('Hits', 'Hits from PyBAR')
 
         tree.Branch('event_number', AddressOf(myHit, 'event_number'), 'event_number/L')
-        tree.Branch('trigger_number', AddressOf(myHit, 'trigger_number'), 'trigger_number/I')
-        tree.Branch('relative_BCID', AddressOf(myHit, 'relative_BCID'), 'relative_BCID/S')
-        tree.Branch('LVL1ID', AddressOf(myHit, 'LVL1ID'), 'LVL1ID/S')
-        tree.Branch('column', AddressOf(myHit, 'column'), 'column/S')
-        tree.Branch('row', AddressOf(myHit, 'row'), 'row/S')
-        tree.Branch('BCID', AddressOf(myHit, 'BCID'), 'BCID/S')
-        tree.Branch('TDC', AddressOf(myHit, 'TDC'), 'TDC/S')
-        tree.Branch('trigger_status', AddressOf(myHit, 'trigger_status'), 'trigger_status/S')
-        tree.Branch('service_record', AddressOf(myHit, 'service_record'), 'service_record/I')
-        tree.Branch('event_status', AddressOf(myHit, 'event_status'), 'event_status/S')
+        tree.Branch('trigger_number', AddressOf(myHit, 'trigger_number'), 'trigger_number/i')
+        tree.Branch('relative_BCID', AddressOf(myHit, 'relative_BCID'), 'relative_BCID/b')
+        tree.Branch('LVL1ID', AddressOf(myHit, 'LVL1ID'), 'LVL1ID/s')
+        tree.Branch('column', AddressOf(myHit, 'column'), 'column/b')
+        tree.Branch('row', AddressOf(myHit, 'row'), 'row/s')
+        tree.Branch('tot', AddressOf(myHit, 'tot'), 'tot/b')
+        tree.Branch('BCID', AddressOf(myHit, 'BCID'), 'BCID/s')
+        tree.Branch('TDC', AddressOf(myHit, 'TDC'), 'TDC/s')
+        tree.Branch('trigger_status', AddressOf(myHit, 'trigger_status'), 'trigger_status/b')
+        tree.Branch('service_record', AddressOf(myHit, 'service_record'), 'service_record/i')
+        tree.Branch('event_status', AddressOf(myHit, 'event_status'), 'event_status/s')
 
         progress_bar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.ETA()], maxval=hits.shape[0])
         progress_bar.start()
@@ -55,6 +57,7 @@ def convert_hit_table(input_filename, output_filename):
             myHit.LVL1ID = int(hit['LVL1ID'])
             myHit.column = int(hit['column'])
             myHit.row = int(hit['row'])
+            myHit.tot = int(hit['tot'])
             myHit.BCID = int(hit['BCID'])
             myHit.TDC = int(hit['TDC'])
             myHit.trigger_status = int(hit['trigger_status'])
@@ -69,4 +72,4 @@ def convert_hit_table(input_filename, output_filename):
 
 
 if __name__ == "__main__":
-    convert_hit_table('data\data_interpreted.h5', 'output.root')
+    convert_hit_table('test.h5', 'output.root')
