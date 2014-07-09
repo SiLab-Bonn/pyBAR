0d8242f4040b94c4829aaa6d22b12911a3a357f0
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2014-07-09 13:55:22 +0000

    MAINT: read config and settings from h5 file

--- trunk/host/scan_ext_trigger.py
+++ trunk/host/scan_ext_trigger.py
@@ -175,7 +175,6 @@ class ExtTriggerScan(ScanBase):
         from analysis.analyze_raw_data import AnalyzeRawData
         output_file = self.scan_data_filename + "_interpreted.h5"
         with AnalyzeRawData(raw_data_file=scan.scan_data_filename + ".h5", analyzed_data_file=output_file) as analyze_raw_data:
-            analyze_raw_data.interpreter.set_trig_count(self.register.get_global_register_value("Trig_Count"))
             analyze_raw_data.create_source_scan_hist = True
 #             analyze_raw_data.create_hit_table = True
             analyze_raw_data.create_cluster_size_hist = True
@@ -185,7 +184,7 @@ class ExtTriggerScan(ScanBase):
                 analyze_raw_data.create_tdc_hist = True  # histogram the hit TDC information
                 analyze_raw_data.interpreter.use_tdc_word(True)  # align events at the TDC word
             analyze_raw_data.interpreter.set_warning_output(False)
-            analyze_raw_data.interpret_word_table(fei4b=scan.register.fei4b)
+            analyze_raw_data.interpret_word_table()
             analyze_raw_data.interpreter.print_summary()
             analyze_raw_data.plot_histograms(scan_data_filename=scan.scan_data_filename)
 
