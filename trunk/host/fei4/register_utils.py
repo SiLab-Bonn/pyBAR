03be9c1e0d874b48ebe5e0d546cc59bb8d49eede
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2014-07-31 08:38:24 +0000

    MAINT: make_pixel_mask docstring

--- trunk/host/fei4/register_utils.py
+++ trunk/host/fei4/register_utils.py
@@ -209,7 +209,7 @@ class FEI4RegisterUtils(object):
         steps : int
             Number of mask steps, e.g. steps=3 (every third pixel is enabled), steps=336 (one pixel per column), steps=672 (one pixel per double column).
         shift : int
-            Shift mask by given value to the bottom (towards higher row numbers). From 0 to steps - 1.
+            Shift mask by given value to the bottom (towards higher row numbers). From 0 to (steps - 1).
         default : int
             Value of pixels that are not selected by the mask.
         value : int
