d2703d1a39829ea300763d86554941b60e48bab3
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2014-09-23 11:17:48 +0000

    MAINT: comment not used imports

--- trunk/host/scan_analog.py
+++ trunk/host/scan_analog.py
@@ -1,11 +1,11 @@
-from daq.readout import get_col_row_array_from_data_record_array, save_raw_data_from_data_dict_iterable, convert_data_array, data_array_from_data_dict_iterable, is_data_record, is_data_from_channel
-from analysis.plotting.plotting import plot_occupancy, make_occupancy_hist
+# from daq.readout import get_col_row_array_from_data_record_array, save_raw_data_from_data_dict_iterable, convert_data_array, data_array_from_data_dict_iterable, is_data_record, is_data_from_channel
+# from analysis.plotting.plotting import plot_occupancy, make_occupancy_hist
 from analysis.analyze_raw_data import AnalyzeRawData
 from fei4.register_utils import invert_pixel_mask
 from scan.scan import ScanBase
 from scan.scan_utils import scan_loop
 from scan.run_manager import RunManager
-import logging
+# import logging
 
 
 class AnalogScan(ScanBase):
