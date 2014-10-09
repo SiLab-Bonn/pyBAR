77d4ca14b11649d2166f328b486c06a9616a3aab
Jens Janssen <janssen@physik.uni-bonn.de>
2014-10-09 13:56:26 +0000

    SVN: splitting up into several files

---
+++ host/pybar/daq/fei4_record.py
@@ -0,0 +1,87 @@
+from collections import OrderedDict
+from basil.utils.BitLogic import BitLogic
+
+from pybar.daq.readout_utils import is_data_header, is_address_record, is_value_record, is_service_record, is_data_record
+
+
+class FEI4Record(object):
+    """Record Object
+
+    """
+    def __init__(self, data_word, chip_flavor):
+        self.record_rawdata = int(data_word)
+        self.record_dict = OrderedDict()
+        if not (self.record_rawdata & 0xF0000000):  # FE data
+            self.record_dict.update([('channel', (self.record_rawdata & 0x0F000000) >> 24)])
+            self.chip_flavor = str(chip_flavor).lower()
+            self.chip_flavors = ['fei4a', 'fei4b']
+            if self.chip_flavor not in self.chip_flavors:
+                raise KeyError('Chip flavor is not of type {}'.format(', '.join('\'' + flav + '\'' for flav in self.chip_flavors)))
+            self.record_word = BitLogic.from_value(value=self.record_rawdata, size=28)
+            if is_data_header(self.record_rawdata):
+                self.record_type = "DH"
+                if self.chip_flavor == "fei4a":
+                    self.record_dict.update([('start', self.record_word[23:19].tovalue()), ('header', self.record_word[18:16].tovalue()), ('flag', self.record_word[15:15].tovalue()), ('lvl1id', self.record_word[14:8].tovalue()), ('bcid', self.record_word[7:0].tovalue())])
+                elif self.chip_flavor == "fei4b":
+                    self.record_dict.update([('start', self.record_word[23:19].tovalue()), ('header', self.record_word[18:16].tovalue()), ('flag', self.record_word[15:15].tovalue()), ('lvl1id', self.record_word[14:10].tovalue()), ('bcid', self.record_word[9:0].tovalue())])
+            elif is_address_record(self.record_rawdata):
+                self.record_type = "AR"
+                self.record_dict.update([('start', self.record_word[23:19].tovalue()), ('header', self.record_word[18:16].tovalue()), ('type', self.record_word[15:15].tovalue()), ('address', self.record_word[14:0].tovalue())])
+            elif is_value_record(self.record_rawdata):
+                self.record_type = "VR"
+                self.record_dict.update([('start', self.record_word[23:19].tovalue()), ('header', self.record_word[18:16].tovalue()), ('value', self.record_word[15:0].tovalue())])
+            elif is_service_record(self.record_rawdata):
+                self.record_type = "SR"
+                if self.chip_flavor == "fei4a":
+                    self.record_dict.update([('start', self.record_word[23:19].tovalue()), ('header', self.record_word[18:16].tovalue()), ('code', self.record_word[15:10].tovalue()), ('counter', self.record_word[9:0].tovalue())])
+                elif self.chip_flavor == "fei4b":
+                    if self.record_word[15:10].tovalue() == 14:
+                        self.record_dict.update([('start', self.record_word[23:19].tovalue()), ('header', self.record_word[18:16].tovalue()), ('code', self.record_word[15:10].tovalue()), ('lvl1id[11:5]', self.record_word[9:3].tovalue()), ('bcid[12:10]', self.record_word[2:0].tovalue())])
+                    elif self.record_word[15:10].tovalue() == 15:
+                        self.record_dict.update([('start', self.record_word[23:19].tovalue()), ('header', self.record_word[18:16].tovalue()), ('code', self.record_word[15:10].tovalue()), ('skipped', self.record_word[9:0].tovalue())])
+                    elif self.record_word[15:10].tovalue() == 16:
+                        self.record_dict.update([('start', self.record_word[23:19].tovalue()), ('header', self.record_word[18:16].tovalue()), ('code', self.record_word[15:10].tovalue()), ('truncation flag', self.record_word[9:9].tovalue()), ('truncation counter', self.record_word[8:4].tovalue()), ('l1req', self.record_word[3:0].tovalue())])
+                    else:
+                        self.record_dict.update([('start', self.record_word[23:19].tovalue()), ('header', self.record_word[18:16].tovalue()), ('code', self.record_word[15:10].tovalue()), ('counter', self.record_word[9:0].tovalue())])
+            elif is_data_record(self.record_rawdata):
+                self.record_type = "DR"
+                self.record_dict.update([('column', self.record_word[23:17].tovalue()), ('row', self.record_word[16:8].tovalue()), ('tot1', self.record_word[7:4].tovalue()), ('tot2', self.record_word[3:0].tovalue())])
+            else:
+                self.record_type = "UNKNOWN FE WORD"
+                self.record_dict.update([('word', self.record_word.tovalue())])
+    #             raise ValueError('Unknown data word: ' + str(self.record_word.tovalue()))
+        else:
+            self.record_type = "OTHER DATA WORD"
+            self.record_dict.update([('word', self.record_word.tovalue())])
+
+    def __len__(self):
+        return len(self.record_dict)
+
+    def __getitem__(self, key):
+        if not (isinstance(key, (int, long)) or isinstance(key, basestring)):
+            raise TypeError()
+        try:
+            return self.record_dict[key.lower()]
+        except TypeError:
+            return self.record_dict[self.record_dict.iterkeys()[int(key)]]
+
+    def next(self):
+        return self.record_dict.iteritems().next()
+
+    def __iter__(self):
+        return self.record_dict.iteritems()
+
+    def __eq__(self, other):
+        try:
+            return self.record_type.lower() == other.lower()
+        except:
+            try:
+                return self.record_type == other.record_type
+            except:
+                return False
+
+    def __str__(self):
+        return self.record_type + ' {}'.format(' '.join(key + ':' + str(val) for key, val in self.record_dict.iteritems()))
+
+    def __repr__(self):
+        return repr(self.__str__())
--- host/pybar/daq/readout_utils.py
+++ host/pybar/daq/readout_utils.py
@@ -1,6 +1,342 @@
 import logging
 import numpy as np
-import readout
+
+
+def convert_data_array(array, filter_func=None, converter_func=None):  # TODO: add copy parameter, otherwise in-place
+    '''Filter and convert raw data numpy array (numpy.ndarray)
+
+    Parameters
+    ----------
+    array : numpy.array
+        Raw data array.
+    filter_func : function
+        Function that takes array and returns true or false for each item in array.
+    converter_func : function
+        Function that takes array and returns an array or tuple of arrays.
+
+    Returns
+    -------
+    data_array : numpy.array
+        Data numpy array of specified dimension (converter_func) and content (filter_func)
+    '''
+#     if filter_func != None:
+#         if not hasattr(filter_func, '__call__'):
+#             raise ValueError('Filter is not callable')
+    if filter_func:
+        array = array[filter_func(array)]
+#     if converter_func != None:
+#         if not hasattr(converter_func, '__call__'):
+#             raise ValueError('Converter is not callable')
+    if converter_func:
+        array = converter_func(array)
+    return array
+
+
+def convert_data_iterable(data_iterable, filter_func=None, converter_func=None):  # TODO: add concatenate parameter
+    '''Convert raw data in data iterable.
+
+    Parameters
+    ----------
+    data_iterable : iterable
+        Iterable where each element is a tuple with following content: (raw data, timestamp_start, timestamp_stop, status).
+    filter_func : function
+        Function that takes array and returns true or false for each item in array.
+    converter_func : function
+        Function that takes array and returns an array or tuple of arrays.
+
+    Returns
+    -------
+    data_list : list
+        Data list of the form [(converted data, timestamp_start, timestamp_stop, status), (...), ...]
+    '''
+    data_list = []
+    for item in data_iterable:
+        data_list.append((convert_data_array(item[0], filter_func=filter_func, converter_func=converter_func), item[1], item[2], item[3]))
+    return data_list
+
+
+def data_array_from_data_iterable(data_iterable):
+    '''Convert data iterable to raw data numpy array.
+
+    Parameters
+    ----------
+    data_iterable : iterable
+        Iterable where each element is a tuple with following content: (raw data, timestamp_start, timestamp_stop, status).
+
+    Returns
+    -------
+    data_array : numpy.array
+        concatenated data array
+    '''
+    data_array = np.concatenate([item[0] for item in data_iterable])
+    return data_array
+
+
+def is_data_from_channel(channel=4):  # function factory
+    '''Select data from channel
+
+    Parameters:
+    channel : int
+        Channel number (4 is default channel on Single Chip Card)
+
+    Returns:
+    Function
+
+    Usage:
+    # 1
+    is_data_from_channel_4 = is_data_from_channel(4)
+    data_from_channel_4 = data_array[is_data_from_channel_4(data_array)]
+    # 2
+    filter_func = logical_and(is_data_record, is_data_from_channel(3))
+    data_record_from_channel_3 = data_array[filter_func(data_array)]
+    # 3
+    is_raw_data_from_channel_3 = is_data_from_channel(3)(raw_data)
+
+    Similar to:
+    f_ch3 = functoools.partial(is_data_from_channel, channel=3)
+    l_ch4 = lambda x: is_data_from_channel(x, channel=4)
+
+    Note:
+    Trigger data not included
+    '''
+    if channel > 0 and channel < 5:
+        def f(value):
+            return np.equal(np.right_shift(np.bitwise_and(value, 0x7F000000), 24), channel)
+        f.__name__ = "is_data_from_channel_" + str(channel)  # or use inspect module: inspect.stack()[0][3]
+        return f
+    else:
+        raise ValueError('Invalid channel number')
+
+
+def logical_and(f1, f2):  # function factory
+    '''Logical and from functions.
+
+    Parameters
+    ----------
+    f1, f2 : function
+        Function that takes array and returns true or false for each item in array.
+
+    Returns
+    -------
+    Function
+
+    Examples
+    --------
+    filter_func=logical_and(is_data_record, is_data_from_channel(4))  # new filter function
+    filter_func(array) # array that has Data Records from channel 4
+    '''
+    def f(value):
+        return np.logical_and(f1(value), f2(value))
+    f.__name__ = f1.__name__ + "_and_" + f2.__name__
+    return f
+
+
+def logical_or(f1, f2):  # function factory
+    '''Logical or from functions.
+
+    Parameters
+    ----------
+    f1, f2 : function
+        Function that takes array and returns true or false for each item in array.
+
+    Returns
+    -------
+    Function
+    '''
+    def f(value):
+        return np.logical_or(f1(value), f2(value))
+    f.__name__ = f1.__name__ + "_or_" + f2.__name__
+    return f
+
+
+def logical_not(f):  # function factory
+    '''Logical not from functions.
+
+    Parameters
+    ----------
+    f1, f2 : function
+        Function that takes array and returns true or false for each item in array.
+
+    Returns
+    -------
+    Function
+    '''
+    def f(value):
+        return np.logical_not(f(value))
+    f.__name__ = "not_" + f.__name__
+    return f
+
+
+def logical_xor(f1, f2):  # function factory
+    '''Logical xor from functions.
+
+    Parameters
+    ----------
+    f1, f2 : function
+        Function that takes array and returns true or false for each item in array.
+
+    Returns
+    -------
+    Function
+    '''
+    def f(value):
+        return np.logical_xor(f1(value), f2(value))
+    f.__name__ = f1.__name__ + "_xor_" + f2.__name__
+    return f
+
+
+def is_fe_record(value):
+    return not is_trigger_data(value) and not is_status_data(value)
+
+
+def is_data_header(value):
+    return np.equal(np.bitwise_and(value, 0x00FF0000), 0b111010010000000000000000)
+
+
+def is_address_record(value):
+    return np.equal(np.bitwise_and(value, 0x00FF0000), 0b111010100000000000000000)
+
+
+def is_value_record(value):
+    return np.equal(np.bitwise_and(value, 0x00FF0000), 0b111011000000000000000000)
+
+
+def is_service_record(value):
+    return np.equal(np.bitwise_and(value, 0x00FF0000), 0b111011110000000000000000)
+
+
+def is_data_record(value):
+    return np.logical_and(np.logical_and(np.less_equal(np.bitwise_and(value, 0x00FE0000), 0x00A00000), np.less_equal(np.bitwise_and(value, 0x0001FF00), 0x00015000)), np.logical_and(np.not_equal(np.bitwise_and(value, 0x00FE0000), 0x00000000), np.not_equal(np.bitwise_and(value, 0x0001FF00), 0x00000000)))
+
+
+def is_trigger_word(value):
+    return np.equal(np.bitwise_and(value, 0x80000000), 0b10000000000000000000000000000000)
+
+
+def is_tdc_word(value):
+    return np.equal(np.bitwise_and(value, 0x40000000), 0b01000000000000000000000000000000)
+
+
+def is_status_data(value):
+    '''Select status data
+    '''
+    return np.equal(np.bitwise_and(value, 0xFF000000), 0x00000000)
+
+
+def is_trigger_data(value):
+    '''Select trigger data (trigger number)
+    '''
+    return np.equal(np.bitwise_and(value, 0xFF000000), 0x80000000)
+
+
+def is_tdc_data(value):
+    '''Select tdc data
+    '''
+    return np.equal(np.bitwise_and(value, 0xF0000000), 0x40000000)
+
+
+def get_address_record_address(value):
+    '''Returns the address in the address record
+    '''
+    return np.bitwise_and(value, 0x0000EFFF)
+
+
+def get_address_record_type(value):
+    '''Returns the type in the address record
+    '''
+    return np.right_shift(np.bitwise_and(value, 0x00008000), 14)
+
+
+def get_value_record(value):
+    '''Returns the value in the value record
+    '''
+    return np.bitwise_and(value, 0x0000FFFF)
+
+# def def get_col_row_tot_array_from_data_record_array(max_tot=14):
+
+
+def get_col_row_tot_array_from_data_record_array(array):
+    '''Convert raw data array to column, row, and ToT array
+
+    Parameters
+    ----------
+    array : numpy.array
+        Raw data array.
+
+    Returns
+    -------
+    Tuple of arrays.
+    '''
+    def get_col_row_tot_1_array_from_data_record_array(value):
+        return np.right_shift(np.bitwise_and(value, 0x00FE0000), 17), np.right_shift(np.bitwise_and(value, 0x0001FF00), 8), np.right_shift(np.bitwise_and(value, 0x000000F0), 4)
+#         return (value & 0xFE0000)>>17, (value & 0x1FF00)>>8, (value & 0x0000F0)>>4 # numpy.vectorize()
+
+    def get_col_row_tot_2_array_from_data_record_array(value):
+        return np.right_shift(np.bitwise_and(value, 0x00FE0000), 17), np.add(np.right_shift(np.bitwise_and(value, 0x0001FF00), 8), 1), np.bitwise_and(value, 0x0000000F)
+#         return (value & 0xFE0000)>>17, ((value & 0x1FF00)>>8)+1, (value & 0x0000F) # numpy.vectorize()
+
+    col_row_tot_1_array = np.column_stack(get_col_row_tot_1_array_from_data_record_array(array))
+    col_row_tot_2_array = np.column_stack(get_col_row_tot_2_array_from_data_record_array(array))
+#     print col_row_tot_1_array, col_row_tot_1_array.shape, col_row_tot_1_array.dtype
+#     print col_row_tot_2_array, col_row_tot_2_array.shape, col_row_tot_2_array.dtype
+    # interweave array here
+    col_row_tot_array = np.vstack((col_row_tot_1_array.T, col_row_tot_2_array.T)).reshape((3, -1), order='F').T  # http://stackoverflow.com/questions/5347065/interweaving-two-numpy-arrays
+#     print col_row_tot_array, col_row_tot_array.shape, col_row_tot_array.dtype
+    # remove ToT > 14 (late hit, no hit) from array, remove row > 336 in case we saw hit in row 336 (no double hit possible)
+    try:
+        col_row_tot_array_filtered = col_row_tot_array[col_row_tot_array[:, 2] < 14]  # [np.logical_and(col_row_tot_array[:,2]<14, col_row_tot_array[:,1]<=336)]
+#         print col_row_tot_array_filtered, col_row_tot_array_filtered.shape, col_row_tot_array_filtered.dtype
+    except IndexError:
+        # logging.warning('Array is empty')
+        return np.array([], dtype=np.dtype('>u4')), np.array([], dtype=np.dtype('>u4')), np.array([], dtype=np.dtype('>u4'))
+    return col_row_tot_array_filtered[:, 0], col_row_tot_array_filtered[:, 1], col_row_tot_array_filtered[:, 2]  # column, row, ToT
+
+
+def get_col_row_array_from_data_record_array(array):
+    col, row, _ = get_col_row_tot_array_from_data_record_array(array)
+    return col, row
+
+
+def get_row_col_array_from_data_record_array(array):
+    col, row, _ = get_col_row_tot_array_from_data_record_array(array)
+    return row, col
+
+
+def get_tot_array_from_data_record_array(array):
+    _, _, tot = get_col_row_tot_array_from_data_record_array(array)
+    return tot
+
+
+def get_occupancy_mask_from_data_record_array(array, occupancy):
+    pass  # TODO:
+
+
+def get_col_row_iterator_from_data_records(array):  # generator
+    for item in np.nditer(array):  # , flags=['multi_index']):
+        yield np.right_shift(np.bitwise_and(item, 0x00FE0000), 17), np.right_shift(np.bitwise_and(item, 0x0001FF00), 8)
+        if np.not_equal(np.bitwise_and(item, 0x0000000F), 15):
+            yield np.right_shift(np.bitwise_and(item, 0x00FE0000), 17), np.add(np.right_shift(np.bitwise_and(item, 0x0001FF00), 8), 1)
+
+
+def get_row_col_iterator_from_data_records(array):  # generator
+    for item in np.nditer(array, flags=['multi_index']):
+        yield np.right_shift(np.bitwise_and(item, 0x0001FF00), 8), np.right_shift(np.bitwise_and(item, 0x00FE0000), 17)
+        if np.not_equal(np.bitwise_and(item, 0x0000000F), 15):
+            yield np.add(np.right_shift(np.bitwise_and(item, 0x0001FF00), 8), 1), np.right_shift(np.bitwise_and(item, 0x00FE0000), 17)
+
+
+def get_col_row_tot_iterator_from_data_records(array):  # generator
+    for item in np.nditer(array, flags=['multi_index']):
+        yield np.right_shift(np.bitwise_and(item, 0x00FE0000), 17), np.right_shift(np.bitwise_and(item, 0x0001FF00), 8), np.right_shift(np.bitwise_and(item, 0x000000F0), 4)  # col, row, ToT1
+        if np.not_equal(np.bitwise_and(item, 0x0000000F), 15):
+            yield np.right_shift(np.bitwise_and(item, 0x00FE0000), 17), np.add(np.right_shift(np.bitwise_and(item, 0x0001FF00), 8), 1), np.bitwise_and(item, 0x0000000F)  # col, row+1, ToT2
+
+
+def get_tot_iterator_from_data_records(array):  # generator
+    for item in np.nditer(array, flags=['multi_index']):
+        yield np.right_shift(np.bitwise_and(item, 0x000000F0), 4)  # ToT1
+        if np.not_equal(np.bitwise_and(item, 0x0000000F), 15):
+            yield np.bitwise_and(item, 0x0000000F)  # ToT2
 
 
 def interpret_pixel_data(data, dc, pixel_array, invert=True):
@@ -20,13 +356,13 @@ def interpret_pixel_data(data, dc, pixel_array, invert=True):
     '''
 
     # data validity cut, VR has to follow an AR
-    index_value = np.where(readout.is_address_record(data))[0] + 1  # assume value record follows address record
-    index_value = index_value[readout.is_value_record(data[index_value])]  # delete all non value records
+    index_value = np.where(is_address_record(data))[0] + 1  # assume value record follows address record
+    index_value = index_value[is_value_record(data[index_value])]  # delete all non value records
     index_address = index_value - 1  # calculate address record indices that are followed by an value record
 
     # create the pixel address/value arrays
-    address = readout.get_address_record_address(data[index_address])
-    value = readout.get_value_record(data[index_address + 1])
+    address = get_address_record_address(data[index_address])
+    value = get_value_record(data[index_address + 1])
 
     # split array for each bit in pixel data, split is done on decreasing address values
     address_split = np.array_split(address, np.where(np.diff(address.astype(np.int32)) < 0)[0] + 1)
