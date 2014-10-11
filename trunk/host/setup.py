1e56eb773d03738995a55820c75665e4e049888f
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2014-10-11 23:23:03 +0000

    MIANT: comment ENH: setup.py install working

--- trunk/host/setup.py
+++ trunk/host/setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
+# Installing package from sources:
+# python setup.py install
+# For developers (creating a link to the sources):
+# python setup.py develop
+#
+# Building source distribution:
+# python setup.py sdist
+# The generated source file is needed for installing by a tool like pip (pip install ...).
+
+
 # from distutils.core import setup
 from setuptools import setup
 from setuptools import find_packages
 
-# Building source distribution:
-# python setup.py sdist
-# The generated source file is needed for installing by a tool like pip.
 
 f = open('VERSION', 'r')
 version = f.readline().strip()
@@ -26,8 +33,9 @@ setup(
     maintainer=author,
     author_email=author_email,
     maintainer_email=author_email,
+    install_requires=['cython'],
     requires=['pySiLibUSB (>=1.0.0)', 'bitarray (>=0.8.1)', 'progressbar (>=2.4)', 'basil (>=2.0.0)'],
     packages=find_packages(),  # exclude=['*.tests', '*.test']),
     include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
-#     package_data={'': ['*.txt', 'VERSION'], 'docs': ['*'], 'examples': ['*'], 'pybar': ['*'], 'tests': ['*']},  # you do not need to use this option if you are using include_package_data
+    package_data={'': ['*.txt', 'VERSION'], 'docs': ['*'], 'examples': ['*'], 'pybar': ['*.yaml', '*.bit']},
 )
