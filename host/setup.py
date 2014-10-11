7bde2706eba5311d1b83cd5a954aadf997d274c6
Jens Janssen <janssen@physik.uni-bonn.de>
2014-10-11 21:03:55 +0000

    ENH: fix setup

--- host/setup.py
+++ host/setup.py
@@ -1,21 +1,29 @@
-#!/usr/bin/env python   
+#!/usr/bin/env python
 
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 from setuptools import find_packages
 
 f = open('VERSION', 'r')
-basil_version = f.readline().strip()
+version = f.readline().strip()
 f.close()
+
+author = 'Jens Janssen'
+author_email = 'janssen@physik.uni-bonn.de'
 
 setup(
     name='pyBAR',
-    version=basil_version,
+    version=version,
     description='pyBAR: Bonn ATLAS Readout in Pyhton',
     url='https://silab-redmine.physik.uni-bonn.de/projects/pybar',
     license='BSD 3-Clause ("BSD New" or "BSD Simplified") License',
     long_description='',
-    requires=['bitarray (>=0.8.1)', 'pySiLibUSB (>=1.0.0)', 'progressbar (>=2.3)', 'basil (>=2.0.0)'],
-    packages=find_packages(exclude=['*.tests', '*.test']),
-#    package_data={'': ['*.txt', 'VERSION'], 'docs': ['*'], 'examples': ['*']},  #  you do not need to use this option if you are using include_package_data
+    author=author,
+    maintainer=author,
+    author_email=author_email,
+    maintainer_email=author_email,
+    requires=['pySiLibUSB (>=1.0.0)', 'bitarray (>=0.8.1)', 'progressbar (>=2.4)', 'basil (>=2.0.0)'],
+    packages=find_packages(),  # exclude=['*.tests', '*.test']),
     include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
+#     package_data={'': ['*.txt', 'VERSION'], 'docs': ['*'], 'examples': ['*'], 'pybar': ['*'], 'tests': ['*']},  # you do not need to use this option if you are using include_package_data
 )
