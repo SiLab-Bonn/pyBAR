d63a8433810eca08c4be5b895088cae724806bea
jejan <jejan@65a71841-4583-47e0-a5e0-152ae865ff8c>
2014-10-13 09:08:41 +0000

    ENH: more comments

--- trunk/host/examples/primlist/primlist.py
+++ trunk/host/examples/primlist/primlist.py
@@ -2,7 +2,8 @@ from pybar.run_manager import RunManager  # importing run manager
 
 if __name__ == "__main__":
     runmngr = RunManager('../../pybar/configuration.yaml')  # loading configuration file, specifying hardware configuration and module configuration.
-    runmngr.run_primlist('primlist.plst')  # executing primlist.plst file
+    runmngr.run_primlist('primlist.plst')  # executing primlist.plst file, specific scan parameters are set inside the primlist file
+    #
     # After finishing the primlist: you will find the module data relative to the configuration.yaml file
     # If configuration.yaml is in /host/pybar/ the module data will be /host/pybar/<module_id> (where <module_id> is given from configuration.yaml)
     # After finishing the first scan, FE configuration can be commented out in configuration.yaml (for that use '#'):
@@ -19,4 +20,8 @@ if __name__ == "__main__":
     # fe_configuration:
     #     configuration : 5 # run number
     #     ...
+    #
+    # It is recommended to make copy of configuration.yaml for each module / specific hardware configuration.
+    #
+    # Feel free to modify / change the code :-)
     #
