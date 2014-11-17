aeee2656e3eb20c12aff9848e291e123af456fef
DavidLP <pohl@physik.uni-bonn.de>
2014-11-17 17:39:40 +0000

    BUG: correct naming of the meta table

--- host/pybar/analysis/RawDataConverter/data_struct.py
+++ host/pybar/analysis/RawDataConverter/data_struct.py
@@ -4,9 +4,12 @@ import numpy as np
 
 
 class MetaTable(tb.IsDescription):
-    start_index = tb.UInt32Col(pos=0)
-    stop_index = tb.UInt32Col(pos=1)
-    length = tb.UInt32Col(pos=2)
+    index_start = tb.UInt32Col(pos=0)
+#     start_index
+    index_stop = tb.UInt32Col(pos=1)
+#     stop_index
+    data_length = tb.UInt32Col(pos=2)
+#     length = tb.UInt32Col(pos=2)
     # https://github.com/PyTables/PyTables/issues/230
     #timestamp = tb.Time64Col(pos=3)
     timestamp = tb.Float64Col(pos=3)
