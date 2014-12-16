de60259e658e4dc4901be65ff8067feff009fa56
Jens Janssen <janssen@physik.uni-bonn.de>
2014-12-16 17:10:48 +0000

    SVN: adding properties ENH: adding license to file

--- device/MultiIO/FPGA/src/top.v
+++ device/MultiIO/FPGA/src/top.v
@@ -1,7 +1,24 @@
 /**
+ * This file is part of pyBAR.
+ * 
+ * pyBAR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * pyBAR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with pyBAR.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+ /**
  * ------------------------------------------------------------
  * Copyright (c) All rights reserved 
- * SiLab , Physics Institute of Bonn University , All Right 
+ * SiLab, Institute of Physics, University of Bonn
  * ------------------------------------------------------------
  *
  * SVN revision information:
