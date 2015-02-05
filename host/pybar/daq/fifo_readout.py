86481412439d8ebb989f7bd9b1bc2b4e0b75ff48
DavidLP <pohl@physik.uni-bonn.de>
2015-02-05 18:29:23 +0100

    BUG: changes to address bug report #3. Still needs crosscheck

--- host/pybar/daq/fifo_readout.py
+++ host/pybar/daq/fifo_readout.py
@@ -264,12 +264,12 @@ class FifoReadout(object):
     def reset_rx(self, channels=None):
         logging.info('Resetting RX')
         if channels:
-            filter(lambda channel: self.dut[channel]['SOFT_RESET'], channels)
+            filter(lambda channel: self.dut[channel]['RESET'], channels)
         else:
             if self.dut.name == 'usbpix' or self.dut.name == 'usbpix_sim':
-                filter(lambda channel: self.dut[channel]['SOFT_RESET'], ['rx_1', 'rx_2', 'rx_3', 'rx_4'])
+                filter(lambda channel: self.dut[channel]['RESET'], ['rx_1', 'rx_2', 'rx_3', 'rx_4'])
             elif self.dut.name == 'usbpix_gpac':
-                filter(lambda channel: self.dut[channel]['SOFT_RESET'], ['rx_fe'])
+                filter(lambda channel: self.dut[channel]['RESET'], ['rx_fe'])
         sleep(0.1)  # sleep here for a while
 
     def get_rx_sync_status(self, channels=None):
