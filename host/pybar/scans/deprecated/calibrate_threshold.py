eab1460cfee27ced4b7776bfab85896413633f92
DavidLP <david-leon.pohl@rub.de>
2015-02-10 11:20:25 +0100

    ENH: steps to fix analysis

--- host/pybar/scans/deprecated/calibrate_threshold.py
+++ host/pybar/scans/deprecated/calibrate_threshold.py
@@ -24,7 +24,7 @@ class ThresholdCalibration(FastThresholdScan):
     ''' Threshold calibration scan
     '''
     _default_run_conf = FastThresholdScan._default_run_conf
-    _default_run_conf['scan_parameters'] = [('PlsrDAC', (0, None)), ('GDAC', np.linspace(60., 80., num=2, dtype=np.uint32).tolist())]
+    _default_run_conf['scan_parameters'] = [('PlsrDAC', (0, None)), ('GDAC', np.linspace(60., 120., num=4, dtype=np.uint32).tolist())]
 #     _default_run_conf['scan_parameters'].extend([('GDAC', np.linspace(50., 100., num=2, dtype=np.uint32).tolist())])
     _default_run_conf.update({
         "ignore_columns": (1, 78, 79, 80),
@@ -33,9 +33,11 @@ class ThresholdCalibration(FastThresholdScan):
         "create_result_plots": True,
         "configuration_file": "K:\\pyBAR\\host\\data\\threshold_calibration",  # output file with the calibration data
         "overwrite_output_files": False,
+        "send_data": 'tcp://127.0.0.1:5678',  # send data to socket (e.g. for Online Monitor)
     })
 
     def scan(self):
+        return
         logging.info('Taking threshold data at following ' + self.scan_parameters._fields[1] + ' values: %s' % str(self.scan_parameters[1]))
 
         for index, parameter_value in enumerate(self.scan_parameters[1]):
@@ -57,7 +59,19 @@ class ThresholdCalibration(FastThresholdScan):
         self.raw_data_file.append_item(data, scan_parameters=self.scan_parameters._asdict(), new_file=True, flush=False,)
 
     def analyze(self):
-        pass
+        with AnalyzeRawData(raw_data_file=r'C:\Users\DavidLP\git\pyBAR\host\pybar\module_test\44_module_test_threshold_calibration.h5', create_pdf=True) as analyze_raw_data:
+            analyze_raw_data.create_hit_table = True
+            # Turn off all hit histograms; created for each GDAC setting later
+            analyze_raw_data.create_tot_hist = False
+            analyze_raw_data.create_tot_pixel_hist = False
+            analyze_raw_data.create_occupancy_hist = False
+            analyze_raw_data.interpreter.set_warning_output(False)  # so far the data structure in a threshold scan was always bad, too many warnings given
+            analyze_raw_data.interpret_word_table()
+            analyze_raw_data.interpreter.print_summary()
+            analyze_raw_data.plot_histograms()
+        
+        
+        
 #         def store_calibration_data_as_table(out_file_h5, mean_threshold_calibration, mean_threshold_rms_calibration, threshold_calibration):
 #             logging.info("Storing calibration data in a table...")
 #             filter_table = tb.Filters(complib='blosc', complevel=5, fletcher32=False)
@@ -76,11 +90,11 @@ class ThresholdCalibration(FastThresholdScan):
 #                 mean_threshold_calib_table.row['mean_threshold'] = mean_threshold_calibration[parameter_value_index]
 #                 mean_threshold_calib_table.row['threshold_rms'] = mean_threshold_rms_calibration[parameter_value_index]
 #                 mean_threshold_calib_table.row.append()
-# 
+#  
 #             threshold_calib_table.flush()
 #             mean_threshold_calib_table.flush()
 #             logging.info("done")
-# 
+#  
 #         def store_calibration_data_as_array(out_file_h5, mean_threshold_calibration, mean_threshold_rms_calibration, threshold_calibration):
 #             logging.info("Storing calibration data in an array...")
 #             filter_table = tb.Filters(complib='blosc', complevel=5, fletcher32=False)
@@ -91,32 +105,32 @@ class ThresholdCalibration(FastThresholdScan):
 #             mean_threshold_calib_rms_array[:] = mean_threshold_rms_calibration
 #             threshold_calib_array[:] = threshold_calibration
 #             logging.info("done")
-#     
+#      
 #         def create_calibration(scan_data_filenames, ignore_columns, fei4b=False):
 #             logging.info("Analyzing and plotting results...")
 #             output_h5_filename = calibration_configuration['configuration_file'] + '.h5'
 #             logging.info('Saving calibration in: %s' % output_h5_filename)
-#     
+#      
 #             if calibration_configuration['create_plots'] or calibration_configuration['create_result_plots']:
 #                 output_pdf_filename = calibration_configuration['configuration_file'] + '.pdf'
 #                 logging.info('Saving plot in: %s' % output_pdf_filename)
 #                 output_pdf = PdfPages(output_pdf_filename)
-#     
+#      
 #             mean_threshold_calibration = np.empty(shape=(len(calibration_configuration['parameter_values']),), dtype='<f8')  # array to hold the analyzed data in ram
 #             mean_threshold_rms_calibration = np.empty(shape=(len(calibration_configuration['parameter_values']),), dtype='<f8')  # array to hold the analyzed data in ram
 #             threshold_calibration = np.empty(shape=(80, 336, len(calibration_configuration['parameter_values'])), dtype='<f8')  # array to hold the analyzed data in ram
-#     
+#      
 #             progress_bar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', analysis_utils.ETA()], maxval=len(calibration_configuration['parameter_values']))
 #             progress_bar.start()
-#     
+#      
 #             for parameter_value_index, parameter_value in enumerate(calibration_configuration['parameter_values']):
 #                 if calibration_configuration['ignore_parameter_values'] is not None and parameter_value in calibration_configuration['ignore_parameter_values']:
 #                     continue
-#     
+#      
 #                 raw_data_file = scan_data_filenames[parameter_value]
 #                 analyzed_data_file = raw_data_file[:-3] + '_interpreted.h5'
 #                 analyze(raw_data_file=raw_data_file, analyzed_data_file=analyzed_data_file, fei4b=fei4b)
-#     
+#      
 #                 with tb.openFile(analyzed_data_file, mode="r") as in_file_h5:
 #                     # mask the not scanned columns for analysis and plotting
 #                     occupancy_masked = mask_columns(pixel_array=in_file_h5.root.HistOcc[:], ignore_columns=ignore_columns)
@@ -135,21 +149,21 @@ class ThresholdCalibration(FastThresholdScan):
 #                     threshold_calibration[:, :, parameter_value_index] = thresholds_masked.T
 #                 progress_bar.update(parameter_value_index)
 #             progress_bar.finish()
-#     
+#      
 #             if calibration_configuration['create_result_plots']:
 #                 plot_scatter(x=calibration_configuration['parameter_values'], y=mean_threshold_calibration, title='Threshold calibration', x_label=calibration_configuration["parameter_name"], y_label='Mean threshold', log_x=False, filename=output_pdf)
 #                 plot_scatter(x=calibration_configuration['parameter_values'], y=mean_threshold_calibration, title='Threshold calibration', x_label=calibration_configuration["parameter_name"], y_label='Mean threshold', log_x=True, filename=output_pdf)
 #                 plot_scatter(x=calibration_configuration['parameter_values'], y=mean_threshold_rms_calibration, title='Threshold calibration', x_label=calibration_configuration["parameter_name"], y_label='Threshold RMS', log_x=False, filename=output_pdf)
 #                 plot_scatter(x=calibration_configuration['parameter_values'], y=mean_threshold_rms_calibration, title='Threshold calibration', x_label=calibration_configuration["parameter_name"], y_label='Threshold RMS', log_x=True, filename=output_pdf)
-#     
+#      
 #             if calibration_configuration['create_plots'] or calibration_configuration['create_result_plots']:
 #                 output_pdf.close()
-#     
+#      
 #             # store the calibration data into a hdf5 file as an easy to read table and as an array for quick data access
 #             with tb.openFile(output_h5_filename, mode="w") as out_file_h5:
 #                 store_calibration_data_as_array(out_file_h5=out_file_h5, mean_threshold_calibration=mean_threshold_calibration, mean_threshold_rms_calibration=mean_threshold_rms_calibration, threshold_calibration=threshold_calibration)
 #                 store_calibration_data_as_table(out_file_h5=out_file_h5, mean_threshold_calibration=mean_threshold_calibration, mean_threshold_rms_calibration=mean_threshold_rms_calibration, threshold_calibration=threshold_calibration)
-#     
+#      
 #             if not os.path.isfile(analyzed_data_file) or calibration_configuration['overwrite_output_files']:
 #                 with AnalyzeRawData(raw_data_file=raw_data_file, analyzed_data_file=analyzed_data_file) as analyze_raw_data:
 #                     analyze_raw_data.create_tot_hist = False
@@ -162,7 +176,7 @@ class ThresholdCalibration(FastThresholdScan):
 #             #         analyze_raw_data.interpreter.print_summary()
 #             else:
 #                 logging.debug(analyzed_data_file + ' exists already, skip analysis.')
-
+#  
 #     def reanalyze(fei4b=False):
 #         data_files = analysis_utils.get_data_file_names_from_scan_base(calibration_configuration['scan_name'], filter_file_words=['analyzed', 'interpreted', 'cut_', 'cluster_sizes', 'trigger_fe'], parameter=True)
 #         data_files_par = analysis_utils.get_parameter_from_files(data_files, parameters=calibration_configuration["parameter_name"], unique=True, sort=True)
@@ -171,7 +185,7 @@ class ThresholdCalibration(FastThresholdScan):
 #             scan_data_filenames[parameter[calibration_configuration["parameter_name"]][0]] = file_name
 #         calibration_configuration['parameter_values'] = sorted(scan_data_filenames.keys())
 #         create_calibration(scan_data_filenames=scan_data_filenames, ignore_columns=calibration_configuration['ignore_columns'], fei4b=fei4b)
-
+#  
 #     def mask_columns(pixel_array, ignore_columns):
 #         idx = np.array(ignore_columns) - 1  # from FE to Array columns
 #         m = np.zeros_like(pixel_array)
