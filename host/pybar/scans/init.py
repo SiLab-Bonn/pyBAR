68b78b820261ef122e7ec35b55530dda8892da54
Jens Janssen <janssen@physik.uni-bonn.de>
2015-02-24 17:54:49 +0100

    GIT: adding Init scan

---
+++ host/pybar/scans/init.py
@@ -0,0 +1,22 @@
+import logging
+
+from pybar.fei4_run_base import Fei4RunBase
+from pybar.run_manager import RunManager
+
+
+class Init(Fei4RunBase):
+    '''Init scan
+    '''
+    _default_run_conf = {}
+
+    def configure(self):
+        pass
+
+    def scan(self):
+        logging.info('Init run...')
+
+    def analyze(self):
+        pass
+
+if __name__ == "__main__":
+    RunManager('../configuration.yaml').run_run(Init)
