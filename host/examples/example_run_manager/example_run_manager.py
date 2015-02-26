10b3e14aed8c17c14872e1663ebb795b58145609
Jens Janssen <janssen@physik.uni-bonn.de>
2015-02-26 10:49:52 +0100

    MAINT: update run manager example

--- host/examples/example_run_manager/example_run_manager.py
+++ host/examples/example_run_manager/example_run_manager.py
@@ -4,23 +4,51 @@ from pybar.scans.scan_ext_trigger import ExtTriggerScan
 from pybar.run_manager import run_status
 
 if __name__ == "__main__":
-    # The FE flavor can be set in configuration.yaml (for that use either fei4a or fei4b).
+    # The FE flavor can be set inside configuration.yaml (set the value to fei4a or fei4b).
+    # The initial FE configuration will be created during the first run.
+    #
+    # (from configuration.yaml)
+    # fe_flavor : fei4a
+    # ...
+    #
+    # Initializing the run manager:
     runmngr = RunManager('../../pybar/configuration.yaml')  # loading configuration file, specifying hardware configuration and module configuration.
     #
-    # Running primlist:
-    runmngr.run_primlist('example_run_manager.plst', skip_remaining=True)  # executing primlist.plst file, specific scan parameters are set inside the primlist file, skip remaining scans on error
-    # Each scan has a default run configuration, which is defined inside the corresponding scan file in /host/pybar/scans/. It is not necessary to define scan parameters inside primlist file.
+    # During the first run, an module data directory relative to the configuration.yaml file will be created.
+    # If configuration.yaml is placed inside /host/pybar/ the module data will be stored inside /host/pybar/<module_id> (where <module_id> is defined inside configuration.yaml).
     #
-    # Running a single scan and changing scan parameters:
+    # If fe_configuration inside configuration.yaml is not given, the latest valid FE configuration file will be taken (the file with the highest run number and run status 'FINISHED').
+    #
+    # (from configuration.yaml)
+    # fe_configuration:
+    # ...
+    #
+    # If no configuration file exists, a initial configuration will be create according to fe_flavor.
+    # To load a specific configuration file, a path to FE configuration file or a run number (e.g. 5) can be given:
+    #
+    # (from configuration.yaml)
+    # fe_configuration: 1
+    # ...
+    #
+    # This will retain the configuration for the following scans.
+    # Please note: no configuration file exists at this stage because no run was executed so far.
+    #
+    # Executing runs defined by a primlist:
+    runmngr.run_primlist('example_run_manager.plst', skip_remaining=True)  # executing primlist.plst file, specific run configuration are set inside the primlist file, skip remaining scans on error
+    # press Ctrl-C to abort the run at any time
+    # Each scan has a default run configuration, which is defined inside the corresponding scan file in /host/pybar/scans/.
+    # It is not necessary to add run parameters to the primlist file. If not given they are taken from the default run configuration (_default_run_conf).
+    #
+    # Running a single scan and changing default run configuration (_default_run_conf):
     join = runmngr.run_run(run=AnalogScan, run_conf={"scan_parameters": [('PlsrDAC', 500)], "n_injections": 1000}, use_thread=True)  # run_run returns a function object when use_thread is True
-    status = join()  # waiting here for finishing the scan, press Ctrl-C to abort the scan at any time
-    print 'Status:', status  # will wait for scan to be finished and returns run status
+    status = join()  # waiting here for finishing the run, press Ctrl-C to abort the run at any time
+    print 'Status:', status  # will wait for run to be finished and returns run status
     #
     # Or use a run configuration file:
     status = runmngr.run_run(run=AnalogScan, run_conf="run_configuration.txt")  # using no thread
     print 'Status:', status
     #
-    # Example for a loop of scans, which is failing:
+    # Example for a loop of runs, which is failing:
     for delay in range(14, 50, 16):
         join = runmngr.run_run(ExtTriggerScan, run_conf={"trigger_delay": delay, "no_data_timeout": 60}, use_thread=True)  # use thread
         print 'Status:', join(timeout=5)  # join has a timeout, return None if run has not yet finished
@@ -28,21 +56,6 @@ if __name__ == "__main__":
         if join() != run_status.finished:  # status OK?
             print 'ERROR!'
             break  # jump out
-    #
-    # After finishing the primlist/run: you will find the module data relative to the configuration.yaml file.
-    # If configuration.yaml is in /host/pybar/ the module data will be /host/pybar/<module_id> (where <module_id> is defined inside configuration.yaml).
-    # After finishing the first scan, the FE flavor can be commented out in configuration.yaml (for that use '#'):
-    #
-    # (from configuration.yaml)
-    # fe_configuration : # fei4a
-    # ...
-    #
-    # If fe_configuration is not given, the latest valid FE configuration will be taken (highest run number with status 'FINISHED').
-    # To load a specific configuration file, a path to FE configuration file or a run number (e.g. 5) can be used:
-    #
-    # (from configuration.yaml)
-    # fe_configuration: 5
-    # ...
     #
     # The configuration.yaml can be extended to change the default run parameters for each scan:
     #
