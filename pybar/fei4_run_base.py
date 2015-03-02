af4b7090ba8528e2293c700f07268d35e23898ab
Jens Janssen <janssen@physik.uni-bonn.de>
2015-03-02 01:32:41 +0100

    ENH: do not force dut_configuration

--- pybar/fei4_run_base.py
+++ pybar/fei4_run_base.py
@@ -215,8 +215,8 @@ class Fei4RunBase(RunBase):
                     self.dut.init(dut_configuration)
                 else:
                     self.dut.init(self.conf['dut_configuration'])
-            else:
-                raise ValueError('dut_configuration not given')
+#             else:
+#                 raise ValueError('dut_configuration not given')
 
             # additional init of the DUT
             self.init_dut()
