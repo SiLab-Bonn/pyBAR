d9f37703d2a332cdd8913033b5aeee0309fc5951
Jens Janssen <janssen@physik.uni-bonn.de>
2015-04-14 11:29:24 +0200

    MAINT: rename scan, fixes #9

--- pybar/scans/scan_init.py
+++ pybar/scans/scan_init.py
@@ -4,7 +4,7 @@ from pybar.fei4_run_base import Fei4RunBase
 from pybar.run_manager import RunManager
 
 
-class Init(Fei4RunBase):
+class InitScan(Fei4RunBase):
     '''Init scan
     '''
     _default_run_conf = {}
@@ -19,4 +19,4 @@ class Init(Fei4RunBase):
         pass
 
 if __name__ == "__main__":
-    RunManager('../configuration.yaml').run_run(Init)
+    RunManager('../configuration.yaml').run_run(InitScan)
