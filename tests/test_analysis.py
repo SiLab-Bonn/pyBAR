f95956f653d06f312462e6c0c51f819d33fb9be5
DavidLP <david-leon.pohl@rub.de>
2015-04-30 11:54:12 +0200

    ENH: add not exact value comparison of arrays, addressing #12 MAINT: rename calibration data files to autouse correct data files from given scan base

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -61,7 +61,7 @@ def get_array_differences(first_array, second_array):
         return ': ' + return_str
 
 
-def compare_h5_files(first_file, second_file, expected_nodes=None, detailed_comparison=True):
+def compare_h5_files(first_file, second_file, expected_nodes=None, detailed_comparison=True, exact=True):
     '''Takes two hdf5 files and check for equality of all nodes.
     Returns true if the node data is equal and the number of nodes is the number of expected nodes.
     It also returns a error string containing the names of the nodes that are not equal.
@@ -84,9 +84,9 @@ def compare_h5_files(first_file, second_file, expected_nodes=None, detailed_comp
     error_msg = ""
     with tb.open_file(first_file, 'r') as first_h5_file:
         with tb.open_file(second_file, 'r') as second_h5_file:
-            expected_nodes = sum(1 for _ in enumerate(first_h5_file.root)) if expected_nodes is None else expected_nodes  # set the number of expected nodes
-            nodes = sum(1 for _ in enumerate(second_h5_file.root))  # calculated the number of nodes
-            if nodes != expected_nodes:
+            n_expected_nodes = sum(1 for _ in enumerate(first_h5_file.root)) if expected_nodes is None else expected_nodes  # set the number of expected nodes
+            n_nodes = sum(1 for _ in enumerate(second_h5_file.root))  # calculated the number of nodes
+            if n_nodes != n_expected_nodes:
                 checks_passed = False
                 error_msg += 'The number of nodes in the file is wrong.\n'
             for node in second_h5_file.root:  # loop over all nodes and compare each node, do not abort if one node is wrong
@@ -94,15 +94,16 @@ def compare_h5_files(first_file, second_file, expected_nodes=None, detailed_comp
                 try:
                     expected_data = first_h5_file.get_node(first_h5_file.root, node_name)[:]
                     data = second_h5_file.get_node(second_h5_file.root, node_name)[:]
-                    try:
-                        if not (expected_data == data).all():  # compare the arrays for each element
+                    if (exact or expected_data.dtype.names is not None):  # exact comparison if exact is set and on recarray data (np.allclose does not work on recarray)
+                        if not np.all(expected_data == data):  # compare the arrays for each element
                             checks_passed = False
                             error_msg += node_name
                             if detailed_comparison:
                                 error_msg += get_array_differences(expected_data, data)
                             error_msg += '\n'
-                    except AttributeError:  # .all() only works on non scalars, recarray is somewhat a scalar
-                        if not (expected_data == data):
+                    else:
+                        if not np.allclose(expected_data, data):
+                            np.allclose(expected_data, data)
                             checks_passed = False
                             error_msg += node_name
                             if detailed_comparison:
@@ -211,10 +212,10 @@ class TestAnalysis(unittest.TestCase):
         os.remove(tests_data_folder + 'unit_test_data_4_interpreted_2.h5')
         os.remove(tests_data_folder + 'unit_test_data_4.pdf')
         os.remove(tests_data_folder + 'unit_test_data_4_parameter_128.pdf')
-        os.remove(tests_data_folder + 'hit_or_calibration_data.pdf')
-        os.remove(tests_data_folder + 'hit_or_calibration_data_calibration.pdf')
-        os.remove(tests_data_folder + 'hit_or_calibration_data_interpreted.h5')
-        os.remove(tests_data_folder + 'hit_or_calibration_data_calibration.h5')
+        os.remove(tests_data_folder + 'hit_or_calibration.pdf')
+        os.remove(tests_data_folder + 'hit_or_calibration_calibration.pdf')
+        os.remove(tests_data_folder + 'hit_or_calibration_interpreted.h5')
+        os.remove(tests_data_folder + 'hit_or_calibration_calibration.h5')
 
     def test_libraries_stability(self):  # calls 50 times the constructor and destructor to check the libraries
         progress_bar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.ETA()], maxval=50, term_width=80)
@@ -251,7 +252,7 @@ class TestAnalysis(unittest.TestCase):
         self.assertTrue(data_equal, msg=error_msg)
 
     def test_threshold_analysis(self):  # test the created interpretation file of the threshold data against the stored one
-        data_equal, error_msg = compare_h5_files(tests_data_folder + 'unit_test_data_2_result.h5', tests_data_folder + 'unit_test_data_2_interpreted.h5')
+        data_equal, error_msg = compare_h5_files(tests_data_folder + 'unit_test_data_2_result.h5', tests_data_folder + 'unit_test_data_2_interpreted.h5', exact=False)
         self.assertTrue(data_equal, msg=error_msg)
 
     def test_hit_data_analysis(self):  # test the hit histograming/clustering starting from the predefined interpreted data
@@ -370,11 +371,12 @@ class TestAnalysis(unittest.TestCase):
                 pass
             self.assertTrue(exception_ok & np.all(array == array_fast))
 
+#     @unittest.skip('No working right now')
     def test_hit_or_calibration(self):
-        create_hitor_calibration(tests_data_folder + 'hit_or_calibration_data')
-        data_equal, error_msg = compare_h5_files(tests_data_folder + 'hit_or_calibration_interpreted_result.h5', tests_data_folder + 'hit_or_calibration_data_interpreted.h5')
+        create_hitor_calibration(tests_data_folder + 'hit_or_calibration')
+        data_equal, error_msg = compare_h5_files(tests_data_folder + 'hit_or_calibration_interpreted_result.h5', tests_data_folder + 'hit_or_calibration_interpreted.h5')
         self.assertTrue(data_equal, msg=error_msg)
-        data_equal, error_msg = compare_h5_files(tests_data_folder + 'hit_or_calibration_result.h5', tests_data_folder + 'hit_or_calibration_data_calibration.h5')
+        data_equal, error_msg = compare_h5_files(tests_data_folder + 'hit_or_calibration_result.h5', tests_data_folder + 'hit_or_calibration_calibration.h5')
         self.assertTrue(data_equal, msg=error_msg)
 
 if __name__ == '__main__':
