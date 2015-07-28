1930f4db198fec4521c580f2e2d4202960c6335c
David Pohl <pohl@physik.uni-bonn.de>
2015-07-28 20:17:54 +0200

    ENH: do PlsrDAC calibration with pixels on to counteract pixel mask related shift of the PulserDAC transfer function

--- pybar/scans/calibrate_plsr_dac.py
+++ pybar/scans/calibrate_plsr_dac.py
@@ -10,14 +10,17 @@ import matplotlib.pyplot as plt
 
 from pybar.fei4_run_base import Fei4RunBase
 from pybar.run_manager import RunManager
+from pybar.fei4.register_utils import make_pixel_mask
 
 
 class PlsrDacScan(Fei4RunBase):
     _default_run_conf = {
-        "colpr_address": range(25, 39),  # the double column range to measure the PlsrDAC output voltage for
+        "colpr_address": range(25, 26),  # the double column range to measure the PlsrDAC output voltage for
         "scan_parameter": 'PlsrDAC',
         "scan_parameter_steps": range(0, 1024, 33),
-        "fit_range": [0, 700]
+        "fit_range": [0, 700],
+        "mask_steps": 3,  # number of injections per PlsrDAC step
+        "enable_shift_masks": ["Enable", "C_High", "C_Low"]
     }
 
     def set_scan_parameter(self, parameter, value):
@@ -25,18 +28,22 @@ class PlsrDacScan(Fei4RunBase):
         commands.extend(self.register.get_commands("ConfMode"))
         self.register.set_global_register_value(parameter, value)
         commands.extend(self.register.get_commands("WrRegister", name=[parameter]))
+        commands.extend(self.register.get_commands("RunMode"))
         self.register_utils.send_commands(commands)
 
     def configure(self):
         self.dut['Multimeter'].init()
         logging.info('Initialized multimeter %s' % self.dut['Multimeter'].get_name())
-        # Route Vcal to external pin
         commands = []
         commands.extend(self.register.get_commands("ConfMode"))
+        enable_mask = make_pixel_mask(steps=self.mask_steps, shift=0, default=0, value=1)  # Activate pixels for injection, although they are not read out
+        map(lambda mask_name: self.register.set_pixel_register_value(mask_name, enable_mask), self.enable_shift_masks)
+        commands.extend(self.register.get_commands("WrFrontEnd", same_mask_for_all_dc=True, name=self.enable_shift_masks, joint_write=True))
         self.register.set_global_register_value('Colpr_Mode', 0)
         self.register.set_global_register_value('ExtDigCalSW', 0)
-        self.register.set_global_register_value('ExtAnaCalSW', 1)
+        self.register.set_global_register_value('ExtAnaCalSW', 1)  # Route Vcal to external pin
         commands.extend(self.register.get_commands("WrRegister", name=['Colpr_Addr', 'Colpr_Mode', 'ExtDigCalSW', 'ExtAnaCalSW']))
+        commands.extend(self.register.get_commands("RunMode"))
         self.register_utils.send_commands(commands)
 
     def set_column(self, column_address):
@@ -48,7 +55,7 @@ class PlsrDacScan(Fei4RunBase):
         self.register_utils.send_commands(commands)
 
     def scan(self):
-        description = [('column_address', np.uint32), (self.scan_parameter, np.int32), ('voltage', np.float)]
+        description = [('column_address', np.uint32), (self.scan_parameter, np.int32), ('voltage', np.float)]  # output data table description
         data = self.raw_data_file.h5_file.create_table(self.raw_data_file.h5_file.root, name='plsr_dac_data', description=np.zeros((1, ), dtype=description).dtype, title='Data from PlsrDAC calibration scan')
 
         progress_bar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.AdaptiveETA()], maxval=len(self.scan_parameter_steps) * len(self.colpr_address), term_width=80)
@@ -77,12 +84,12 @@ class PlsrDacScan(Fei4RunBase):
 
     def analyze(self):
         logging.info('Analyze and plot results')
-        with tb.open_file(self.output_filename + '.h5', 'r+') as in_file_h5:
+        with tb.open_file(self.output_filename + '.h5', 'r') as in_file_h5:
             data = in_file_h5.root.plsr_dac_data[:]
             # Calculate mean PlsrDAC transfer function
             mean_data = np.zeros(shape=(len(self.scan_parameter_steps), ), dtype=[(self.scan_parameter, np.int32), ('voltage_mean', np.float), ('voltage_rms', np.float)])
             for index, parameter in enumerate(self.scan_parameter_steps):
-                mean_data[self.scan_parameter][index] = parameter 
+                mean_data[self.scan_parameter][index] = parameter
                 mean_data['voltage_mean'][index] = data['voltage'][data[self.scan_parameter] == parameter].mean()
                 mean_data['voltage_rms'][index] = data['voltage'][data[self.scan_parameter] == parameter].std()
             plt.errorbar(self.scan_parameter_steps, mean_data['voltage_mean'], mean_data['voltage_rms'])
