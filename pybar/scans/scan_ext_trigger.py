e27dd36a1fe0e5649e01c2dd634abf428d96f2fd
Jens Janssen <janssen@physik.uni-bonn.de>
2015-11-06 12:56:23 +0100

    BUG: fix crash when None

--- pybar/scans/scan_ext_trigger.py
+++ pybar/scans/scan_ext_trigger.py
@@ -113,7 +113,10 @@ class ExtTriggerScan(Fei4RunBase):
         self.fifo_readout.start(reset_sram_fifo=False, clear_buffer=True, callback=self.handle_data, errback=self.handle_err, no_data_timeout=self.no_data_timeout)
         self.dut['TDC']['ENABLE'] = self.enable_tdc
         self.dut['TLU']['TRIGGER_COUNTER'] = 0
-        self.dut['TLU']['MAX_TRIGGERS'] = self.max_triggers
+        if self.max_triggers:
+            self.dut['TLU']['MAX_TRIGGERS'] = self.max_triggers
+        else:
+            self.dut['TLU']['MAX_TRIGGERS'] = 0  # infinity triggers
         self.dut['CMD']['EN_EXT_TRIGGER'] = True
 
         def timeout():
