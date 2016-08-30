077a4f5dff36077f1976d656652db72fbbc365a9
Jens Janssen <janssen@physik.uni-bonn.de>
2016-08-30 15:34:58 +0200

    REG: fix import

--- pybar/scans/__init__.py
+++ pybar/scans/__init__.py
@@ -1,7 +1,9 @@
 from pybar.scans.calibrate_hit_or import HitOrCalibration, create_hitor_calibration
 from pybar.scans.calibrate_plsr_dac_transient import PlsrDacTransientCalibration
-from pybar.scans.calibrate_plsr_dac import PlsrDacCalibration
+from pybar.scans.calibrate_plsr_dac import PlsrDacCalibration, plot_pulser_dac
 from pybar.scans.calibrate_pulser_dac_correction import PulserDacCorrectionCalibration
+from pybar.scans.calibrate_plsr_dac_transient import PlsrDacTransientCalibration
+from pybar.scans.calibrate_plsr_dac_transient_advanced import PlsrDacTransientCalibrationAdvanced
 from pybar.scans.calibrate_threshold import ThresholdCalibration, create_threshold_calibration
 from pybar.scans.calibrate_tot import TotCalibration
 from pybar.scans.scan_analog import AnalogScan
@@ -31,4 +33,4 @@ from pybar.scans.tune_tdac import TdacTuning
 from pybar.scans.tune_threshold_baseline import ThresholdBaselineTuning
 from pybar.scans.tune_tlu import TluTuning
 
-__all__ = ["HitOrCalibration", "create_hitor_calibration", "PlsrDacTransientCalibration", "PlsrDacCalibration", "PulserDacCorrectionCalibration", "ThresholdCalibration", "create_threshold_calibration", "TotCalibration", "AnalogScan", "CrosstalkScan", "DigitalScan", "ExtTriggerGdacScan", "StopModeExtTriggerScan", "ExtTriggerScan", "FEI4SelfTriggerScan", "HitDelayScan", "IleakScan", "InitScan", "IVScan", "FastThresholdScan", "ThresholdScan", "RegisterTest", "TdcTest", "FdacTuning", "FeedbackTuning", "Fei4Tuning", "GdacTuning", "HotPixelTuning", "MergedPixelsTuning", "NoiseOccupancyScan", "StuckPixelScan", "TdacTuning", "ThresholdBaselineTuning", "TluTuning"]
+__all__ = ["HitOrCalibration", "create_hitor_calibration", "PlsrDacTransientCalibration", "PlsrDacTransientCalibrationAdvanced", "PlsrDacCalibration", "plot_pulser_dac", "PulserDacCorrectionCalibration", "ThresholdCalibration", "create_threshold_calibration", "TotCalibration", "AnalogScan", "CrosstalkScan", "DigitalScan", "ExtTriggerGdacScan", "StopModeExtTriggerScan", "ExtTriggerScan", "FEI4SelfTriggerScan", "HitDelayScan", "IleakScan", "InitScan", "IVScan", "FastThresholdScan", "ThresholdScan", "RegisterTest", "TdcTest", "FdacTuning", "FeedbackTuning", "Fei4Tuning", "GdacTuning", "HotPixelTuning", "MergedPixelsTuning", "NoiseOccupancyScan", "StuckPixelScan", "TdacTuning", "ThresholdBaselineTuning", "TluTuning"]
