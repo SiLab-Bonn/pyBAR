2d7a1cfd428794095d0891d2d3cab279eb98144b
Jens Janssen <janssen@physik.uni-bonn.de>
2017-07-26 17:41:02 +0200

    MAINT: updating firmware for MMC3 8 chip, using SiTCP V11

--- firmware/mmc3_beast_eth/src/mmc3_beast_eth.v
+++ firmware/mmc3_beast_eth/src/mmc3_beast_eth.v
@@ -1,4 +1,29 @@
+/**
+ * This file is part of pyBAR.
+ * 
+ * pyBAR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * pyBAR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with pyBAR.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+/**
+ * ------------------------------------------------------------
+ * Copyright (c) All rights reserved
+ * SiLab, Institute of Physics, University of Bonn
+ * ------------------------------------------------------------
+ */
+
+`timescale 1ps / 1ps
+`default_nettype none
 
 module mmc3_beast_eth(
     input wire RESET_N,
