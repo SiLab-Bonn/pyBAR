927adc22be2e0f3a77761bd84c3141d40b934ce9
Jens Janssen <janssen@physik.uni-bonn.de>
2019-09-11 13:06:34 +0200

    MAINT: remove deprecated code

--- pybar/utils/utils.py
+++ pybar/utils/utils.py
@@ -218,51 +218,6 @@ def iterable(item):
     else:
         return [item]
 
-# # {{{ http://code.activestate.com/recipes/285264/ (r1)
-# ---------------------------------------------------------
-# natsort.py: Natural string sorting.
-# ---------------------------------------------------------
-
-# By Seo Sanghyeon.  Some changes by Connelly Barnes.
-
-
-def try_int(s):
-    "Convert to integer if possible."
-    try:
-        return int(s)
-    except Exception:
-        return s
-
-
-def natsort_key(s):
-    "Used internally to get a tuple by which s is sorted."
-    import re
-    return map(try_int, re.findall(r'(\d+|\D+)', s))
-
-
-def natcmp(a, b):
-    "Natural string comparison, case sensitive."
-    return cmp(natsort_key(a), natsort_key(b))
-
-
-def natcasecmp(a, b):
-    "Natural string comparison, ignores case."
-    return natcmp(a.lower(), b.lower())
-
-
-def natsort(seq, cmp=natcmp):
-    "In-place natural string sort."
-    seq.sort(cmp)
-
-
-def natsorted(seq, cmp=natcmp):
-    "Returns a copy of seq, sorted by natural string sort."
-    import copy
-    temp = copy.copy(seq)
-    natsort(temp, cmp)
-    return temp
-# # end of http://code.activestate.com/recipes/285264/ }}}
-
 
 def get_iso_time():
     '''returns time as ISO string, mapping to and from datetime in ugly way
